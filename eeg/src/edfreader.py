743800328a3b9a66b230717c735e700f7045183d
Juuso Korhonen <juuso.korhonen@aalto.fi>
2020-04-30 11:31:53 +0300

    parent 0054d8a4f49f4c9da5bb3d7677dd40d163cd547e author Juuso Korhonen <juuso.korhonen@aalto.fi> 1588235513 +0300 committer Juuso Korhonen <juuso.korhonen@aalto.fi> 1588250261 +0300
    parent 0054d8a4f49f4c9da5bb3d7677dd40d163cd547e
    author Juuso Korhonen <juuso.korhonen@aalto.fi> 1588235513 +0300
    committer Juuso Korhonen <juuso.korhonen@aalto.fi> 1588250249 +0300
    
    parent 0054d8a4f49f4c9da5bb3d7677dd40d163cd547e
    author Juuso Korhonen <juuso.korhonen@aalto.fi> 1588235513 +0300
    committer Juuso Korhonen <juuso.korhonen@aalto.fi> 1588250220 +0300
    
    Rebasing from master
    
    Removing file from another branch.
    
    ADD: output folder to ignored dirs
    
    MOD: Added output folder for generated data
    
    ADD: Readersender dependency
    
    ADD: Configuration for flake8 linter
    
    ADD: readersender
    
    ADD: EDF_TEST_FILE default location
    
    ADD: EDFReader class and pytest tests
    
    ADD: Placeholder for KafkaSender
    
    FIX: small syntax error
    
    ADD: reset() method to EDFReader
    
    DEL: file that belongs to another branch
    
    DEL: file that belongs to another branch
    
    ADD: output folder to ignored dirs
    
    ADD: Readersender dependency
    
    ADD: Configuration for flake8 linter
    
    ADD: readersender
    
    ADD: EDF_TEST_FILE default location
    
    ADD: EDFReader class and pytest tests
    
    ADD: Placeholder for KafkaSender
    
    ADD: reset() method to EDFReader
    
    DEL: Files that belong to another branch

---
+++ eeg/src/edfreader.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pathlib
+import logging
+import numpy as np
+import mne.io
+from readersender import Reader
+
+
+class EDFReader(Reader):
+    """A Reader class the read EDF data files row-by-row.
+    """
+    def __init__(self, edf_file, *args, **kwargs):
+        """Initializes an EDFReader with given file.
+
+        Parameters
+        ----------
+        edf_file : path or str_like
+            Path to EDF file.
+        """
+        super().__init__(*args, **kwargs)
+
+        edf_path = pathlib.Path(edf_file)
+        if not edf_path.is_file():
+            raise FileNotFoundError("File '{}' not found.".format(edf_file))
+        self._edf_path = edf_path
+
+        self._edf_data = None
+        self._edf_fs = 0
+        self._edf_len = 0
+        self._edf_iter = None
+
+    @property
+    def edf_file(self):
+        """The EDF file attached to this reader.
+        """
+        return str(self._edf_path)
+
+    @edf_file.setter
+    def edf_file(self, edf_file):
+        """Sets the EDF file to read.
+
+        Parameters
+        ----------
+        edf_file : path or str_like
+            Path to EDF file.
+
+        Notes
+        -----
+        If reader will be disconnected after changing the file.
+        """
+        if self.connected():
+            self.disconnect()
+
+        edf_path = pathlib.Path(edf_file)
+        if not edf_path.is_file():
+            raise FileNotFoundError("File '{}' not found.".format(edf_file))
+        self._edf_path = edf_path
+
+        self._edf_data = None
+        self._edf_fs = 0
+        self._edf_len = 0
+        self._edf_iter = None
+
+    @property
+    def data(self):
+        """Returns the loaded EDF data.
+
+        Notes
+        -----
+        Call 'connect()' before this command.
+        """
+        if not self.connected:
+            raise RuntimeError("Call 'connect()' before trying to read data.")
+
+        return self._edf_data
+
+    @property
+    def data_len(self):
+        """Returns the size of the loaded EDF file.
+
+        Notes
+        -----
+        Call 'connect()' before this command.
+        """
+        if not self.connected:
+            raise RuntimeError("Call 'connect()' before trying to read data.")
+
+        return self._edf_len
+
+    @property
+    def data_frequency(self):
+        """Returns the frequency of the loaded EDF file.
+
+        Notes
+        -----
+        Call 'connect()' before this command.
+        """
+        if not self.connected:
+            raise RuntimeError("Call 'connect()' before trying to read data.")
+
+        return self._edf_fs
+
+    @property
+    def connected(self):
+        """Checks if EDF file is read to memory.
+        """
+        return self._edf_data is not None
+
+    def connect(self):
+        """Reads the EDF file to memory.
+        """
+        if not self.connected:
+            # Read the data on at a time
+            self.log("Preloading data", logging.INFO)
+            raw = mne.io.read_raw_edf(self._edf_path, preload=True)
+            data = raw.get_data(return_times=False)
+            self._edf_data = np.transpose(data)
+            self._edf_fs = raw.info['sfreq']
+            self._edf_len = len(self._edf_data)
+            self._edf_iter = None
+            self.log(f"Frequency = {self._edf_fs}; Cycle time = {1.0 / self._edf_fs}.",
+                     logging.INFO)
+
+    def disconnect(self):
+        """Deletes the EDF file from memory.
+        """
+        if self.connected:
+            self._edf_data = None
+            self._fs = 0
+            self._edf_len = 0
+            self._edf_iter = None
+
+    def reset(self):
+        """Resets the data to the beginning.
+
+        Notes
+        -----
+        Call 'connect()' before this command.
+        """
+        if self._edf_iter is not None:
+            # Delete the iterator
+            self._edf_iter = None
+
+    def read(self):
+        """Reads a single line from EDF file.
+
+        Yields
+        ------
+        np.array
+            A single line from EDF file.
+        """
+        if not self.connected:
+            raise RuntimeError("Call 'connect()' before trying to read data.")
+
+        if self._edf_iter is None:
+            # Initialize an iterator
+            self._edf_iter = iter(self._edf_data)
+
+        try:
+            return next(self._edf_iter)
+        except StopIteration:
+            return None
---
+++ eeg/src/test_edfreader.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import dotenv
+import logging
+import pytest
+dotenv.load_dotenv()
+
+
+def test_edfreader():
+    """Tests EDFReader class.
+    """
+    from readersender import (ReaderSender, Reader)
+    from edfreader import EDFReader
+
+    test_edf_file = os.path.join("..", os.getenv('DATASET_PATH'), os.getenv('EDF_TEST_FILE'))
+    if not os.path.isfile(test_edf_file):
+        raise FileNotFoundError("File '{}' not found. Cannot test reading EDF. Perhaps you need to set 'DATASET_PATH' and 'EDF_TEST_FILE' in .env.".format(test_edf_file))
+
+    reader = EDFReader(edf_file=test_edf_file, logger=logging.getLogger('test_edfreader'))
+    assert isinstance(reader, EDFReader)
+    assert isinstance(reader, Reader)
+    assert isinstance(reader, ReaderSender)
+
+    assert not reader.connected
+    with pytest.raises(RuntimeError):
+        reader.read()   # NOTE: This should fail as we are not connected
+
+    reader.connect()
+    assert reader.connected
+
+    data_len = reader.data_len
+    assert data_len > 0
+
+    data_f = reader.data_frequency
+    assert data_f > 0
+
+    for _ in range(data_len):
+        assert reader.read() is not None
+
+    assert reader.read() is None   # Iteration has stopped, so it should be None
+    assert reader.read() is None   # None should repeat
+
+    assert reader.connected
+    reader.disconnect()
+    assert not reader.connected
+
+    with pytest.raises(RuntimeError):
+        reader.data
+
+    with pytest.raises(RuntimeError):
+        reader.data_len
+
+    with pytest.raises(RuntimeError):
+        reader.data_frequency
+
+    # Test reading the same data again
+    reader.connect()
+    assert reader.connected
+
+    data_len = reader.data_len
+    assert data_len > 0
+
+    data_f = reader.data_frequency
+    assert data_f > 0
+
+    # Read in a bit of data
+    for _ in range(min(10, data_len-1)):   # Note min-func makes sure that we don't over extend
+        assert reader.read() is not None
+
+    reader.disconnect()
+    assert not reader.connected
+
+    # This should now reset the data count
+    reader.connect()
+    assert reader.connected
+
+    # We should now read in all data
+    for _ in range(data_len):
+        assert reader.read() is not None
+
+    assert reader.read() is None   # Iteration has stopped, so it should be None
+
+    # Try index reset
+    reader.reset()
+
+    # Read in all data
+    for _ in range(data_len):
+        assert reader.read() is not None
+
+    assert reader.read() is None   # Iteration has stopped, so it should be None
