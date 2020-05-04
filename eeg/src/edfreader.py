5bb2dcd3b2ef15eef1215bb940b846821a6b4acd
Juuso Korhonen <juuso.korhonen@aalto.fi>
2020-05-04 16:38:55 +0300

    MOD: reduce boilerplate using decorators

--- eeg/src/edfreader.py
+++ eeg/src/edfreader.py
@@ -5,6 +5,7 @@ import logging
 import numpy as np
 import mne.io
 from readersender import Reader
+from readersender.helpers import (only_connected, only_disconnected)
 
 
 class EDFReader(Reader):
@@ -63,85 +64,72 @@ class EDFReader(Reader):
         self._edf_iter = None
 
     @property
+    @only_connected(action='raise')
     def data(self):
         """Returns the loaded EDF data.
 
-        Notes
-        -----
-        Call 'connect()' before this command.
         """
-        if not self.connected:
-            raise RuntimeError("Call 'connect()' before trying to read data.")
-
         return self._edf_data
 
     @property
+    @only_connected(action='raise')
     def data_len(self):
         """Returns the size of the loaded EDF file.
 
-        Notes
-        -----
-        Call 'connect()' before this command.
         """
-        if not self.connected:
-            raise RuntimeError("Call 'connect()' before trying to read data.")
-
         return self._edf_len
 
     @property
+    @only_connected(action='raise')
     def data_frequency(self):
         """Returns the frequency of the loaded EDF file.
 
-        Notes
-        -----
-        Call 'connect()' before this command.
         """
-        if not self.connected:
-            raise RuntimeError("Call 'connect()' before trying to read data.")
-
         return self._edf_fs
 
     @property
     def connected(self):
         """Checks if EDF file is read to memory.
+
         """
         return self._edf_data is not None
 
+    @only_disconnected()
     def connect(self):
         """Reads the EDF file to memory.
-        """
-        if not self.connected:
-            # Read the data on at a time
-            self.log("Preloading data", logging.INFO)
-            raw = mne.io.read_raw_edf(self._edf_path, preload=True)
-            data = raw.get_data(return_times=False)
-            self._edf_data = np.transpose(data)
-            self._edf_fs = raw.info['sfreq']
-            self._edf_len = len(self._edf_data)
-            self._edf_iter = None
-            self.log(f"Frequency = {self._edf_fs}; Cycle time = {1.0 / self._edf_fs}.",
-                     logging.INFO)
 
+        """
+        # Read the data on at a time
+        self.log("Preloading data", logging.INFO)
+        raw = mne.io.read_raw_edf(self._edf_path, preload=True)
+        data = raw.get_data(return_times=False)
+        self._edf_data = np.transpose(data)
+        self._edf_fs = raw.info['sfreq']
+        self._edf_len = len(self._edf_data)
+        self._edf_iter = None
+        self.log(f"Frequency = {self._edf_fs}; Cycle time = {1.0 / self._edf_fs}.",
+                 logging.INFO)
+
+    @only_connected()
     def disconnect(self):
         """Deletes the EDF file from memory.
-        """
-        if self.connected:
-            self._edf_data = None
-            self._fs = 0
-            self._edf_len = 0
-            self._edf_iter = None
 
+        """
+        self._edf_data = None
+        self._fs = 0
+        self._edf_len = 0
+        self._edf_iter = None
+
+    @only_connected()
     def reset(self):
         """Resets the data to the beginning.
 
-        Notes
-        -----
-        Call 'connect()' before this command.
         """
         if self._edf_iter is not None:
             # Delete the iterator
             self._edf_iter = None
 
+    @only_connected(action='raise')
     def read(self):
         """Reads a single line from EDF file.
 
@@ -150,9 +138,6 @@ class EDFReader(Reader):
         np.array
             A single line from EDF file.
         """
-        if not self.connected:
-            raise RuntimeError("Call 'connect()' before trying to read data.")
-
         if self._edf_iter is None:
             # Initialize an iterator
             self._edf_iter = iter(self._edf_data)
