45a70475f1edf7cd1b8b71faf922298eb5bb1ac0
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2020-12-06 13:43:42 +0200

    ADD: Kafka-LabVIEW bridge to pass values from Kafka to LabVIEW

---
+++ kafka/topic_queue.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from queue import Queue
+
+from .listener import KafkaListener
+
+class TopicQueue():
+    """A helper class that pushes new data from several topics into a single queue.
+
+    """
+    def __init__(self, topics):
+        """Initialize the topic queue.
+
+        Parameters
+        ----------
+        topics : array_like
+            An array of topic names.
+        """
+        self._topics = topics
+        self._queue = Queue()
+
+        callback = lambda topic, value:\
+            self._queue.put({
+                'topic': topic,
+                'value': value
+            })
+
+        # Initialize listener for each topic.
+        self._listeners = [
+            KafkaListener(
+                topic=topic,
+                callback=callback,
+            ) for topic in topics
+        ]
+
+        # Start listener threads.
+        for listener in self._listeners:
+            listener.start()
+
+    def get(self):
+        """Pop from the queue.
+
+        Returns
+        -------
+        dict
+            A dictionary of the form
+            {
+                'topic': topic,
+                'value: value
+            }
+            where topic is a string indicating the name of the topic, and value is the new value.
+        """
+        return self._queue.get()
