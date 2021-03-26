578e65db972ee23e6e608def6f349bebfac673bc
Juuso Korhonen <juuso.korhonen@aalto.fi>
2021-03-26 11:06:33 +0200

    AsyncAPI generation with 'make asyncapi'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,8 @@ source_suffix = ['.rst', '.md']
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.githubpages", "recommonmark", "sphinx_markdown_tables"]
+extensions = ["sphinx.ext.githubpages",
+              "recommonmark", "sphinx_markdown_tables"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
