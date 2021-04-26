c3b78b2eaa63462c7e338cbdc39842bc73606382
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2021-04-26 14:24:34 +0300

    ADD: Simple healthcheck for frontend
    At the moment, only show the status of the connection between the frontend and the backend.
    
    Also fix previous style issues that Vue warned of.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,18 +2,28 @@ import Vue from "vue";
 import VueSocketIO from "vue-socket.io";
 import SocketIO from "socket.io-client";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+
 import App from "./App.vue";
 import router from "./router";
+
+library.add(faCircle);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
 // XXX(okahilak): Connection objects must be used here for CORS to work in Socket.IO connection,
 //   see https://github.com/MetinSeylan/Vue-Socket.io/issues/295.
-const socketConnectionState = SocketIO('http://localhost:5000');
+const socketConnectionState = SocketIO("http://localhost:5000");
 
-Vue.use(new VueSocketIO({
+Vue.use(
+  new VueSocketIO({
     debug: false,
-    connection: socketConnectionState,
+    connection: socketConnectionState
   })
 );
 
