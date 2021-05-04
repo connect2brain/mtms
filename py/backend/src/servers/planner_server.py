a51e9d4cc02417df855e47245c7df26b719f4d58
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2021-05-04 16:40:01 +0300

    MOD: Add type annotations to planner_server.py
    Also remove None default values from function arguments.

--- py/backend/src/servers/planner_server.py
+++ py/backend/src/servers/planner_server.py
@@ -4,34 +4,54 @@
 import logging
 import json
 from functools import partial
+from typing import Any, Dict, List, Tuple, TypedDict
+
+from flask_socketio import SocketIO
+from pykafka.producer import Producer
+
+from mtms.kafka.kafka import Kafka
+from mtms.kafka.listener import KafkaListener
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s [%(levelname)s] (%(threadName)-10s) %(message)s',)
+
+Position = Tuple[float, float, float]
+
+class AddPointData(TypedDict):
+    position: Position
+
+class PointAddedData(TypedDict):
+    visible: bool
+    name: str
+    type: str
+    comment: str
+    position: Position
 
 class PlannerServer:
     """A backend server for the planner.
 
     """
 
-    _NEURONAVIGATION_TOPICS = [
+    _NEURONAVIGATION_TOPICS: List[str] = [
         'Set cross focal point',
     ]
-    _COMMAND_ADD_POINT = 'point.add'
+    _COMMAND_ADD_POINT: str = 'point.add'
 
-    def __init__(self, kafka=None, socketio=None):
+    def __init__(self, kafka: Kafka, socketio: SocketIO) -> None:
         """Initialize the planner server.
 
         Parameters
         ----------
-        kafka : Kafka
+        kafka
             A Kafka object to communicate with Kafka.
-        socketio : flask_socketio.SocketIO
+        socketio
             A SocketIO object to which the event listeners are added.
         """
-        self._socketio = socketio
-        self._kafka = kafka
+        self._socketio: SocketIO = socketio
+        self._kafka: Kafka = kafka
 
         # Handlers for messages sent from neuronavigation.
+        topic: str
         for topic in self._NEURONAVIGATION_TOPICS:
             socketio.on_event(
                 topic,
@@ -45,22 +65,22 @@ class PlannerServer:
         )
 
         # A Kafka listener for adding a new point.
-        self._add_point_listener = self._kafka.get_listener(
+        self._add_point_listener: KafkaListener = self._kafka.get_listener(
             topic=self._COMMAND_ADD_POINT,
             callback=self._point_added,
         )
         self._add_point_listener.start()
 
-        self.id_ = 0
+        self.id_: int = 0
 
-    def _handle_neuronavigation_message(self, topic, data):
+    def _handle_neuronavigation_message(self, topic: str, data: Dict[Any, Any]) -> None:
         """A handler for messages from neuronavigation.
 
         Parameters
         ----------
-        topic : str
+        topic
             The topic in which the message is sent.
-        data : dict
+        data
             A dict consisting of the message.
         """
         # XXX: For now, broadcast the message with Socket.IO. Later, ensure that neuronavigation
@@ -68,15 +88,17 @@ class PlannerServer:
         #   from Socket.IO.
         self._socketio.emit(topic, data)
 
-    def _add_point(self, data):
+    def _add_point(self, data: AddPointData) -> None:
         """When a command is received from the front-end to add a new point, create the
         initial values for the point and pass the message to create a point to Kafka.
 
         Parameters
         ----------
-        data : dict
+        data
             The data sent from the front-end with the command. Should consist of
-            'position' key, with a value that consists of a list of three numbers.
+            'position' key, with a value that consists of a list of three floats.
+
+            See type AddPointData for the specification.
 
             An example:
 
@@ -85,7 +107,7 @@ class PlannerServer:
             }
         """
         self.id_ += 1
-        value = {
+        value: PointAddedData = {
             'visible': False,
             'name': "Target-" + str(self.id_),
             'type': "Target",
@@ -95,18 +117,18 @@ class PlannerServer:
 
         # XXX: Recreating the producer for each command is slow with PyKafka, revisit after changing
         #   the Kafka library to a faster one.
-        producer = self._kafka.get_producer(topic="point.add")
+        producer: Producer = self._kafka.get_producer(topic="point.add")
         producer.produce(bytes(json.dumps(value), encoding='utf8'))
 
-    def _point_added(self, topic, data):
+    def _point_added(self, topic: str, data: str):
         """When a command is received from Kafka to add a new point, pass that command onto the
         front-end.
 
         Parameters
         ----------
-        topic : str
+        topic
             The name of the topic in which the command is sent.
-        data : str
+        data
             A json dict consisting of the attributes of the new point. The dict should
             consist of the keys 'visible', 'name', 'type', 'comment', and 'position'.
 
@@ -123,5 +145,5 @@ class PlannerServer:
         # TODO: The format and the content of the received message needs to be checked.
         #       A natural place for those checks would be inside Kafka listener, so this
         #       function can then assume a valid message.
-        data = json.loads(data)
+        data: PointAddedData = json.loads(data)
         self._socketio.emit(topic, data)
