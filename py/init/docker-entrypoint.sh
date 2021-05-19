0eca49b4dd1d7ae8277f46464e81bc194917f3c2
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2021-05-19 15:15:46 +0300

    MOD: Wait for init service to complete before starting backend

--- py/init/docker-entrypoint.sh
+++ py/init/docker-entrypoint.sh
@@ -4,4 +4,11 @@ set -e
 
 ./wait-for-it.sh "$KAFKA_IP:$KAFKA_PORT" -t 60
 
+# Run the initialization
 poetry run python py/init/src/runner.py
+
+echo Initialization complete. Telling that to other services by opening port $INIT_PORT.
+while true
+do
+  nc -lnvp $INIT_PORT
+done
