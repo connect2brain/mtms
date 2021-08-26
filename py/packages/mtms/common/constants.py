98eb343b12a87e1303fe42af9b2b4656a3af5668
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2021-08-26 22:27:54 +0300

    ADD: Show status of the serial port connection in UI
    - Needs to be combined with the InVesalius commit 871f59ad so that
      the status of the serial port connection is published in the correctly
      named event. Updating InVesalius in a later commit. Meanwhile, this
      commit does not do anything, but does not break anything, either.

--- py/packages/mtms/common/constants.py
+++ py/packages/mtms/common/constants.py
@@ -3,8 +3,10 @@
 
 # Kafka commands shared across modules.
 
-KAFKA_COMMAND_SET_STIMULATION_PARAMETERS: str = 'stimulation.set_parameters'
 KAFKA_COMMAND_SET_COIL_AT_TARGET: str = 'stimulation.set_coil_at_target'
+KAFKA_COMMAND_SET_STIMULATION_PARAMETERS: str = 'stimulation.set_parameters'
+
+KAFKA_COMMAND_SET_SERIAL_PORT_CONNECTION: str = 'status.set_serial_port_connection'
 
 # Other constants.
 
