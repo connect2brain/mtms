340be9ceeac2759c0f36a1008ca14d7a6b3e0816
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2021-08-26 19:24:01 +0300

    MOD: Small changes to UI layout and organization
    - Move ConnectToBrain text and logo to About page so that the front
      page only contains the navigation links.
    
    - Move navigation-related code from App.vue to Nav.vue and delete
      Home.vue.
    
    - Remove $navigator-active-link-font-color, as the router does not
      have an active link anymore.

---
+++ frontend/src/views/Nav.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="nav">
+    <div id="nav">
+      <!-- XXX: It is not a good practice to have a bogus value for href attribute,
+                see:
+
+                https://developer.mozilla.org/en-US/docs/Web/API/Window/open#best_practices
+
+                However, maybe it works well enough for our use case.
+      -->
+      <a href="#" v-on:click.stop.prevent="openWindow('Calibration')">Calibration</a> |
+      <a href="#" v-on:click.stop.prevent="openWindow('Planner')">Planner</a> |
+      <a href="#" v-on:click.stop.prevent="openWindow('TMS')">TMS</a> |
+      <a href="#" v-on:click.stop.prevent="openWindow('EEG')">EEG</a> |
+      <a href="#" v-on:click.stop.prevent="openWindow('About')">About</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  created() {
+    this.WINDOW_INFO = {
+      EEG: {
+        url: '/eeg',
+        width: 980,
+        height: 750
+      },
+      Calibration: {
+        url: '/calibration',
+        width: 300,
+        height: 480
+      },
+      Planner: {
+        url: '/planner',
+        width: 420,
+        height: 420
+      },
+      TMS: {
+        url: '/tms',
+        width: 400,
+        height: 400
+      },
+      About: {
+        url: '/about',
+        width: 400,
+        height: 400
+      }
+    };
+  },
+
+  methods: {
+    openWindow: function (windowName) {
+      const info = this.WINDOW_INFO[windowName];
+
+      const url = info['url'];
+      const width = info['width'];
+      const height = info['height'];
+
+      const windowFeatures = `width=${width}, height=${height}, menubar=1, toolbar=1, resizable=0`;
+
+      const windowReference = window.open(url, windowName, windowFeatures);
+      windowReference.focus();
+
+      return false;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "../styles/_colors.scss";
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: $default-font-color;
+}
+</style>
