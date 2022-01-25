e2e173af8db50f527f0f601c926de9f7dac60618
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2022-01-25 03:38:29 +0200

    FIX: Fix setup.py when using ROS in Linux
    For some reason, pbr doesn't work properly in Linux installation
    of ROS. Specifically, using pbr's custom way of specifying data_files
    crashes with an obscure error.
    
    Therefore, ditch pbr and instead generate data_files parameter
    for setup function call in setup.py manually by traversing
    the directory tree.
    
    (NB: We are merging two repositories; this commit is from
    invesalius_ros repository.)

--- ros2_ws/src/neuronavigation_pkg/setup.py
+++ ros2_ws/src/neuronavigation_pkg/setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import find_packages, setup
 
 # Search for InVesalius's internal packages in invesalius3 directory, allowing
@@ -9,30 +10,43 @@ package_dir = {s: 'invesalius3/' + s.replace('.', '/') for s in packages}
 packages.append('invesalius3')
 packages.append('bridge')
 
-setup(
-    # Use pbr extension to setuptools. Without that, copying data files, e.g.,
-    # the directory structure in invesalius_pkg/locale, is hard because
-    # 'data_files' parameter in setuptools does not support copying directories;
-    # it only supports copying single files without retaining the directory
-    # structure. See, e.g.,
-    #
-    # https://stackoverflow.com/questions/27829754/include-entire-directory-in-\
-    # python-setup-py-data-files
-    #
-    setup_requires=['pbr'],
-    pbr=True,
+# XXX: setuptools doesn't directly support copying directories while retaining the
+#   directory structure: the directory has to be specified for each copied file separately.
+#   Therefore, manually generate the list of files to copy by traversing the directory
+#   using os.walk().
+#
+#   Adapted from:
+#
+#   https://stackoverflow.com/questions/27829754/include-entire-directory-in-python-setup-py-data-files
+#
+def generate_data_files():
+    dirs = (
+        ('lib/python3.8/site-packages/locale', 'invesalius3/locale'),
+        ('lib/python3.8/site-packages/icons', 'invesalius3/icons'),
+        ('lib/python3.8/site-packages/invesalius_cy', 'invesalius3/invesalius_cy'),
+    )
 
+    data_files = []
+    for dest_dir, source_dir in dirs:
+        for dirpath, _, filenames in os.walk(source_dir):
+            dirpath_relative = os.path.relpath(dirpath, source_dir)
+            install_dir = os.path.join(dest_dir, dirpath_relative)
+            list_entry = (install_dir, [os.path.join(dirpath, f) for f in filenames if not f.startswith('.')])
+            data_files.append(list_entry)
+
+    data_files.append(('share/ament_index/resource_index/packages', ['resource/neuronavigation_pkg']))
+    data_files.append(('share/neuronavigation_pkg', ['package.xml']))
+
+    return data_files
+
+setup(
     name='neuronavigation_pkg',
     version='0.0.1',
     packages=packages,
     package_dir=package_dir,
     install_requires=['setuptools'],
-
-    # XXX: Does not seems to work: generates a file 'not-zip-safe' to the egg-info
-    #   directory. Most likely due to pbr conflicting with setuptools.
-    #
+    data_files=generate_data_files(),
     zip_safe=True,
-
     author='Olli-Pekka Kahilakoski',
     author_email='olli-pekka.kahilakoski@aalto.fi',
     description='Python packages for neuronavigation',
