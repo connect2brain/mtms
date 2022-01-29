2fcc0f4bda50ebe1c2ae8c11362ce54c242b890e
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2022-01-29 02:00:14 +0200

    ROS migration: migrate 'remove target' function in planner

--- ros2_ws/src/planner/planner/planner.py
+++ ros2_ws/src/planner/planner/planner.py
@@ -6,7 +6,33 @@ from geometry_msgs.msg import Point
 from mtms_interfaces.msg import PlannerState, Target
 from std_msgs.msg import Bool, Float64, String
 
-from mtms_interfaces.srv import AddTarget, ToggleSelect
+from mtms_interfaces.srv import AddTarget, RemoveTarget, ToggleSelect
+
+
+# TODO: Move this from here to some utils-like module
+#
+def drop_unique(l, cond):
+    """Given a list and a condition, drop the element for which the condition is True.
+    Modify the list in place. Return the index of the dropped element.
+
+    If the condition is True for none of the elements or more than one element, raise an error.
+
+    Parameters
+    ----------
+    l
+        The list of interest.
+    cond
+        A function that takes an element of the list and return a boolean.
+    """
+    index = None
+    for i, element in enumerate(l):
+        if cond(element):
+            assert index is None, "Condition is True for more than one element"
+            index = i
+    assert index is not None, "Condition is True for none of the elements"
+    del l[index]
+
+    return index
 
 
 class Planner(Node):
@@ -14,6 +40,7 @@ class Planner(Node):
     def __init__(self):
         super().__init__('planner')
         self.create_service(AddTarget, '/planner/add_target', self.add_target_callback)
+        self.create_service(RemoveTarget, '/planner/remove_target', self.remove_target_callback)
         self.create_service(ToggleSelect, '/planner/toggle_select', self.toggle_select_callback)
 
         # Persist the latest sample.
@@ -74,6 +101,21 @@ class Planner(Node):
             msg.targets.append(target)
 
         self._state_publisher.publish(msg)
+
+        response.success = True
+        return response
+
+    def remove_target_callback(self, request, response):
+        self.get_logger().info('Incoming request')
+
+        state = self._state
+        if state is None:
+            response.success = False
+            return response
+
+        idx = drop_unique(state.targets, lambda target: target.name == request.name)
+
+        self._state_publisher.publish(state)
 
         response.success = True
         return response
