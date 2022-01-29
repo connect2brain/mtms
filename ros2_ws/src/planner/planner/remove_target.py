84341ca0393e899c619cddc915ea6e4e22289f91
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2022-01-29 02:35:54 +0200

    ROS migration: Create StateNode class for planner
    Extract common parts of different planner functions
    (add target etc.) into the class, namely, keeping track
    of the planner state.

--- ros2_ws/src/planner/planner/remove_target.py
+++ ros2_ws/src/planner/planner/remove_target.py
@@ -1,9 +1,10 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import DurabilityPolicy, HistoryPolicy, QoSProfile
 
 from mtms_interfaces.msg import PlannerState, Target
 from mtms_interfaces.srv import RemoveTarget
+
+from .state_node import StateNode
 
 
 # TODO: Move this from here to some utils-like module
@@ -32,35 +33,11 @@ def drop_unique(l, cond):
     return index
 
 
-class RemoveTargetNode(Node):
+class RemoveTargetNode(StateNode):
 
     def __init__(self):
         super().__init__('remove_target')
         self.create_service(RemoveTarget, '/planner/remove_target', self.remove_target_callback)
-
-        # Persist the latest sample.
-        qos = QoSProfile(
-            depth=1,
-            durability=DurabilityPolicy.TRANSIENT_LOCAL,
-            history=HistoryPolicy.KEEP_LAST,
-        )
-
-        self._state_publisher = self.create_publisher(
-            PlannerState,
-            "/planner/state",
-            qos
-        )
-        self._state_subscriber = self.create_subscription(
-            PlannerState,
-            '/planner/state',
-            self.state_updated,
-            10
-        )
-        self._state = None
-
-    def state_updated(self, msg):
-        self.get_logger().info('Planner state updated')
-        self._state = msg
 
     def remove_target_callback(self, request, response):
         self.get_logger().info('Incoming request')
--- ros2_ws/src/planner/planner/toggle_select.py
+++ ros2_ws/src/planner/planner/toggle_select.py
@@ -1,6 +1,5 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import DurabilityPolicy, HistoryPolicy, QoSProfile
 
 from geometry_msgs.msg import Point
 from mtms_interfaces.msg import PlannerState, Target
@@ -8,36 +7,14 @@ from std_msgs.msg import Bool, Float64, String
 
 from mtms_interfaces.srv import ToggleSelect
 
+from .state_node import StateNode
 
-class ToggleSelectNode(Node):
+
+class ToggleSelectNode(StateNode):
 
     def __init__(self):
         super().__init__('toggle_select')
         self.create_service(ToggleSelect, '/planner/toggle_select', self.toggle_select_callback)
-
-        # Persist the latest sample.
-        qos = QoSProfile(
-            depth=1,
-            durability=DurabilityPolicy.TRANSIENT_LOCAL,
-            history=HistoryPolicy.KEEP_LAST,
-        )
-
-        self._state_publisher = self.create_publisher(
-            PlannerState,
-            "/planner/state",
-            qos
-        )
-        self._state_subscriber = self.create_subscription(
-            PlannerState,
-            '/planner/state',
-            self.state_updated,
-            10
-        )
-        self._state = None
-
-    def state_updated(self, msg):
-        self.get_logger().info('Planner state updated')
-        self._state = msg
 
     def toggle_select_callback(self, request, response):
         self.get_logger().info('Incoming request')
