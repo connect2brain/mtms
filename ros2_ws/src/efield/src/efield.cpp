bcfc384283c8e6f1442589866cea0b8e46453731
Renan <renan_hiroshi@hotmail.com>
2022-03-14 14:43:58 +0200

    ADD: send mesh to cpp file
    (NB: We are merging two repositories; this commit is from
    invesalius_ros repository.)

--- ros2_ws/src/efield/src/efield.cpp
+++ ros2_ws/src/efield/src/efield.cpp
@@ -3,6 +3,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "neuronavigation_interfaces/msg/pose_using_euler_angles.hpp"
+#include "shape_msgs/msg/mesh.hpp"
 
 using std::placeholders::_1;
 
@@ -11,16 +12,24 @@ class MinimalSubscriber : public rclcpp::Node
   public:
     MinimalSubscriber() : Node("efield")
     {
-      subscription_ = this->create_subscription<neuronavigation_interfaces::msg::PoseUsingEulerAngles>(
-      "neuronavigation/coil_pose", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+      subscription_pose_ = this->create_subscription<neuronavigation_interfaces::msg::PoseUsingEulerAngles>(
+      "neuronavigation/coil_pose", 10, std::bind(&MinimalSubscriber::topic_callback_pose, this, _1));
+       subscription_mesh_ = this->create_subscription<shape_msgs::msg::Mesh>(
+      "neuronavigation/coil_mesh", 10, std::bind(&MinimalSubscriber::topic_callback_mesh, this, _1));
     }
 
   private:
-    void topic_callback(const neuronavigation_interfaces::msg::PoseUsingEulerAngles::SharedPtr msg) const
+    void topic_callback_pose(const neuronavigation_interfaces::msg::PoseUsingEulerAngles::SharedPtr msg) const
     {
       RCLCPP_INFO(this->get_logger(), "X: %f Y: %f Z: %f", msg->position.x, msg->position.y, msg->position.z);
     }
-    rclcpp::Subscription<neuronavigation_interfaces::msg::PoseUsingEulerAngles>::SharedPtr subscription_;
+    rclcpp::Subscription<neuronavigation_interfaces::msg::PoseUsingEulerAngles>::SharedPtr subscription_pose_;
+
+    void topic_callback_mesh(const shape_msgs::msg::Mesh::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "vertices: %f triangles: %f", msg->vertices[0].x, msg->triangles);
+    }
+    rclcpp::Subscription<shape_msgs::msg::Mesh>::SharedPtr subscription_mesh_;
 };
 
 int main(int argc, char * argv[])
