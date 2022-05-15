73378b2e11ac85418759ebcfb3c427b70e6ca41e
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2022-05-16 00:44:32 +0300

    ADD: Event ID to PulseEvent and TriggerOutEvent

--- ros2_ws/src/fpga_bridge/src/fpga_bridge.cpp
+++ ros2_ws/src/fpga_bridge/src/fpga_bridge.cpp
@@ -12,7 +12,7 @@
 #include "fpga_interfaces/msg/pulse_piece.hpp"
 #include "fpga_interfaces/msg/pulse_event.hpp"
 #include "fpga_interfaces/msg/trigger_out_event.hpp"
-#include "fpga_interfaces/msg/timing.hpp"
+#include "fpga_interfaces/msg/event_info.hpp"
 
 #include "fpga_interfaces/msg/safety_monitor_errors.hpp"
 #include "fpga_interfaces/msg/safety_monitor_state.hpp"
@@ -166,15 +166,16 @@ void send_pulse_event(const std::shared_ptr<fpga_interfaces::srv::SendPulseEvent
 
   uint8_t channel = pulse_event.channel;
 
-  /* Serialize timing. */
+  /* Serialize event info. */
 
-  fpga_interfaces::msg::Timing timing = pulse_event.timing;
+  fpga_interfaces::msg::EventInfo event_info = pulse_event.event_info;
 
-  uint8_t wait_for_trigger = timing.wait_for_trigger;
-  uint64_t time_us = timing.time_us;
+  uint16_t event_id = event_info.event_id;
+  uint8_t wait_for_trigger = event_info.wait_for_trigger;
+  uint64_t time_us = event_info.time_us;
 
+  add_uint16_to_serialized_message(event_id);
   add_byte_to_serialized_message(wait_for_trigger);
-
   add_uint64_to_serialized_message(time_us);
 
   /* Serialize pulse. */
