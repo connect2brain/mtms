0a88e2f8639cdb416ed0e614e8939149d57ed6ee
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-05-19 15:43:36 +0300

    ADD: front connection to ros and simple table view for target points

---
+++ front/src/services/ros.ts
@@ -0,0 +1,98 @@
+import ROSLIB from 'roslib';
+import { ROS_URL } from '../utils/constants';
+import { TargetMessage } from '../types/ros';
+
+export const ros = new ROSLIB.Ros({
+  url : ROS_URL
+});
+
+ros.on('connection', () => {
+  console.log('ROS connected to', ROS_URL)
+})
+
+ros.on('error', (error) => {
+  console.log('Error connecting ROS to', ROS_URL, ',error:', error)
+})
+
+ros.on('close', () => {
+  console.log('ROS closed ws connection')
+})
+
+export const listener = new ROSLIB.Topic({
+  ros : ros,
+  name : '/neuronavigation/focus',
+  messageType : 'neuronavigation_interfaces/PoseUsingEulerAngles',
+});
+
+//listener.subscribe(updatePosition);
+
+/* Set up add_target service. */
+export const addTargetClient = new ROSLIB.Service({
+  ros : ros,
+  name : '/planner/add_target',
+  serviceType : 'mtms_interfaces/AddTarget'
+});
+
+/* Set up toggle_select service. */
+export const toggleSelectService = new ROSLIB.Service({
+  ros : ros,
+  name : '/planner/toggle_select',
+  serviceType : 'mtms_interfaces/ToggleSelect'
+});
+
+/* Set up rename_target service. */
+export const renameTargetService = new ROSLIB.Service({
+  ros : ros,
+  name : '/planner/rename_target',
+  serviceType : 'mtms_interfaces/RenameTarget'
+});
+
+/* Set up remove_target service. */
+export const removeTargetService = new ROSLIB.Service({
+  ros : ros,
+  name : '/planner/remove_target',
+  serviceType : 'mtms_interfaces/RemoveTarget'
+});
+
+/* Set up set_target service. */
+export const setTargetService = new ROSLIB.Service({
+  ros : ros,
+  name : '/planner/set_target',
+  serviceType : 'mtms_interfaces/SetTarget'
+});
+
+/* Set up toggle_visible service. */
+export const toggleVisibleService = new ROSLIB.Service({
+  ros : ros,
+  name : '/planner/toggle_visible',
+  serviceType : 'mtms_interfaces/ToggleVisible'
+});
+
+/* Set up change_comment service. */
+export const changeCommentService = new ROSLIB.Service({
+  ros : ros,
+  name : '/planner/change_comment',
+  serviceType : 'mtms_interfaces/ChangeComment'
+});
+
+/* Set up toggle_navigation service.*/
+export const toggleNavigationService = new ROSLIB.Service({
+  ros : ros,
+  name : '/planner/toggle_navigation',
+  serviceType : 'mtms_interfaces/ToggleNavigation'
+});
+
+/* Set up listener for coil at target. */
+export const coilAtTargetListener = new ROSLIB.Topic({
+  ros : ros,
+  name : '/neuronavigation/coil_at_target',
+  messageType : 'std_msgs/Bool',
+});
+//coilAtTargetListener.subscribe(updateCoilAtTarget);
+
+/* Set up listener for planner state. */
+export const stateListener = new ROSLIB.Topic<TargetMessage>({
+  ros : ros,
+  name : '/planner/state',
+  messageType : 'mtms_interfaces/PlannerState',
+});
---
+++ front/src/types/ros.ts
@@ -0,0 +1,34 @@
+import ROSLIB from 'roslib';
+
+export interface EulerAngles {
+  alpha: number;
+  beta: number;
+  gamma: number;
+}
+export interface Position {
+  x: number;
+  y: number;
+  z: number;
+}
+
+export interface PoseUsingEulerAngles {
+  orientation: EulerAngles;
+  position: Position;
+}
+
+export interface Target {
+  name: string;
+  type: string;
+  comment: string;
+  visible: boolean;
+  selected: boolean;
+  target: boolean;
+  pose: PoseUsingEulerAngles;
+
+  intensity: number;
+  iti: number;
+}
+
+export interface TargetMessage extends ROSLIB.Message {
+  targets: Target[]
+}
