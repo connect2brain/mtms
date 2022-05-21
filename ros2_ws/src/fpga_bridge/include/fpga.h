749d0c5434c32a965cd36fe54fc71e441335be8f
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2022-05-21 14:05:39 +0300

    MOD: Extract FPGA handling to its own module in FPGA bridge

---
+++ ros2_ws/src/fpga_bridge/include/fpga.h
@@ -0,0 +1,14 @@
+#ifndef FPGA_H
+#define FPGA_H
+
+#include "NiFpga.h"
+
+bool init_fpga(void);
+bool close_fpga(void);
+bool run_fpga(void);
+
+extern NiFpga_Session session;
+extern NiFpga_Status status;
+extern bool fpga_opened;
+
+#endif
