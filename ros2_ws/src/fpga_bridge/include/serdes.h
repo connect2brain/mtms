b6b1b1627572d5a48b9d5be4f1530f957b487a71
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2022-05-21 14:14:56 +0300

    MOD: Extract serialization to its own module in FPGA bridge

---
+++ ros2_ws/src/fpga_bridge/include/serdes.h
@@ -0,0 +1,18 @@
+#ifndef SERDES_H
+#define SERDES_H
+
+#include <stdint.h>
+
+#define MAX_SERIALIZED_MESSAGE_LENGTH 100
+
+void init_serialized_message();
+void finalize_serialized_message();
+void add_byte_to_serialized_message(uint8_t byte);
+void add_uint16_to_serialized_message(uint16_t value);
+void add_uint32_to_serialized_message(uint32_t value);
+void add_uint64_to_serialized_message(uint64_t value);
+
+extern uint8_t serialized_message[MAX_SERIALIZED_MESSAGE_LENGTH];
+extern uint8_t length;
+
+#endif
--- ros2_ws/src/fpga_bridge/src/stimulation_pulse_event_handler.cpp
+++ ros2_ws/src/fpga_bridge/src/stimulation_pulse_event_handler.cpp
@@ -7,54 +7,7 @@
 
 #include "NiFpga_board_control.h"
 #include "fpga.h"
-
-#define GET_BYTE(var,n) (uint8_t)((var) >> (8 * (n)))
-
-#define MAX_SERIALIZED_MESSAGE_LENGTH 100
-
-uint8_t serialized_message[MAX_SERIALIZED_MESSAGE_LENGTH] = {0};
-uint8_t length = 0;
-
-#define ESCAPE_CHARACTER 0x1B
-#define TRANSPARENCY_MODIFIER 0x20
-#define START_OF_MESSAGE 0xFE
-#define END_OF_MESSAGE 0xFF
-
-void init_serialized_message() {
-  length = 0;
-  serialized_message[length++] = START_OF_MESSAGE;
-}
-
-void finalize_serialized_message() {
-  serialized_message[length++] = END_OF_MESSAGE;
-}
-
-void add_byte_to_serialized_message(uint8_t byte) {
-  if (byte == START_OF_MESSAGE || byte == END_OF_MESSAGE) {
-    serialized_message[length++] = ESCAPE_CHARACTER;
-    serialized_message[length++] = byte^TRANSPARENCY_MODIFIER;
-  } else {
-    serialized_message[length++] = byte;
-  }
-}
-
-void add_uint16_to_serialized_message(uint16_t value) {
-  for (uint8_t i = 0; i < 2; i++) {
-    add_byte_to_serialized_message(GET_BYTE(value, 1 - i));
-  }
-}
-
-void add_uint32_to_serialized_message(uint32_t value) {
-  for (uint8_t i = 0; i < 4; i++) {
-    add_byte_to_serialized_message(GET_BYTE(value, 3 - i));
-  }
-}
-
-void add_uint64_to_serialized_message(uint64_t value) {
-  for (uint8_t i = 0; i < 8; i++) {
-    add_byte_to_serialized_message(GET_BYTE(value, 7 - i));
-  }
-}
+#include "serdes.h"
 
 const uint8_t channel_pulse_fifos[3] = {
   NiFpga_board_control_HostToTargetFifoU8_Channel1PulseFIFO,
