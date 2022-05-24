ffa791c5b72a2efd9f91818701a7016e6b3369f1
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-05-24 12:04:03 +0300

    UPD: rename pulse configuration

---
+++ front/src/views/PulseSequenceConfiguration.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useMemo, useState } from 'react'
+
+const PulseSequenceConfiguration = () => {
+
+    return (
+        <p>hei</p>
+    )
+}
+
+export default PulseSequenceConfiguration
