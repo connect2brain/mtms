15430e1cb15d0010a99ba7834c1fcdfb66f9bdcd
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-05-27 16:24:47 +0300

    ADD: adds isi control

--- front/src/views/PulseSequenceConfiguration.tsx
+++ front/src/views/PulseSequenceConfiguration.tsx
@@ -57,34 +57,81 @@ const PulseSequenceConfiguration = () => {
     console.log('starting sequence:', sequenceData)
   }
 
+  const isiChangeHandler = (value: string, index: number) => {
+    const newIsis = [...isis]
+    newIsis[index] = Number(value)
+    setIsis(newIsis)
+  }
+
+  const nofPulsesInBurstChangeHandler = (value: number) => {
+    const last = isis.length > 0 ? isis[isis.length - 1] : 500
+
+    const nofElementsToAdd = value - isis.length
+
+    let newIsis: number[]
+    if (nofElementsToAdd > 0) {
+      const elementsToAdd = Array(nofElementsToAdd).fill(last)
+      newIsis = isis.concat(...elementsToAdd)
+    } else {
+      newIsis = isis.slice(0, value)
+    }
+
+    setNofPulsesInBursts(value)
+    setIsis(newIsis)
+  }
+
   return (
     <div>
       <label htmlFor='iti'>Inter-train interval (iti) </label>
-      <input name='iti' type='number' value={iti} onChange={(event) => setIti(Number(event.target.value))} />
+      <input name='iti' type='number' value={iti} min={0} onChange={(event) => setIti(Number(event.target.value))} />
       <br />
       <label htmlFor='ibi'>Inter-burst interval (ibi) </label>
-      <input name='ibi' type='number' value={ibi} onChange={(event) => setIbi(Number(event.target.value))} />
+      <input name='ibi' type='number' value={ibi} min={0} onChange={(event) => setIbi(Number(event.target.value))} />
       <br />
+
       <label htmlFor='nofPulsesInBursts'>Number of pulses in bursts </label>
       <input
         name='nofPulsesInBursts'
         type='number'
+        min={0}
         value={nofPulsesInBursts}
-        onChange={(event) => setNofPulsesInBursts(Number(event.target.value))}
+        onChange={(event) => nofPulsesInBurstChangeHandler(Number(event.target.value))}
       />
       <br />
       <label htmlFor='nofBurstsInTrains'>Number of bursts in trains </label>
       <input
         name='nofBurstsInTrains'
         type='number'
+        min={0}
         value={nofBurstsInTrains}
         onChange={(event) => setNofBurstsInTrains(Number(event.target.value))}
       />
       <br />
+      <br />
+
+      {isis.map((isi, index) => {
+        return (
+          <div key={`isi-${index}`}>
+            <label htmlFor={`isi-${index}`}>Interstimulus interval (isi) {index + 1} </label>
+            <input
+              name={`isi-${index}`}
+              type='number'
+              min={0}
+              value={isi}
+              onChange={(event) => isiChangeHandler(event.target.value, index)}
+            />
+            <br />
+          </div>
+        )
+      })}
+
+      <br />
+
       <label htmlFor='nofTrains'>Number of trains </label>
       <input
         name='nofTrains'
         type='number'
+        min={0}
         value={nofTrains}
         onChange={(event) => setNofTrains(Number(event.target.value))}
       />
