4d77b10fc9c7b21fb50481dbc82703699e46843f
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-05-27 14:49:46 +0300

    ADD: channels

--- front/src/providers/state.tsx
+++ front/src/providers/state.tsx
@@ -1,14 +1,50 @@
-import create from 'zustand';
-import { devtools } from 'zustand/middleware';
+import create from 'zustand'
+import { devtools } from 'zustand/middleware'
+import { ChannelInfoWithEnabled } from '../types/pulseSequence'
+
+const initialChannels: ChannelInfoWithEnabled[] = [
+  {
+    channelIndex: 1,
+    enabled: true,
+    voltage: 0,
+  },
+  {
+    channelIndex: 2,
+    enabled: false,
+    voltage: 0,
+  },
+  {
+    channelIndex: 3,
+    enabled: false,
+    voltage: 0,
+  },
+  {
+    channelIndex: 4,
+    enabled: false,
+    voltage: 0,
+  },
+  {
+    channelIndex: 5,
+    enabled: false,
+    voltage: 0,
+  },
+  {
+    channelIndex: 6,
+    enabled: false,
+    voltage: 0,
+  },
+]
 
 type Store = {
-  asd: string;
-  setAsd: (asd: string) => void;
+  channels: ChannelInfoWithEnabled[]
+  setChannels: (channels: ChannelInfoWithEnabled[]) => void
 }
 
 const useStore = create<Store>(
   devtools((set) => ({
-    asd: 'toimii',
-    setAsd: asd => set({ asd })
+    channels: initialChannels,
+    setChannels: (channels) => set((state) => ({ channels })),
   })),
-);
+)
+
+export default useStore
