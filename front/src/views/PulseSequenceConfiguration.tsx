25a531e2c5d7f687061cb2bd641b92b6c576058e
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-05-27 15:21:42 +0300

    ADD: labels and buttons for ibi and iti

--- front/src/views/PulseSequenceConfiguration.tsx
+++ front/src/views/PulseSequenceConfiguration.tsx
@@ -3,21 +3,17 @@ import { ChannelInfoWithEnabled, Experiment, PulseSequence } from '../types/puls
 import styled from 'styled-components'
 import useStore from '../providers/state'
 
-const Channel = ({ channelIndex, enabled, voltage }: ChannelInfoWithEnabled) => {
+const Channel = (props: ChannelInfoWithEnabled) => {
+  const { channelIndex, enabled, voltage } = props
   const { channels, setChannels } = useStore((state) => state)
 
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const target = event.target.name
     const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value
     const newChannels = [...channels]
-    const oldChannel = {
-      channelIndex,
-      enabled,
-      voltage
-    }
     newChannels[channelIndex - 1] = {
-      ...oldChannel,
-      [target]: value
+      ...props,
+      [target]: value,
     }
     setChannels(newChannels)
   }
@@ -25,9 +21,8 @@ const Channel = ({ channelIndex, enabled, voltage }: ChannelInfoWithEnabled) => 
   return (
     <ChannelContainer enabled={enabled}>
       <input name='enabled' type='checkbox' onChange={handleOnChange} />
-      Channel {channelIndex} {' '}
-      <label>Voltage: </label>
-      <input name='voltage' type='number' disabled={!enabled} value={voltage} onChange={handleOnChange}/>
+      Channel {channelIndex} <label htmlFor='voltage'>Voltage: </label>
+      <input name='voltage' type='number' disabled={!enabled} value={voltage} onChange={handleOnChange} />
     </ChannelContainer>
   )
 }
@@ -43,18 +38,40 @@ const PulseSequenceConfiguration = () => {
 
   const [description, setDescription] = useState<string>('')
 
+  const startSequence = (event: any) => {
+    const sequenceData = {}
+
+    console.log('starting sequence:', sequenceData)
+  }
+
   return (
     <div>
-      <form>
-        <ChannelsContainer>
-          {channels.map((channel) => (
-            <Channel
-              key={`channel-${channel.channelIndex}`}
-              {...channel}
-            />
-          ))}
-        </ChannelsContainer>
-      </form>
+      <label htmlFor='iti'>Inter-train interval (iti) {' '}</label>
+      <input name='iti' type='number' value={500}/>
+      <br/>
+      <label htmlFor='ibi'>Inter-burst interval (ibi) {' '}</label>
+      <input name='ibi' type='number' value={500}/>
+      <br/>
+      <label htmlFor='nofPulsesInBursts'>Number of pulses in bursts {' '}</label>
+      <input name='nofPulsesInBursts' type='number' value={3}/>
+      <br/>
+      <label htmlFor='nofBurstsInTrains'>Number of bursts in trains {' '}</label>
+      <input name='nofBurstsInTrains' type='number' value={3}/>
+      <br/>
+      <label htmlFor='nofTrains'>Number of trains {' '}</label>
+      <input name='nofTrains' type='number' value={1}/>
+      <br/>
+      <br/>
+
+      <h2>Channels</h2>
+      <ChannelsContainer>
+        {channels.map((channel) => (
+          <Channel key={`channel-${channel.channelIndex}`} {...channel} />
+        ))}
+      </ChannelsContainer>
+
+      <br/>
+      <button onClick={startSequence}>Start sequence</button>
     </div>
   )
 }
