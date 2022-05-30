63c3ddfe698b0ed4b4778711b024a2fd487e2686
Renan <renan_hiroshi@hotmail.com>
2022-05-30 10:43:59 +0300

    Resolve merge conflict

--- ros2_ws/src/efield/ros_entrypoint.sh
+++ ros2_ws/src/efield/ros_entrypoint.sh
@@ -3,5 +3,6 @@ set -e
 
 source /opt/ros/galactic/setup.bash
 source /app/ros2_ws/install/setup.bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/ros2_ws/src/efield/efield_libraries/lib/
 
 ros2 run efield efield_service
