76cd491c9d0e142a25231774e15a7be9eebc7913
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-05-31 14:36:44 +0300

    UPD: Modifies message type to actual message types instead of dictionaries

--- ros2_ws/src/pulse_sequence_controller/pulse_sequence_controller/start_pulse_sequence.py
+++ ros2_ws/src/pulse_sequence_controller/pulse_sequence_controller/start_pulse_sequence.py
@@ -2,8 +2,8 @@ import rclpy
 from rclpy.node import Node
 
 from mtms_interfaces.srv import StartPulseSequence
-from pulses import generate_pulse
-from time import time_ns
+from mtms_interfaces.msg import StimulationPulseEvent, EventInfo
+from .pulses import generate_pulse
 
 START_DELAY = 100
 
@@ -11,59 +11,71 @@ START_DELAY = 100
 class StartPulseSequenceNode(Node):
     def __init__(self):
         super().__init__('start_pulse_sequence')
-        self.create_service(StartPulseSequence, '/stimulation/start_experiment', self.start_pulse_sequence)
-        self.start_time = time_ns() / 1000
+        self.create_service(StartPulseSequence, '/stimulation/start_experiment', self.start_pulse_sequence_callback)
+        self.start_time = self.get_clock().now().nanoseconds / 1000
 
     def sequence_is_possible(self, sequence):
         return True
 
     # All times are relative to self.start_time, which is the time when the experiment command was received
     def calculate_time_us(self, train_interval, burst_interval, stimulus_interval):
-        return self.start_time + START_DELAY + train_interval + burst_interval + stimulus_interval
+        return int(self.start_time + START_DELAY + train_interval + burst_interval + stimulus_interval)
 
-    def start_pulse_sequence(self, request, response):
+    def send_stimulation_pulse_event(self, event):
+        self.get_logger().info("SENDING: " + str(event))
+
+    def start_pulse_sequence_callback(self, request, response):
         self.get_logger().info('Incoming request')
-        pulse_sequence = request.pulse_sequence
+        self.get_logger().info(str(request))
+        pulse_sequence = request.experiment.pulse_sequence
         if not self.sequence_is_possible(pulse_sequence):
             response.success = False
+            response.status = 'sequence is not possible'
+            response.sequence = []
             return response
 
         event_id = 0
         train_interval = 0
         burst_interval = 0
+
+        sequence = []
+
         for train_index in range(pulse_sequence.nof_trains):
             for burst_index in range(pulse_sequence.nof_bursts_in_trains):
                 for pulse_index in range(pulse_sequence.nof_pulses_in_bursts):
 
-                    # Send command for each enabled channel
+                    # Send event for each enabled channel
                     for channel_info in pulse_sequence.channel_info:
-                        channel_index, voltage = channel_info
+                        channel_index = channel_info.channel_index
+                        voltage = channel_info.voltage
+
                         pulse = generate_pulse(channel_index, voltage)
 
-                        isi = 0 if pulse_index == 0 else pulse_sequence.isis[pulse_sequence]
+                        # isi for the first pulse is 0 as there are no preceding pulses to "wait" for
+                        isi = 0 if pulse_index == 0 else pulse_sequence.isis[pulse_index - 1]
 
-                        event_info = {
-                            'event_id': event_id,
-                            'wait_for_trigger': False,
-                            'time_us': self.calculate_time_us(train_interval, burst_interval, isi),
-                            'delay_us': 0
-                        }
+                        event_info = EventInfo()
+                        event_info.event_id = event_id
+                        event_info.wait_for_trigger = False
+                        event_info.time_us = self.calculate_time_us(train_interval, burst_interval, isi)
+                        event_info.delay_us = 0
 
-                        stimulation_pulse_event = {
-                            'stimulation_pulse_event': {
-                                'pieces': pulse,
-                                'channel': channel_index,
-                                'event_info': event_info
-                            }
-                        }
-                        self.get_logger().info(str(stimulation_pulse_event))
+                        stimulation_pulse_event = StimulationPulseEvent()
+                        stimulation_pulse_event.pieces = pulse
+                        stimulation_pulse_event.channel = channel_index
+                        stimulation_pulse_event.event_info = event_info
+
+                        self.send_stimulation_pulse_event(stimulation_pulse_event)
+                        sequence.append(stimulation_pulse_event)
 
                         event_id += 1
                 burst_interval += pulse_sequence.ibi
 
             train_interval += pulse_sequence.iti
-
+        self.get_logger().info(str(sequence))
         response.success = True
+        response.sequence = sequence
+
         return response
 
 
