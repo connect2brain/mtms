e581cd168aae1bcd8980f62a292ed99b887dee7b
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-05-31 14:36:44 +0300

    UPD: Modifies message type to actual message types instead of dictionaries

--- ros2_ws/src/pulse_sequence_controller/pulse_sequence_controller/pulses.py
+++ ros2_ws/src/pulse_sequence_controller/pulse_sequence_controller/pulses.py
@@ -1,3 +1,5 @@
+from mtms_interfaces.msg import StimulationPulsePiece
+
 fullEvent = {
     'stimulation_pulse_event': {
         'pieces': [
@@ -15,17 +17,34 @@ fullEvent = {
     }
 }
 
-pulse = [
+_pulse = [
     {'mode': 4, 'duration_in_ticks': 10000},
     {'mode': 3, 'duration_in_ticks': 30000},
     {'mode': 2, 'duration_in_ticks': 50000}
 ]
 
 pulses = {}
+TICK_DURATION_IN_US = 25
 
-for index in range(0, 6):
-    pulses[index + 1] = pulse
+
+def pulse_piece_from_dict(d):
+    piece = StimulationPulsePiece()
+    piece.mode = d["mode"]
+    piece.duration_in_ticks = d["duration_in_ticks"]
+    return piece
 
 
 def generate_pulse(channel, voltage):
     return pulses[channel]
+
+
+def pulse_length_in_us(pulse):
+    s = 0
+    for piece in pulse:
+        s += piece.duration_in_ticks * TICK_DURATION_IN_US
+    return s
+
+
+for index in range(0, 6):
+    pieces = list(map(lambda piece: pulse_piece_from_dict(piece), _pulse))
+    pulses[index + 1] = pieces
