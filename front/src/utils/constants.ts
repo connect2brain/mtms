f48d7d24944eec9900987a42973ac36f79888ba6
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-06-17 14:50:09 +0300

    ADD: clearing ros state to test

--- front/src/utils/constants.ts
+++ front/src/utils/constants.ts
@@ -1,1 +1,6 @@
-export const ROS_URL = `ws://${process.env.REACT_APP_ROS_URL ?? 'localhost:9090'}`
+export const ROS_URL =
+  process.env.REACT_APP_ENV === 'dev' || process.env.REACT_APP_ENV === 'prod'
+    ? 'ws://localhost:9090'
+    : 'ws://rosbridge_test:9090'
+
+//`ws://${Cypress.env('ROS_URL') ?? 'localhost:9090'}`
