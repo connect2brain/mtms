48ef1589d832e2dab5a0772c414eb0984d3d5dd3
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-07-04 14:28:17 +0300

    ADD: status_monitor

---
+++ ros2_ws/src/fpga_bridge/src/nodes/status_monitor_bridge.cpp
@@ -0,0 +1,94 @@
+#include <chrono>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "fpga_interfaces/msg/version.hpp"
+
+#include "fpga_interfaces/msg/status_monitor_state.hpp"
+#include "fpga_interfaces/msg/channel_status.hpp"
+
+#include "fpga.h"
+#include "NiFpga_mTMS.h"
+
+#define CHANNEL_COUNT 5
+
+using namespace std::chrono_literals;
+
+NiFpga_mTMS_IndicatorU16 voltage_indicators[CHANNEL_COUNT] = {
+    NiFpga_mTMS_IndicatorU16_Channel1Capacitorvoltage,
+    NiFpga_mTMS_IndicatorU16_Channel2Capacitorvoltage,
+    NiFpga_mTMS_IndicatorU16_Channel3Capacitorvoltage,
+    NiFpga_mTMS_IndicatorU16_Channel4Capacitorvoltage,
+    NiFpga_mTMS_IndicatorU16_Channel5Capacitorvoltage
+};
+
+NiFpga_mTMS_IndicatorU16 temperature_indicators[CHANNEL_COUNT] = {
+    NiFpga_mTMS_IndicatorU16_Channel1Temperature,
+    NiFpga_mTMS_IndicatorU16_Channel2Temperature,
+    NiFpga_mTMS_IndicatorU16_Channel3Temperature,
+    NiFpga_mTMS_IndicatorU16_Channel4Temperature,
+    NiFpga_mTMS_IndicatorU16_Channel5Temperature
+};
+
+class StatusMonitorBridge : public rclcpp::Node
+{
+  public:
+    StatusMonitorBridge()
+    : Node("status_monitor_bridge")
+    {
+      status_monitor_publisher_ = this->create_publisher<fpga_interfaces::msg::StatusMonitorState>("/fpga/status_monitor_state", 10);
+      timer_ = this->create_wall_timer(20ms, std::bind(&StatusMonitorBridge::publish_status_monitor_state, this));
+    }
+
+  private:
+    void publish_status_monitor_state()
+    {
+
+      fpga_interfaces::msg::ChannelStatus channel_statuses[CHANNEL_COUNT];
+
+      for (auto i = 0; i < CHANNEL_COUNT; i++) {
+        fpga_interfaces::msg::ChannelStatus channel_status = fpga_interfaces::msg::ChannelStatus();
+        channel_status.channel_index = i;
+
+        NiFpga_MergeStatus(&status,
+                           NiFpga_ReadU16(
+                               session,
+                               voltage_indicators[i],
+                               &channel_status.voltage
+                           ));
+
+        NiFpga_MergeStatus(&status,
+                           NiFpga_ReadU16(
+                               session,
+                               voltage_indicators[i],
+                               &channel_status.temperature
+                           ));
+
+        channel_statuses[i] = channel_status;
+      }
+
+      fpga_interfaces::msg::StatusMonitorState state = fpga_interfaces::msg::StatusMonitorState();
+      state.channel_statuses = channel_statuses;
+
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<fpga_interfaces::msg::StatusMonitorState>::SharedPtr status_monitor_publisher_;
+};
+
+int main(int argc, char **argv)
+{
+  if (!init_fpga())
+  {
+    return 1;
+  }
+
+  rclcpp::init(argc, argv);
+
+  RCLCPP_INFO(rclcpp::get_logger("status_monitor_bridge"), "Status monitor bridge ready.");
+
+  rclcpp::spin(std::make_shared<StatusMonitorBridge>());
+  rclcpp::shutdown();
+
+  close_fpga();
+}
