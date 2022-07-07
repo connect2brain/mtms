2e74d073a09c03d10e9b63be79db521be7249d25
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-07-07 17:06:49 +0300

    ADD: set_target_orientation node

--- ros2_ws/src/planner/setup.py
+++ ros2_ws/src/planner/setup.py
@@ -47,7 +47,8 @@ setup(
             'toggle_visible_pulse = planner.toggle_visible_pulse:main',
             'change_pulse_index = planner.change_pulse_index:main',
             'master_state = planner.master_state:main',
-            'add_pulse_to_pulse_sequence = planner.add_pulse_to_pulse_sequence:main'
+            'add_pulse_to_pulse_sequence = planner.add_pulse_to_pulse_sequence:main',
+            'set_target_orientation = planner.set_target_orientation:main'
         ],
     },
 )
