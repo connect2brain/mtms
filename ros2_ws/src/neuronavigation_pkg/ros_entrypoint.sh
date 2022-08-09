b5fcd2d49fba3bf66dec8d3014c1de3165c2ed0b
sotodela <ana.sotodelacruz@gmail.com>
2022-08-09 13:42:15 +0300

    Add missing packages and pypolaris wheel

--- ros2_ws/src/neuronavigation_pkg/ros_entrypoint.sh
+++ ros2_ws/src/neuronavigation_pkg/ros_entrypoint.sh
@@ -3,5 +3,10 @@ set -e
 
 source /opt/ros/galactic/setup.bash
 source /app/ros2_ws/install/setup.bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/python3.8/dist-packages/pypolaris
+
 
 ros2 run neuronavigation_pkg start
+# Multithreading
+export MKL_NUM_THREADS=16
+export OMP_NUM_THREADS=16
