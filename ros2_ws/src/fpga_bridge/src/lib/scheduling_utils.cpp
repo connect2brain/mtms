d337abab4e3067138c33f5a5cd75a6712444b7e1
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-08-23 11:22:05 +0300

    ADD: scheduling utils to fpga bridge

---
+++ ros2_ws/src/fpga_bridge/src/lib/scheduling_utils.cpp
@@ -0,0 +1,17 @@
+//
+// Created by alqio on 23.8.2022.
+// Adapted from https://github.com/ros-realtime/ros2-realtime-examples/blob/rolling/minimal_scheduling
+// Apache 2.0 License. No major changes
+//
+
+#include "scheduling_utils.h"
+
+void set_thread_scheduling(std::thread::native_handle_type thread, int policy, int sched_priority) {
+  struct sched_param param;
+  param.sched_priority = sched_priority;
+  auto ret = pthread_setschedparam(thread, policy, &param);
+  if (ret > 0) {
+    throw std::runtime_error(
+        "Couldn't set scheduling priority and policy. Error code: " + std::string(strerror(errno)));
+  }
+}
--- ros2_ws/src/fpga_bridge/src/nodes/trigger_out_event_handler.cpp
+++ ros2_ws/src/fpga_bridge/src/nodes/trigger_out_event_handler.cpp
@@ -8,6 +8,7 @@
 #include "fpga.h"
 #include "serdes.h"
 #include "memory_utils.h"
+#include "scheduling_utils.h"
 
 const NiFpga_mTMS_HostToTargetFifoU8 trigger_out_fifo = NiFpga_mTMS_HostToTargetFifoU8_HosttoTargetSignalOutFIFO;
 
@@ -84,9 +85,9 @@ int main(int argc, char **argv) {
   RCLCPP_INFO(rclcpp::get_logger("trigger_out_event_handler"), "Trigger out event handler ready.");
 
 #ifdef ON_UNIX
-  //set_default_thread_stacksize(1024 * 50); //50 MB, default in unix is 8 MB
   lock_memory();
   preallocate_memory(1024 * 1024 * 10); //10 MB
+  set_thread_scheduling(pthread_self(), DEFAULT_SCHEDULING_POLICY, DEFAULT_SCHEDULING_PRIORITY);
 #endif
 
   rclcpp::spin(node);
