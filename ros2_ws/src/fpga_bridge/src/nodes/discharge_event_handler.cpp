141b0b15b765004b337aff48216a2e1a67f105ae
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-08-23 11:22:05 +0300

    ADD: scheduling utils to fpga bridge

--- ros2_ws/src/fpga_bridge/src/nodes/discharge_event_handler.cpp
+++ ros2_ws/src/fpga_bridge/src/nodes/discharge_event_handler.cpp
@@ -8,6 +8,7 @@
 #include "fpga.h"
 #include "serdes.h"
 #include "memory_utils.h"
+#include "scheduling_utils.h"
 
 const NiFpga_mTMS_HostToTargetFifoU8 discharge_fifo = NiFpga_mTMS_HostToTargetFifoU8_HosttoTargetDischargeFIFO;
 
@@ -86,9 +87,9 @@ int main(int argc, char **argv) {
   RCLCPP_INFO(rclcpp::get_logger("discharge_event_handler"), "Discharge event handler ready.");
 
 #ifdef ON_UNIX
-  //set_default_thread_stacksize(1024 * 1); //1 MB, default in unix is 8 MB
   lock_memory();
   preallocate_memory(1024 * 1024 * 10); //10 MB
+  set_thread_scheduling(pthread_self(), DEFAULT_SCHEDULING_POLICY, DEFAULT_SCHEDULING_PRIORITY);
 #endif
 
   rclcpp::spin(node);
