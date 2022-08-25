1fbcee11e4007a8102bcf87dc52ef7b138b4f753
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-08-25 16:19:37 +0300

    ADD: memory optimization check

--- ros2_ws/src/fpga_bridge/src/nodes/discharge_event_handler.cpp
+++ ros2_ws/src/fpga_bridge/src/nodes/discharge_event_handler.cpp
@@ -82,7 +82,7 @@ int main(int argc, char **argv) {
   auto node = std::make_shared<DischargeEventHandler>();
   RCLCPP_INFO(rclcpp::get_logger("discharge_event_handler"), "Discharge event handler ready.");
 
-#ifdef ON_UNIX
+#if defined(ON_UNIX) && defined(MEMORY_OPTIMIZATION)
   lock_memory();
   preallocate_memory(1024 * 1024 * 10); //10 MB
   set_thread_scheduling(pthread_self(), DEFAULT_SCHEDULING_POLICY, DEFAULT_SCHEDULING_PRIORITY);
