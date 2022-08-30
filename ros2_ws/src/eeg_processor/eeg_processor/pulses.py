1b8ba6a3698dffbb6965d588738c3ed60079b611
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-08-30 11:44:07 +0300

    UPD: eeg processor eeg data wip

---
+++ ros2_ws/src/eeg_processor/eeg_processor/pulses.py
@@ -0,0 +1,69 @@
+from fpga_interfaces.msg import EventInfo, StimulationPulsePiece, StimulationPulseEvent, ChargeEvent
+
+US_TO_TICKS_CONVERSION_RATIO = 40
+
+# Channel modes and times in order
+mode_list = [
+    [0, 2, 1],
+    [0, 2, 1],
+    [0, 2, 1],
+    [0, 2, 1],
+    [0, 2, 1]
+]
+
+time_list = [
+    [60, 30, 37.0],
+    [60, 30, 37.0],
+    [60, 30, 39.1],
+    [60, 30, 39.1],
+    [60, 30, 44.4]
+]
+
+# Generate standard pulse command, values in us
+
+
+def generate_standard_pulse_command():
+    assert len(mode_list) == len(time_list)
+
+    events = []
+    for i, _ in enumerate(time_list):
+        pieces = []
+        for j, _ in enumerate(time_list[i]):
+            piece = StimulationPulsePiece()
+            piece.mode = mode_list[i][j]
+            piece.duration_in_ticks = int(time_list[i][j] * US_TO_TICKS_CONVERSION_RATIO)
+            pieces.append(piece)
+
+        event_info = EventInfo()
+        event_info.event_id = i
+        event_info.execution_condition = 2
+        event_info.time_us = 0
+        event_info.delay_us = 0
+
+        event = StimulationPulseEvent()
+        event.event_info = event_info
+        event.channel = i + 1
+        event.pieces = pieces
+
+        events.append(event)
+
+    return events
+
+
+def generate_standard_charge_command(voltage):
+    events = []
+    for i, _ in enumerate(time_list):
+        event_info = EventInfo()
+        event_info.event_id = i
+        event_info.execution_condition = 2
+        event_info.time_us = 0
+        event_info.delay_us = 0
+
+        event = ChargeEvent()
+        event.channel = i + 1
+        event.target_voltage = voltage
+        event.event_info = event_info
+
+        events.append(event)
+
+    return events
