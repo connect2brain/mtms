ee3c82757b11d5aa9153230679dc5107b70b37cc
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-02 16:26:37 +0300

    ADD: python processor wip

--- ros2_ws/src/processors/python_processor.py
+++ ros2_ws/src/processors/python_processor.py
@@ -1,3 +1,9 @@
+class StimulationEvent:
+    def __init__(self, channel, pieces, event_info) -> None:
+        self.channel = channel
+        self.pieces = pieces
+        self.event_info = event_info
+
 class Processor:
     def __init__(self) -> None:
         self.data = []
@@ -7,6 +13,31 @@ class Processor:
     def init(self):
         print("in python init")
 
-    def data_received(self, data):
-        self.counter = data
-        print(f"in data received, counter: {self.counter}")
+    def data_received(self, data, time_us, first_sample_of_experiment):
+        self.counter += 1
+        print(f"counter: {self.counter}, data: {data}, time_us: {time_us}, first sample of experiment: {first_sample_of_experiment}")
+
+        channel = 1
+        pieces = [
+                {
+                    "mode": 1,
+                    "duration_in_ticks": 500
+                },
+                {
+                    "mode": 1,
+                    "duration_in_ticks": 500
+                },
+                {
+                    "mode": 1,
+                    "duration_in_ticks": 500
+                }
+            ]
+        event_info = {
+                "event_id": 1,
+                "execution_condition": 3,
+                "time_us": 10392930
+            }
+
+        event = StimulationEvent(channel, pieces, event_info)
+
+        return [event]
