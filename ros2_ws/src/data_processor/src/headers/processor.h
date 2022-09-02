d7c70b28f1e6e3b35576236a2e5288033f889e7f
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-02 16:26:37 +0300

    ADD: python processor wip

--- ros2_ws/src/data_processor/src/headers/processor.h
+++ ros2_ws/src/data_processor/src/headers/processor.h
@@ -6,12 +6,16 @@
 #define DATA_PROCESSOR_PROCESSOR_H
 
 #include "string"
+#include "mtms_interfaces/msg/eeg_datapoint.hpp"
+#include "fpga_interfaces/msg/stimulation_pulse_event.hpp"
+#include "fpga_interfaces/msg/stimulation_pulse_piece.hpp"
 
 class ProcessorWrapper {
 public:
   virtual void init() = 0;
 
-  virtual void data_received(int data) = 0;
+  virtual std::vector<fpga_interfaces::msg::StimulationPulseEvent>
+  data_received(mtms_interfaces::msg::EegDatapoint data) = 0;
 
   virtual int close() = 0;
 
