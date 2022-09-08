8983e0615b2da57d2fa0c0fa3aea4d8252975bc2
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-08 15:44:01 +0300

    ADD: matlab compiled to cpp processor

--- ros2_ws/src/data_processor/src/matlab_processor.cpp
+++ ros2_ws/src/data_processor/src/matlab_processor.cpp
@@ -5,19 +5,22 @@
 #include "headers/matlab_processor.h"
 
 MatlabProcessor::MatlabProcessor(std::string script_path) {
+  /*
   matlab = matlab::engine::startMATLAB();
+  matlab::engine::connectMATLAB();
   std::cout << "MATLAB started" << std::endl;
 
   std::string path = "sometext" + script_path + "sometext";
   std::cout << path << std::endl;
   matlab->eval(u"addpath '/home/alqio/workspace/mtms/ros2_ws/src/processors'");
   matlab->eval(u"processor = Processor");
+  //matlab->eval(u"processor.new_datapoint(")
 
+  //matlab::data::Array a = matlab->feval(u"gcd");
   processor_instance = matlab->getVariable(u"processor");
 
   auto add_simple = matlab->getProperty(processor_instance, u"add_simple");
   auto v = add_simple[0];
-  std::cout << v << std::endl;
 
   matlab::data::ArrayFactory factory;
   std::vector<matlab::data::Array> args({factory.createScalar<int32_t>(30)});
@@ -26,7 +29,7 @@ MatlabProcessor::MatlabProcessor(std::string script_path) {
   std::cout << "cresult: " << cresult << std::endl;
   //auto v = cresult[0];
   //std::cout << "cresult: " << v << std::endl;
-
+*/
 }
 
 void MatlabProcessor::init() {
