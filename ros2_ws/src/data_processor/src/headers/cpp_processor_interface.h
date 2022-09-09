228bb0c09bc7d8d20ee7bf05f292271aaf58ed93
alqio <kyosti.alkio@gmail.com>
2022-09-09 15:50:24 +0300

    UPD: fixes matlab codegen processor

--- ros2_ws/src/data_processor/src/headers/cpp_processor_interface.h
+++ ros2_ws/src/data_processor/src/headers/cpp_processor_interface.h
@@ -6,20 +6,25 @@
 #define DATA_PROCESSOR_CPP_PROCESSOR_INTERFACE_H
 
 #include "coder_array.h"
+#include "run_processor_types.h"
 #include <iostream>
-
 
 class MatlabProcessorInterface {
 public:
-  MatlabProcessorInterface *init(unsigned int b_window_size,
-                                 unsigned short channel_count);
-  void data_received(const double channel_data[62]);
+  virtual MatlabProcessorInterface *init(unsigned int b_window_size,
+                                         unsigned short channel_count);
+
+  virtual void data_received(const double channel_data[62],
+                             coder::array<stimulation_pulse_event, 1U> &ret);
+
   coder::array<double, 2U> data;
   unsigned int window_size;
+  coder::array<stimulation_pulse_event, 1U> pulses;
 };
 
 
-typedef MatlabProcessorInterface *create_processor();
+typedef MatlabProcessorInterface *create_processor(unsigned int b_window_size,
+                                                   unsigned short channel_count);
 
 typedef void destroy(MatlabProcessorInterface *);
 
