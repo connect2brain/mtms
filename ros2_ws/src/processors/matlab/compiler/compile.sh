280d1e14527fec2240c9905d7c9db20c948c4838
alqio <kyosti.alkio@gmail.com>
2022-09-09 15:35:23 +0300

    UPD: matlab processor wip 2

--- ros2_ws/src/processors/matlab/compiler/compile.sh
+++ ros2_ws/src/processors/matlab/compiler/compile.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+find . ! (-name 'MatlabProcessorInterface.h' -o -name 'MatlabProcessorInterface.cpp') -type f \( -iname \*.h -o -iname \*.cpp \)  -exec cat {} +
+
+ rm -f {} +
 
 cp ../codegen/lib/run_processor/MatlabProcessorInterface.cpp .
 cp ../codegen/lib/run_processor/MatlabProcessorInterface.h .
+cp ../codegen/lib/run_processor/*.h .
+cp ../codegen/lib/run_processor/*.cpp .
 
 PROCESSOR_H=MatlabProcessorInterface.h
 sed -i -e 's/MatlabProcessorInterface \*init/virtual MatlabProcessorInterface \*init/' $PROCESSOR_H
 sed -i -e 's/void/virtual void/' $PROCESSOR_H
 
-#cmake .
-#make all
+cmake .
+make all
