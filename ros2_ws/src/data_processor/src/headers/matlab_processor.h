cf0bb55501d3fd403af0649c04e791ca89ceb9cd
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-13 17:20:32 +0300

    UPD: wip processors/matlab/MatlabProcessorScript.m

--- ros2_ws/src/data_processor/src/headers/matlab_processor.h
+++ ros2_ws/src/data_processor/src/headers/matlab_processor.h
@@ -9,6 +9,13 @@
 #include "iostream"
 #include "MatlabDataArray.hpp"
 #include "MatlabEngine.hpp"
+#include "run_processor_types.h"
+
+struct MatlabProcessorReturn {
+  matlab::data::TypedArray<double> data;
+  //matlab::data::TypedArray<matlab_fpga_event> events;
+  std::vector<matlab_fpga_event> events;
+};
 
 class MatlabProcessor : public ProcessorWrapper {
 public:
@@ -23,6 +30,7 @@ public:
 private:
   std::unique_ptr<matlab::engine::MATLABEngine> matlab;
   matlab::data::Array processor_instance;
+  std::vector<std::vector<double>> matlab_data;
 };
 
 
--- ros2_ws/src/data_processor/src/matlab_processor.cpp
+++ ros2_ws/src/data_processor/src/matlab_processor.cpp
@@ -4,26 +4,17 @@
 
 #include "headers/matlab_processor.h"
 
+
 MatlabProcessor::MatlabProcessor(const std::string &script_path) {
-  std::cout << "Staring matlab" << std::endl;
+  std::cout << "Starting matlab" << std::endl;
 
   matlab = matlab::engine::startMATLAB();
   matlab::engine::connectMATLAB();
   std::cout << "MATLAB started" << std::endl;
 
-  std::string path = "sometext" + script_path + "sometext";
+  auto path = "addpath " + script_path;
   std::cout << path << std::endl;
-  matlab->eval(u"addpath '/home/alqio/workspace/mtms/ros2_ws/src/processors/matlab'");
-
-
-  matlab::data::ArrayFactory factory;
-  std::vector<matlab::data::Array> args({
-                                            factory.createScalar<int16_t>(30)
-                                        });
-
-  matlab::data::TypedArray<int16_t> result = matlab->feval(u"simple_script", args);
-
-  std::cout << "Result: " << result[0] << std::endl;
+  matlab->eval(matlab::engine::convertUTF8StringToUTF16String(path));
 }
 
 void MatlabProcessor::init() {
@@ -31,6 +22,62 @@ void MatlabProcessor::init() {
 
 
 std::vector<FpgaEvent> MatlabProcessor::data_received(mtms_interfaces::msg::EegDatapoint data) {
+  matlab::data::ArrayFactory factory;
+
+  auto matlab_data_array = factory.createArray<double>({50, 62});
+  auto dim = matlab::data::ArrayDimensions(data.channel_datapoint.size());
+  auto matlab_new_sample = factory.createArray<double>(
+      {1, data.channel_datapoint.size()},
+      data.channel_datapoint.data(),
+      data.channel_datapoint.data() + data.channel_datapoint.size()
+  );
+
+
+  std::vector<matlab::data::Array> args({
+                                            matlab_data_array,
+                                            matlab_new_sample
+                                        });
+
+  std::vector<matlab::data::Array> results = matlab->feval(u"MatlabProcessorScript", 2, args);
+  std::cout << "feval done" << std::endl;
+
+  auto new_data = results[0];
+  auto new_data_typed = (matlab::data::TypedArray<double>) new_data;
+
+  for (auto i: new_data_typed) {
+    //std::cout << i << std::endl;
+  }
+
+  auto events = results[1];
+  auto events_typed = (matlab::data::StructArray) events;
+
+  auto fields = events_typed.getFieldNames();
+  std::vector<matlab::data::MATLABFieldIdentifier> field_names(fields.begin(), fields.end());
+
+  for (unsigned event_index = 0; event_index < events_typed.getNumberOfElements(); event_index++) {
+    std::cout << "event index " << event_index << std::endl;
+    for (auto field: field_names) {
+      auto field_name = field.operator std::string();
+      std::cout << field_name << ": " << std::endl;
+      ///std::cout << field_value[0] << std::endl;
+
+      if (field_name == "channel") {
+        matlab::data::TypedArrayRef<uint8_t> field_value = events_typed[event_index][field];
+        std::cout << +field_value[0] << std::endl;
+      } else if (field_name == "event_type") {
+        matlab::data::StringArray field_value = events_typed[event_index][field];
+        std::cout << "here" << std::endl;
+        auto str = field_value[0].operator std::string();
+        std::cout << "casted to str" << std::endl;
+        std::cout << str << std::endl;
+      } else {
+        std::cout << std::endl;
+      }
+    }
+  }
+
+  std::vector<FpgaEvent> ret;
+  return ret;
 }
 
 int MatlabProcessor::close() {
