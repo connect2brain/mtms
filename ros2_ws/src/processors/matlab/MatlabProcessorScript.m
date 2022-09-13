096c94c2b2cbe88c227ccbec2aba8b171efa125f
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-13 17:20:32 +0300

    UPD: wip processors/matlab/MatlabProcessorScript.m

--- ros2_ws/src/processors/matlab/MatlabProcessorScript.m
+++ ros2_ws/src/processors/matlab/MatlabProcessorScript.m
@@ -1,16 +1,28 @@
 function [updated_data, events] = MatlabProcessorScript(data, new_data_point)
-    if length(data) < 50
-        data(end + 1) = new_data_point;
+    fprintf("data length %d\n", length(data));
+
+    rows = 50;
+    columns = 62;
+
+    % disp(data);
+    disp(size(data));
+    disp(size(new_data_point));
+    disp(size(data(end,:)));
+
+    if length(data) < rows
+        data(end + 1, :) = new_data_point;
     else
-        for i=1:length(data) - 1
-            data(i) = data(i + 1);
+        for i=1:rows - 1
+            data(i, :) = data(i + 1, :);
         end
-        data(end) = new_data_point;
+        data(end, :) = new_data_point;
     end
     
     
     event = create_command("pulse_event", 0);
-
+    disp(event);
     updated_data = data;
-    events = [event];
+    events = (event);
+    disp(events);
+    disp(data);
 end
