4ee575c6be6015b921cc06941ca4469f1577ed9a
alqio <kyosti.alkio@gmail.com>
2022-09-13 13:07:20 +0300

    UPD: renames spike -> peak

--- ros2_ws/src/processors/python/python_processor.py
+++ ros2_ws/src/processors/python/python_processor.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class StimulationEvent:
     def __init__(self, channel, pieces, event_info, event_type) -> None:
         self.channel = channel
@@ -39,10 +42,7 @@ event_info = {
 pulse_event = StimulationEvent(channel, pieces, event_info, event_type)
 charge_event = ChargeEvent(channel, 1200, event_info, "charge")
 
-THRESHOLD = 50  # TODO
 SAMPLING_RATE = 5000
-
-import numpy as np
 
 
 class real_time_peak_detection():
@@ -57,10 +57,8 @@ class real_time_peak_detection():
         self.filteredY = np.array(self.y).tolist()
         self.avgFilter = [0] * len(self.y)
         self.stdFilter = [0] * len(self.y)
-        print(f"avgFilter: {self.avgFilter}")
         self.avgFilter[self.lag - 1] = np.mean(self.y[0:self.lag]).tolist()
         self.stdFilter[self.lag - 1] = np.std(self.y[0:self.lag]).tolist()
-        print(f"avgFilter: {self.avgFilter}")
 
     def thresholding_algo(self, new_value):
         self.y.append(new_value)
@@ -106,48 +104,64 @@ class Processor:
         self.eeg_data_index = 0
         self.data = []
         self.file = None
-        self.spike_at = 0
-        self.peak_detection = real_time_peak_detection([0] * 50, 50, 5.5, 0)
-        self.spike_over = False
+        self.peak_at = 0
+        self.peak_detection = real_time_peak_detection([0] * 50, 50, 4, 0.5)
+        self.peak_over = False
+        self.peaks_detected = 0
 
     def init(self):
         print("in python init")
-        self.file = open('eeg_data.csv', 'w')
-        self.file.write('c3,filtered,spike\n')
+        self.file = open('./data/eeg_python.csv', 'w')
+        self.file.write('c3,filtered,peak\n')
 
     def close(self):
         if self.file:
             self.file.close()
             print("Closed file")
+        print(f"Peaks detected: {self.peaks_detected}")
 
     def laplacian(self, c3, others):
         neg = [(-1) * x for x in others]
         return 4 * c3 + sum(neg)
 
+    def average(self, c3, others):
+        combined = [c3] + others
+        return sum(combined) / len(combined)
+
     def data_received(self, data, time_us, first_sample_of_experiment):
         self.eeg_data_index += 1
-        # print("in python data received")
 
         c3 = data[4]
-        others = [data[20], data[22], data[24], data[26]]  # 5 21 23 25 27
-        filtered = 1  # self.laplacian(c3, others)
+        others = [data[20], data[22], data[24], data[26]]
+        filtered = self.average(c3, others)
+
+        self.data.append(filtered)
+        if len(self.data) > 20:
+            self.data.pop(0)
+
+        filtered = sum(self.data) / len(self.data)
+
         signal = self.peak_detection.thresholding_algo(c3)
-        if signal == 0 and not self.spike_over:
-            self.spike_over = True
+        if signal == 0 and not self.peak_over:
+            self.peak_over = True
 
-        spike = signal == 1
-        spike_mark = 'f'
+        peak = signal != 0
+        if peak:
+            print("Peak detected")
+
+        peak_mark = 'f'
         send_pulse = False
-        if spike and self.spike_over:
-            self.spike_over = False
-            # print(f"Last spike at {self.spike_at}, new spike at {self.eeg_data_index}")
-            self.spike_at = self.eeg_data_index
-            spike_mark = 't'
+        if peak and self.peak_over:
+            self.peak_over = False
+            # print(f"Last peak at {self.peak_at}, new peak at {self.eeg_data_index}")
+            self.peak_at = self.eeg_data_index
+            peak_mark = 't'
             send_pulse = True
+            self.peaks_detected += 1
 
-        #spike_mark = 't' if spike else 'f'
+        peak_mark = 't' if peak else 'f'
         if self.file:
-            self.file.write(f"{c3},{filtered},{spike_mark}\n")
+            self.file.write(f"{c3},{filtered},{peak_mark}\n")
 
         if send_pulse:
             return [pulse_event]
