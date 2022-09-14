0452cbc9bdec28f593543829444efb3512172474
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-14 13:55:47 +0300

    UPD: removes prints and adds logging

--- ros2_ws/src/processors/matlab/MatlabProcessorScript.m
+++ ros2_ws/src/processors/matlab/MatlabProcessorScript.m
@@ -1,13 +1,6 @@
 function [updated_data, events] = MatlabProcessorScript(data, new_data_point)
-    fprintf("data length %d\n", length(data));
-
     rows = 50;
     columns = 62;
-
-    % disp(data);
-    disp(size(data));
-    disp(size(new_data_point));
-    disp(size(data(end,:)));
 
     if length(data) < rows
         data(end + 1, :) = new_data_point;
@@ -17,12 +10,26 @@ function [updated_data, events] = MatlabProcessorScript(data, new_data_point)
         end
         data(end, :) = new_data_point;
     end
+   
+    c3_col = data(:,5);
+    c3 = c3_col(end);
+
+    [signals,avgFilter,stdFilter] = ThresholdingAlgo(c3_col,30, 5.5, 0.7);
     
-    
-    event = create_command("pulse_event", 500);
-    % disp(event);
+    peak = signals(end);
+    peak_mark = "f";
+    if peak ~= 0
+        peak_mark = "t";
+    end
+    %file_id = fopen("eeg_purematlab.csv", "a+");
+    % fprintf(obj.file_id, "c3,filtered,peak\n");
+    %fprintf(file_id, "%6.2f,%f,%s\n", c3, 1, peak_mark);
+    %fclose(file_id);
+
+
+    pulse = create_command("pulse_event", 500);
+    charge = create_command("charge_event", 500);
     updated_data = data;
-    events = (event);
-    % disp(events);
-    disp(data(:,end));
+    events = [pulse, charge];
+
 end
