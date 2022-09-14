7b84658d6c9704850b0df6144b9710fd7222a256
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-14 12:10:57 +0300

    ADD: event parsing from matlab and data argument

--- ros2_ws/src/processors/matlab/create_command.m
+++ ros2_ws/src/processors/matlab/create_command.m
@@ -1,14 +1,14 @@
 function command = create_command(event_type, target_voltage)
-    event_info.event_id = uint16(0);
-    event_info.execution_condition = uint8(0);
-    event_info.time_us = uint64(0);
+    event_info.event_id = uint16(1);
+    event_info.execution_condition = uint8(2);
+    event_info.time_us = uint64(69999);
     
     piece1.mode = uint8(0);
     piece1.duration_in_ticks = uint16(200);
     piece2.mode = uint8(2);
-    piece2.duration_in_ticks = uint16(160);
+    piece2.duration_in_ticks = uint16(269);
     piece3.mode = uint8(1);
-    piece3.duration_in_ticks = uint16(160);
+    piece3.duration_in_ticks = uint16(1);
     
     pieces = [piece1, piece2, piece3];
     
