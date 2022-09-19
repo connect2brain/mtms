d484b5d97a6c9768508808f5f3eb155f2499163e
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-19 15:40:34 +0300

    ADD: adds fpga_event print

--- ros2_ws/src/data_processor/src/headers/fpga_event.h
+++ ros2_ws/src/data_processor/src/headers/fpga_event.h
@@ -9,6 +9,7 @@
 #include "fpga_interfaces/msg/charge_event.hpp"
 #include "fpga_interfaces/msg/discharge_event.hpp"
 #include "fpga_interfaces/msg/stimulation_pulse_piece.hpp"
+#include <iostream>
 
 enum FpgaEventType {
   STIMULATION_PULSE_EVENT = 0,
@@ -40,6 +41,43 @@ struct FpgaEvent {
       return "Charge event";
     } else {
       return "Discharge event";
+    }
+  }
+
+  void print() {
+    std::cout << "Event type: ";
+    if (event_type == STIMULATION_PULSE_EVENT) {
+      std::cout << "Stimulation pulse event" << std::endl;
+      std::cout << "Channel: " << +stimulation_pulse_event.channel << std::endl;
+      std::cout << "Event info: " << std::endl;
+      std::cout << "  Event id: " << stimulation_pulse_event.event_info.event_id << std::endl;
+      std::cout << "  Execution condition: " << +stimulation_pulse_event.event_info.execution_condition << std::endl;
+      std::cout << "  Time us: " << stimulation_pulse_event.event_info.time_us << std::endl;
+      std::cout << "Pieces: " << std::endl;
+      for (auto i = 0; i < 3; i++) {
+        std::cout << "  mode: " << +stimulation_pulse_event.pieces[i].mode << ", duration in ticks: "
+                  << stimulation_pulse_event.pieces[i].duration_in_ticks
+                  << std::endl;
+      }
+    } else if (event_type == CHARGE_EVENT) {
+      std::cout << "Charge event" << std::endl;
+      std::cout << "Channel: " << +charge_event.channel << std::endl;
+      std::cout << "Target voltage: " << charge_event.target_voltage << std::endl;
+      std::cout << "Event info: " << std::endl;
+      std::cout << "  Event id: " << charge_event.event_info.event_id << std::endl;
+      std::cout << "  Execution condition: " << +charge_event.event_info.execution_condition << std::endl;
+      std::cout << "  Time us: " << charge_event.event_info.time_us << std::endl;
+    } else if (event_type == DISCHARGE_EVENT) {
+      std::cout << "Discharge event" << std::endl;
+
+      std::cout << "Channel: " << +discharge_event.channel << std::endl;
+      std::cout << "Target voltage: " << discharge_event.target_voltage << std::endl;
+      std::cout << "Event info: " << std::endl;
+      std::cout << "  Event id: " << discharge_event.event_info.event_id << std::endl;
+      std::cout << "  Execution condition: " << +discharge_event.event_info.execution_condition << std::endl;
+      std::cout << "  Time us: " << discharge_event.event_info.time_us << std::endl;
+    } else {
+      std::cout << "Uknown event type " << +event_type << std::endl;
     }
   }
 
