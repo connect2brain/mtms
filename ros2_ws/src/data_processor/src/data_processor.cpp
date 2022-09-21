315a2afc9cc2f971313c8965da703164c4102c57
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-21 09:45:33 +0300

    ADD: eeg processor functionality to data processor

--- ros2_ws/src/data_processor/src/data_processor.cpp
+++ ros2_ws/src/data_processor/src/data_processor.cpp
@@ -10,7 +10,6 @@ using namespace std::chrono_literals;
 using namespace std::chrono;
 
 DataProcessor::DataProcessor() : Node("data_processor") {
-  RCLCPP_INFO(this->get_logger(), "in data_processor constructor");
   std::string processor_type;
   this->declare_parameter<std::string>("processor_type", "python");
   this->get_parameter("processor_type", processor_type);
@@ -23,88 +22,39 @@ DataProcessor::DataProcessor() : Node("data_processor") {
   this->declare_parameter<int>("loop_count", 5);
   this->get_parameter("loop_count", loop_count);
 
-  RCLCPP_INFO(rclcpp::get_logger("data_processor"), "processor type: %s", processor_type.c_str());
-  if (processor_type == "python") {
-    processor = new PythonProcessor(processor_script_path);
-  } else if (processor_type == "matlab") {
-    processor = new MatlabProcessor(processor_script_path);
-  } else if (processor_type == "cpp") {
-    processor = new CompiledMatlabProcessor(processor_script_path);
-  }
+  std::string filename;
+  this->declare_parameter<std::string>("file", "data.txt");
+  this->get_parameter("file", filename);
 
-  auto subscription_callback = [this](const std::shared_ptr<mtms_interfaces::msg::EegDatapoint> message) -> void {
-    auto start = steady_clock::now();
-
-    auto fpga_events = processor->data_received(*message);
-
-    auto stop = steady_clock::now();
-    auto total = duration_cast<microseconds>(stop - start);
-    //RCLCPP_INFO(this->get_logger(), "Duration: %lu us", total.count());
-    f << std::to_string(total.count()) << "\n";
-
+  auto data_subscription_callback = [this](const std::shared_ptr<mtms_interfaces::msg::EegDatapoint> message) -> void {
   };
 
-  mtms_interfaces::msg::EegDatapoint message = mtms_interfaces::msg::EegDatapoint();
-  for (auto i = 0; i < 62; i++) {
-    message.channel_datapoint.push_back(fRand(0, 100));
-  }
-  processor->init();
-
+  auto trigger_subscription_callback = [this](const std::shared_ptr<mtms_interfaces::msg::Trigger> message) -> void {
+    if (message->index == 1) {
+      trigger_out_client->async_send_request(req);
+    } else if (message->index == 2) {
+      RCLCPP_INFO(this->get_logger(), "Difference between triggers: %lu", message->time_us);
+      f << std::to_string(message->time_us) << "\n";
+    } else {
+      RCLCPP_WARN(rclcpp::get_logger("data_processor"), "Unknown message index %d", message->index);
+    }
+  };
   eeg_data_subscription = this->create_subscription<mtms_interfaces::msg::EegDatapoint>("/eeg/raw_data",
                                                                                         10,
-                                                                                        subscription_callback);
+                                                                                        data_subscription_callback);
 
-  f.open("data.txt", std::ios::out | std::ios::trunc);
+  trigger_subscription = this->create_subscription<mtms_interfaces::msg::Trigger>("/eeg/trigger_received",
+                                                                                  10,
+                                                                                  trigger_subscription_callback);
 
-  if (loop_count > 0) {
-    measure(loop_count);
-  }
+  trigger_out_client = this->create_client<fpga_interfaces::srv::SendTriggerOutEvent>("/fpga/send_trigger_out_event");
 
+  req = std::make_shared<fpga_interfaces::srv::SendTriggerOutEvent::Request>();
+
+  f.open(filename, std::ios::out | std::ios::trunc);
+  
 }
 
-
-void DataProcessor::measure(int repeats) {
-  std::vector<mtms_interfaces::msg::EegDatapoint> events;
-  for (auto j = 0; j < repeats; j++) {
-    mtms_interfaces::msg::EegDatapoint message = mtms_interfaces::msg::EegDatapoint();
-    for (auto i = 0; i < 62; i++) {
-      message.channel_datapoint.push_back(fRand(0, 100));
-    }
-    events.push_back(message);
-  }
-  std::vector<std::chrono::microseconds> times;
-  std::chrono::microseconds total = std::chrono::microseconds(0s);
-  for (auto i = 0; i < repeats; i++) {
-    auto start = high_resolution_clock::now();
-
-    auto fpga_events = processor->data_received(events[i]);
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    times.push_back(duration);
-    total = duration_cast<microseconds>(duration + total);
-
-    for (auto event : fpga_events) {
-      event.print();
-      std::cout << "---" << std::endl;
-    }
-
-  }
-  RCLCPP_INFO(this->get_logger(), "Duration total: %lu us", total.count());
-  RCLCPP_INFO(this->get_logger(), "Average execution time: %f us", ((double) total.count()) / repeats);
-}
-
-int DataProcessor::shutdown() {
-  RCLCPP_INFO(rclcpp::get_logger("data_processor"), "Shutting down data processor");
-  if (processor) {
-    processor->close();
-    //TODO: parse events
-    return 0;
-  }
-  if (f) {
-    f.close();
-  }
-  return 0;
-}
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
@@ -116,7 +66,7 @@ int main(int argc, char *argv[]) {
 
 
   rclcpp::spin(node);
-  node->shutdown();
+  //node->shutdown();
   rclcpp::shutdown();
   return 0;
 }
