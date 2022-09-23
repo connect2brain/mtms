4a710573eca4ac73b72aec3fac2d153f9c96b2c5
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-23 14:10:13 +0300

    ADD: adds back data processor

--- ros2_ws/src/data_processor/src/data_processor.cpp
+++ ros2_ws/src/data_processor/src/data_processor.cpp
@@ -11,6 +11,7 @@ using namespace std::chrono_literals;
 using namespace std::chrono;
 
 DataProcessor::DataProcessor() : Node("data_processor") {
+  RCLCPP_INFO(this->get_logger(), "in data_processor constructor");
   std::string processor_type;
   this->declare_parameter<std::string>("processor_type", "python");
   this->get_parameter("processor_type", processor_type);
@@ -23,91 +24,78 @@ DataProcessor::DataProcessor() : Node("data_processor") {
   this->declare_parameter<int>("loop_count", 5);
   this->get_parameter("loop_count", loop_count);
 
-  std::string filename;
-  this->declare_parameter<std::string>("file", "data.txt");
-  this->get_parameter("file", filename);
+  RCLCPP_INFO(rclcpp::get_logger("data_processor"), "processor type: %s", processor_type.c_str());
+  if (processor_type == "python") {
+    processor = new PythonProcessor(processor_script_path);
+  } else if (processor_type == "matlab") {
+    processor = new MatlabProcessor(processor_script_path);
+  } else if (processor_type == "cpp") {
+    processor = new CompiledMatlabProcessor(processor_script_path);
+  }
 
-  //auto data_subscription_callback = [this](const std::shared_ptr<mtms_interfaces::msg::EegDatapoint> message) -> void {
-  //};
+  auto subscription_callback = [this](const std::shared_ptr<mtms_interfaces::msg::EegDatapoint> message) -> void {
+    auto start = steady_clock::now();
 
-  auto trigger_subscription_callback = [this](const std::shared_ptr<mtms_interfaces::msg::Trigger> message) -> void {
-    //RCLCPP_INFO(this->get_logger(), "Trigger received: %d", message->index);
-    //RCLCPP_WARN(this->get_logger(), "Trigger received: %d", message->index);
-    if (message->index == 1) {
-      trigger_out_client->async_send_request(req);
-    } else if (message->index == 2) {
+    auto fpga_events = processor->data_received(*message);
 
-      if (index < durations.size()) {
-        durations[index++] = message->time_us;
-      } else {
-        index++;
-      }
+    auto stop = steady_clock::now();
+    auto total = duration_cast<microseconds>(stop - start);
+    //RCLCPP_INFO(this->get_logger(), "Duration: %lu us", total.count());
+    f << std::to_string(total.count()) << "\n";
 
-      //RCLCPP_INFO(this->get_logger(), "index: %d", index);
-      if (index % 1000 == 0) {
-        RCLCPP_INFO(this->get_logger(), "Index: %d", index);
-      } 
-      if (index == durations.size()) {
-        RCLCPP_INFO(this->get_logger(), "Writing durations...");
-        for (uint32_t i = 0; i < index; i++) {
-          f << durations[i] << std::endl;
-        }
-        RCLCPP_INFO(this->get_logger(), "Finished");
-        f.flush();
-        f.close();
-      }
+  };
 
-      //f << std::to_string(message->time_us) << std::endl;
-      //f.flush();
+  mtms_interfaces::msg::EegDatapoint message = mtms_interfaces::msg::EegDatapoint();
+  for (auto i = 0; i < 62; i++) {
+    message.channel_datapoint.push_back(fRand(0, 100));
+  }
+  processor->init();
 
-      //RCLCPP_INFO(this->get_logger(), "Difference between triggers: %lu", message->time_us);
-    } else {
-      RCLCPP_WARN(rclcpp::get_logger("data_processor"), "Unknown message index %d", message->index);
+  eeg_data_subscription = this->create_subscription<mtms_interfaces::msg::EegDatapoint>("/eeg/raw_data",
+                                                                                        10,
+                                                                                        subscription_callback);
+
+  f.open("data.txt", std::ios::out | std::ios::trunc);
+
+  if (loop_count > 0) {
+    measure(loop_count);
+  }
+
+}
+
+void DataProcessor::measure(int repeats) {
+  std::vector<mtms_interfaces::msg::EegDatapoint> events;
+  for (auto j = 0; j < repeats; j++) {
+    mtms_interfaces::msg::EegDatapoint message = mtms_interfaces::msg::EegDatapoint();
+    for (auto i = 0; i < 62; i++) {
+      message.channel_datapoint.push_back(fRand(0, 100));
     }
-  };
+    events.push_back(message);
+  }
+  std::vector<std::chrono::microseconds> times;
+  std::chrono::microseconds total = std::chrono::microseconds(0s);
+  for (auto i = 0; i < repeats; i++) {
+    auto start = high_resolution_clock::now();
 
-  static const rmw_qos_profile_t qos_profile = {
-      RMW_QOS_POLICY_HISTORY_KEEP_LAST,
-      1,
-      RMW_QOS_POLICY_RELIABILITY_RELIABLE,
-      RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL,
-      RMW_QOS_DEADLINE_DEFAULT,
-      RMW_QOS_LIFESPAN_DEFAULT,
-      RMW_QOS_POLICY_LIVELINESS_SYSTEM_DEFAULT,
-      RMW_QOS_LIVELINESS_LEASE_DURATION_DEFAULT,
-      false
-  };
+    auto fpga_events = processor->data_received(events[i]);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    times.push_back(duration);
+    total = duration_cast<microseconds>(duration + total);
 
-  auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history,qos_profile.depth),qos_profile);
+    for (auto event: fpga_events) {
+      event.print();
+      std::cout << "---" << std::endl;
+    }
 
-
-  trigger_subscription = this->create_subscription<mtms_interfaces::msg::Trigger>("/eeg/trigger_received",
-                                                                                  qos,
-                                                                                  trigger_subscription_callback);
-
-  trigger_out_client = this->create_client<fpga_interfaces::srv::SendTriggerOutEvent>("/fpga/send_trigger_out_event", qos_profile);
-
-  req = std::make_shared<fpga_interfaces::srv::SendTriggerOutEvent::Request>();
-  auto event = fpga_interfaces::msg::TriggerOutEvent();
-  event.index = 3;
-  event.duration_us = 10000;
-  event.event_info.time_us = 0;
-  event.event_info.execution_condition = 2;
-  event.event_info.event_id = 1;
-
-  req->trigger_out_event = event;
-
-  f.open(filename, std::ios::out | std::ios::trunc);
-
-  durations = std::vector<double>(100000, 0);
-  RCLCPP_INFO(this->get_logger(), "durations size: %lu", durations.size());
-
+  }
+  RCLCPP_INFO(this->get_logger(), "Duration total: %lu us", total.count());
+  RCLCPP_INFO(this->get_logger(), "Average execution time: %f us", ((double) total.count()) / repeats);
 }
 
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-
 
 #if defined(ON_UNIX) && defined(SCHEDULING_OPTIMIZATION)
   RCLCPP_INFO(rclcpp::get_logger("data_processor"), "Setting thread scheduling and memory lock");
