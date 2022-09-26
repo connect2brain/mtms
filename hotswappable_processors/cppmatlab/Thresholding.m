19f139c0b3e37b97c1b3a3fef2e90a350ef5bfac
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-26 15:30:53 +0300

    ADD: adds references to matlab processor thresholding

--- hotswappable_processors/cppmatlab/Thresholding.m
+++ hotswappable_processors/cppmatlab/Thresholding.m
@@ -1,3 +1,5 @@
+% Adapated from Brakel, J.P.G. van (2014). "Robust peak detection algorithm using z-scores". Stack Overflow. Available at: https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data/22640362#22640362 (version: 2020-11-08).
+
 classdef Thresholding < handle
     %THERSHOLDING Summary of this class goes here
     %   Detailed explanation goes here
