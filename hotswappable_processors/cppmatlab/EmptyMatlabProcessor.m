37989a97d83a3597941163bfcea4e13d28e5aecf
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-09-28 16:56:23 +0300

    ADD: phastimate init

---
+++ hotswappable_processors/cppmatlab/EmptyMatlabProcessor.m
@@ -0,0 +1,20 @@
+classdef MatlabProcessor < AbstractMatlabProcessor
+
+    methods
+        function obj = constructor(obj)
+            obj.set_channel_count(63);
+            obj.set_window_size(50);
+            obj.set_auto_enqueue(true);
+        end
+        function on_init_experiment(obj)
+            obj.commands = [];
+        end
+        function on_data_received(obj, channel_data, time_us, first_sample_of_experiment)
+            obj.set_commands([]);
+        end
+        function on_end_experiment(obj)
+            obj.commands = [];
+        end
+    end
+end
+
