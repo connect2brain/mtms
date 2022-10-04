80f53e866efd46c590038e62b1d433368ab87ef8
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-04 16:14:10 +0300

    ADD: eeg batch message

--- front/src/types/eeg.ts
+++ front/src/types/eeg.ts
@@ -11,6 +11,12 @@ export interface EegDatapointMessage extends ROSLIB.Message {
   time: number
   first_sample_of_experiment: boolean
 }
+export interface EegBatch extends ROSLIB.Message {
+  batch: EegDatapoint[]
+}
+export interface EegBatchMessage extends ROSLIB.Message {
+  batch: EegDatapoint[]
+}
 
 export interface EegTrigger {
   index: number
