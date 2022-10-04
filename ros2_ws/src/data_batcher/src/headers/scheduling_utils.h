05ac42cb6e809e1f4e2f0580031ff6ec783e8501
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-04 16:13:28 +0300

    ADD: data batcher

---
+++ ros2_ws/src/data_batcher/src/headers/scheduling_utils.h
@@ -0,0 +1,27 @@
+//
+// Created by alqio on 5.9.2022.
+//
+
+#ifndef DATA_PROCESSOR_SCHEDULING_UTILS_H
+#define DATA_PROCESSOR_SCHEDULING_UTILS_H
+
+#include <pthread.h>
+#include <sys/types.h>
+#include <sched.h>
+#include <string>
+#include <memory>
+#include <thread>
+#include <cstring>
+
+#ifdef __unix__
+#define ON_UNIX
+#endif
+
+#define SCHEDULING_OPTIMIZATION
+
+#define DEFAULT_SCHEDULING_POLICY SCHED_RR
+#define DEFAULT_SCHEDULING_PRIORITY 98
+
+void set_thread_scheduling(std::thread::native_handle_type thread, int policy, int sched_priority);
+
+#endif //DATA_PROCESSOR_SCHEDULING_UTILS_H
