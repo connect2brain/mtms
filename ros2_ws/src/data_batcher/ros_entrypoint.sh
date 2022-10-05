0dd7151767506ebe7d4f7a8f3a7516bb5c01f9e3
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-05 11:25:06 +0300

    ADD: downsample ratio argument to data batcher

--- ros2_ws/src/data_batcher/ros_entrypoint.sh
+++ ros2_ws/src/data_batcher/ros_entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 source /opt/ros/galactic/setup.bash
 source /app/ros2_ws/install/setup.bash
 
-ros2 launch data_batcher data_batcher.launch.py log-level:="$ROS_LOG_LEVEL" batch-size:="$DATA_BATCHER_BATCH_SIZE"
+ros2 launch data_batcher data_batcher.launch.py log-level:="$ROS_LOG_LEVEL" batch-size:="$DATA_BATCHER_BATCH_SIZE" downsample-ratio:="$DATA_BATCHER_DOWNSAMPLE_RATIO"
