23cb2eab04147a06f54467026584ef9f93b9edae
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-06 08:37:30 +0300

    ADD: trigger data publish

--- front/src/components/EegChartStreaming.tsx
+++ front/src/components/EegChartStreaming.tsx
@@ -7,16 +7,17 @@ import {
   Title,
   Tooltip,
   Legend,
+  BarElement,
   ChartOptions,
   ChartData,
 } from 'chart.js'
 import 'chartjs-adapter-date-fns'
 import StreamingPlugin from 'chartjs-plugin-streaming'
 
-import { Line } from 'react-chartjs-2'
+import { Line, Chart } from 'react-chartjs-2'
 import React, { useEffect, useRef, useState } from 'react'
 
-const options: ChartOptions<'line'> = {
+const options: ChartOptions<'line' | 'bar'> = {
   responsive: true,
   animation: {
     duration: 0,
@@ -33,66 +34,116 @@ const options: ChartOptions<'line'> = {
   scales: {
     y: {
       type: 'linear',
-      min: 3020,
-      max: 3220,
+      min: 3550,
+      max: 3620,
     },
     x: {
       type: 'realtime' as const,
-
+      realtime: {
+        duration: 3000,
+      },
       // Change options only for THIS AXIS
     },
   },
 }
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, StreamingPlugin)
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  StreamingPlugin,
+)
 
-type Datapoint = {
+export type Datapoint = {
   x: number
   y: number
 }
 type EegChartProps = {
-  data: Datapoint[]
+  eegData: Datapoint[]
+  triggerData: Datapoint
 }
 
-export const EegChartSteaming = ({ data }: EegChartProps) => {
+export const EegChartSteaming = ({ eegData, triggerData }: EegChartProps) => {
   const chartRef = useRef<any>(null)
 
-  const [chartData, setChartData] = useState<ChartData<'line'>>({
+  const [chartData, setChartData] = useState<ChartData<'line' | 'bar'>>({
     datasets: [
       {
         label: 'Eeg data',
-        data: [],
+        data: eegData,
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
         pointRadius: 0,
+      },
+      {
+        type: 'bar',
+        label: 'Triggers',
+        data: [triggerData],
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
       },
     ],
   })
 
   useEffect(() => {
-    const oldData = chartData.datasets[0].data
-
-    const mappedData = data.map((point) => {
-      const timeInSeconds = point.x / 1000000
-      return {
-        y: point.y,
-        x: Math.round(timeInSeconds), //new Date(point.x),
-      }
-    })
+    const eegOldData = chartData.datasets[0].data
+    const triggerOldData = chartData.datasets[1].data
 
     const newChartData = {
       datasets: [
         {
+          type: 'line' as const,
           label: 'Eeg data',
-          data: oldData.concat(mappedData),
+          data: eegOldData.concat(eegData),
           borderColor: 'rgb(255, 99, 132)',
           backgroundColor: 'rgba(255, 99, 132, 0.5)',
           pointRadius: 0,
         },
+        {
+          type: 'bar' as const,
+          label: 'Triggers',
+          data: triggerOldData, //triggerOldData.concat(triggerData),
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
       ],
     }
+
     setChartData(newChartData)
     chartRef.current?.update('quiet')
-  }, [data])
+  }, [eegData])
 
-  return <Line id={'eeg-chart'} ref={chartRef} data={chartData} options={options} />
+  useEffect(() => {
+    const eegOldData = chartData.datasets[0].data
+    const triggerOldData = chartData.datasets[1].data
+    console.log('in here')
+    const newChartData = {
+      datasets: [
+        {
+          type: 'line' as const,
+          label: 'Eeg data',
+          data: eegOldData,
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          pointRadius: 0,
+        },
+        {
+          type: 'bar' as const,
+          label: 'Triggers',
+          data: triggerOldData.concat(triggerData),
+          borderColor: 'rgb(255, 99, 132)',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+      ],
+    }
+
+    //setChartData(newChartData)
+    chartRef.current?.update('quiet')
+  }, [triggerData])
+
+  return <Chart type={'line'} id={'eeg-chart'} ref={chartRef} data={chartData} options={options} />
 }
--- front/src/views/DataVisualize.tsx
+++ front/src/views/DataVisualize.tsx
@@ -1,43 +1,54 @@
 import React, { useEffect, useState } from 'react'
-import { eegDataSubscriber } from 'services/ros'
-import { EegBatch, EegBatchMessage, EegDatapoint, EegDatapointMessage } from 'types/eeg'
+import { eegDataSubscriber, triggerSubscriber } from 'services/ros'
+import { EegBatch, EegBatchMessage, EegDatapoint, EegDatapointMessage, EegTriggerMessage } from 'types/eeg'
 import { useAppDispatch, useAppSelector } from 'providers/reduxHooks'
-import { addBatch, addEegDatapoint, setEeg } from 'reducers/eegReducer'
+import { addBatch, addEegDatapoint, addEegTrigger, setEeg } from 'reducers/eegReducer'
 import { EegChart } from 'components/EegChart'
-import { EegChartSteaming } from '../components/EegChartStreaming'
+import { Datapoint, EegChartSteaming } from '../components/EegChartStreaming'
 
 const DataVisualize = () => {
   const { eeg } = useAppSelector((state) => state.eegData)
 
-  const [latestBatch, setLatestBatch] = useState<EegDatapoint[]>([])
+  const [latestBatch, setLatestBatch] = useState<Datapoint[]>([])
+
+  const [trigger, setTrigger] = useState<Datapoint>({
+    y: 1,
+    x: 1,
+  })
 
   const dispatch = useAppDispatch()
 
   useEffect(() => {
     console.log('Subscribing to eeg data')
     eegDataSubscriber.subscribe(newEegBatch)
+    triggerSubscriber.subscribe(newTrigger)
   }, [])
 
+  const c3 = (datapoint: number[]) =>
+    datapoint[4] - 0.25 * (datapoint[21] + datapoint[23] + datapoint[25] + datapoint[27])
+
   const newEegBatch = (message: EegBatchMessage) => {
-    const datapoints: EegDatapoint[] = message.batch
-    const newData = eeg.concat(datapoints)
-    //dispatch(addBatch(newData))
-    setLatestBatch(message.batch)
+    const mappedData = message.batch.map((point) => {
+      const timeInSeconds = point.time / 1000000
+      return {
+        y: c3(point.channel_datapoint),
+        x: Math.round(timeInSeconds),
+      }
+    })
+    setLatestBatch(mappedData)
   }
 
-  const c3 = () => {
-    return latestBatch.map((datapoint) => {
-      return {
-        y: datapoint.channel_datapoint[4],
-        x: datapoint.time,
-      }
+  const newTrigger = (message: EegTriggerMessage) => {
+    console.log(message)
+    setTrigger({
+      y: 100000,
+      x: message.time_us,
     })
   }
 
   return (
     <div>
-      Eeg length: {eeg.length}
-      <EegChartSteaming data={c3()} />
+      <EegChartSteaming eegData={latestBatch} triggerData={trigger} />
     </div>
   )
 }
