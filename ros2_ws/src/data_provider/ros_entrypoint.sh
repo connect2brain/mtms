3c8a05dc2fb8268373d0c7c318003a9cb7b94aa9
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-07 14:24:37 +0300

    UPD: renames data_provider --> eeg_simulator

--- ros2_ws/src/data_provider/ros_entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-source /opt/ros/galactic/setup.bash
-source /app/ros2_ws/install/setup.bash
-
-ros2 launch data_provider data_provider.launch.py log-level:="$ROS_LOG_LEVEL" data-file:="$DATA_PROVIDER_DATA_FILE_NAME" sampling-frequency:="$DATA_PROVIDER_SAMPLING_FREQUENCY"
