e4aa3522b21a25305ac6513808925c7f921f5680
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2022-10-23 22:52:07 +0300

    API: CLP: Rename bridge to node in MTMSApi, misnomer

--- mtms_api/MTMSApi.py
+++ mtms_api/MTMSApi.py
@@ -12,27 +12,27 @@ from .MTMSApiNode import MTMSApiNode
 class MTMSApi:
     def __init__(self):
         rclpy.init(args=None)
-        self.bridge = MTMSApiNode()
+        self.node = MTMSApiNode()
 
     # Start and stop
 
     def start_device(self):
-        self.bridge.start_device()
+        self.node.start_device()
         while self.get_device_state() != DeviceState.OPERATIONAL:
             pass
 
     def stop_device(self):
-        self.bridge.stop_device()
+        self.node.stop_device()
         while self.get_device_state() != DeviceState.NOT_OPERATIONAL:
             pass
 
     def start_experiment(self):
-        self.bridge.start_experiment()
+        self.node.start_experiment()
         while self.get_experiment_state() != ExperimentState.STARTED:
             pass
 
     def stop_experiment(self):
-        self.bridge.stop_experiment()
+        self.node.stop_experiment()
         while self.get_experiment_state() != ExperimentState.STOPPED:
             pass
 
@@ -40,50 +40,50 @@ class MTMSApi:
 
     def wait_forever(self):
         while True:
-            self.bridge.wait_for_new_state()
+            self.node.wait_for_new_state()
 
     def wait_until(self, time):
-        self.bridge.wait_for_new_state()
+        self.node.wait_for_new_state()
         while self.get_time() < time:
-            self.bridge.wait_for_new_state()
+            self.node.wait_for_new_state()
 
     def wait(self, time):
         start_time = self.get_wallclock_time()
 
-        self.bridge.wait_for_new_state()
+        self.node.wait_for_new_state()
         while self.get_wallclock_time() < start_time + time:
-            self.bridge.wait_for_new_state()
+            self.node.wait_for_new_state()
 
     # Getters
 
     def get_device_state(self):
-        self.bridge.wait_for_new_state()
-        return DeviceState(self.bridge.system_state.device_state)
+        self.node.wait_for_new_state()
+        return DeviceState(self.node.system_state.device_state)
 
     def get_experiment_state(self):
-        self.bridge.wait_for_new_state()
-        return ExperimentState(self.bridge.system_state.experiment_state)
+        self.node.wait_for_new_state()
+        return ExperimentState(self.node.system_state.experiment_state)
 
     def get_voltage(self, channel):
-        self.bridge.wait_for_new_state()
-        return self.bridge.system_state.channel_states[channel - 1].voltage
+        self.node.wait_for_new_state()
+        return self.node.system_state.channel_states[channel - 1].voltage
 
     def get_temperature(self, channel):
-        self.bridge.wait_for_new_state()
-        return self.bridge.system_state.channel_states[channel - 1].temperature
+        self.node.wait_for_new_state()
+        return self.node.system_state.channel_states[channel - 1].temperature
 
     def get_pulse_count(self, channel):
-        self.bridge.wait_for_new_state()
-        return self.bridge.system_state.channel_states[channel - 1].pulse_count
+        self.node.wait_for_new_state()
+        return self.node.system_state.channel_states[channel - 1].pulse_count
 
     def get_time(self):
-        self.bridge.wait_for_new_state()
-        return self.bridge.system_state.time
+        self.node.wait_for_new_state()
+        return self.node.system_state.time
 
     # Events
     def send_pulse(self, id, channel, waveform, execution_condition=ExecutionCondition.TIMED, time=0):
         time_us = int(time * 10**6)
-        self.bridge.send_pulse(
+        self.node.send_pulse(
             id=id,
             execution_condition=execution_condition,
             time_us=time_us,
@@ -93,7 +93,7 @@ class MTMSApi:
 
     def send_charge(self, id, channel, target_voltage, execution_condition=ExecutionCondition.TIMED, time=0):
         time_us = int(time * 10**6)
-        self.bridge.send_charge(
+        self.node.send_charge(
             id=id,
             execution_condition=execution_condition,
             time_us=time_us,
@@ -103,7 +103,7 @@ class MTMSApi:
 
     def send_discharge(self, id, channel, target_voltage, execution_condition=ExecutionCondition.TIMED, time=0):
         time_us = int(time * 10**6)
-        self.bridge.send_discharge(
+        self.node.send_discharge(
             id=id,
             execution_condition=execution_condition,
             time_us=time_us,
@@ -113,7 +113,7 @@ class MTMSApi:
 
     def send_signal_out(self, id, port, duration_us, execution_condition=ExecutionCondition.TIMED, time=0):
         time_us = int(time * 10**6)
-        self.bridge.send_signal_out(
+        self.node.send_signal_out(
             id=id,
             execution_condition=execution_condition,
             time_us=time_us,
@@ -122,7 +122,7 @@ class MTMSApi:
         )
 
     def send_event_trigger(self):
-        self.bridge.send_event_trigger()
+        self.node.send_event_trigger()
 
         # HACK: This should probably be a feedback message that is received from the FPGA, informing that the
         #   event trigger was successfully generated, similar to the feedback messages from the actual events.
@@ -132,16 +132,16 @@ class MTMSApi:
     # Testing and debugging (undocumented)
 
     def start_device_without_waiting(self):
-        self.bridge.start_device()
+        self.node.start_device()
 
     def stop_device_without_waiting(self):
-        self.bridge.stop_device()
+        self.node.stop_device()
 
     def start_experiment_without_waiting(self):
-        self.bridge.start_experiment()
+        self.node.start_experiment()
 
     def stop_experiment_without_waiting(self):
-        self.bridge.stop_experiment()
+        self.node.stop_experiment()
 
     def wait_until_not_operational(self):
         while self.get_device_state() != DeviceState.NOT_OPERATIONAL:
@@ -181,7 +181,7 @@ class MTMSApi:
 
     # Targeting
     def get_channel_voltages(self, displacement_x, displacement_y, rotation_angle, intensity):
-        return self.bridge.get_channel_voltages(
+        return self.node.get_channel_voltages(
             displacement_x=displacement_x,
             displacement_y=displacement_y,
             rotation_angle=rotation_angle,
@@ -191,11 +191,11 @@ class MTMSApi:
     # Other
 
     def print_system_state(self):
-        self.bridge.wait_for_new_state()
+        self.node.wait_for_new_state()
 
     def get_wallclock_time(self):
         return time.time()
 
     def end(self):
-        self.bridge.destroy_node()
+        self.node.destroy_node()
         rclpy.shutdown()
