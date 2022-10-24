b85c1ec7e0c8bf844f935a48545e5d8feb9b8eed
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-24 15:43:49 +0300

    UPD: updates phastimate to work with isi

---
+++ hotswappable_processors/cppmatlab/get_default_waveform.m
@@ -0,0 +1,19 @@
+function waveform = get_default_waveform(channel)
+    if channel == 1 || channel == 2
+        falling_phase_duration_in_ticks = 1480;
+    elseif channel == 3 || channel == 4
+        falling_phase_duration_in_ticks = 1564;
+    elseif channel == 5
+        falling_phase_duration_in_ticks = 1776;
+    end
+
+    piece1.waveform_phase = uint8(0);
+    piece1.duration_in_ticks = uint16(2400);
+    piece2.waveform_phase = uint8(1);
+    piece2.duration_in_ticks = uint16(1200);
+    piece3.waveform_phase = uint8(2);
+    piece3.duration_in_ticks = uint16(falling_phase_duration_in_ticks);
+
+    waveform = [piece1, piece2, piece3];
+
+end
