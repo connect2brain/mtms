e8486657c857401b5aa5ba750f4c8729510e9140
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-24 13:00:17 +0300

    ADD: webgl plot

---
+++ front/src/components/WebGLPlot.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { WebglPlot, WebglLine, ColorRGBA } from 'webgl-plot'
+import { EegChartProps } from './EegChartStreaming'
+import styled from 'styled-components'
+
+export const WebGLPlot = ({ eegData, triggerData }: EegChartProps) => {
+  const canvasMain = useRef<HTMLCanvasElement>(null)
+
+  const [webGLPlot, setWebGLPlot] = useState<WebglPlot>()
+  const [eegLine, setEegLine] = useState<WebglLine>()
+  const [triggerLine, setTriggerLine] = useState<WebglLine>()
+
+  useEffect(() => {
+    if (canvasMain.current) {
+      const devicePixelRatio = window.devicePixelRatio || 1
+      canvasMain.current.width = canvasMain.current.clientWidth * devicePixelRatio
+      canvasMain.current.height = canvasMain.current.clientHeight * devicePixelRatio
+
+      const webglp = new WebglPlot(canvasMain.current)
+      setWebGLPlot(webglp)
+      const numX = 5000
+
+      const line1 = new WebglLine(new ColorRGBA(1, 0, 0, 1), numX)
+      webglp.addLine(line1)
+
+
+      line1.arrangeX()
+
+      setWebGLPlot(webglp)
+      setEegLine(line1)
+    }
+  }, [])
+
+  useEffect(() => {
+    //console.log('in webgl use effect eeg data')
+    let id = 0
+    let renderPlot = () => {
+      //console.log('in render plot')
+      const y = new Float32Array(eegData.map(datapoint => datapoint.y))
+      //console.log(y)
+      eegLine?.shiftAdd(y)
+      //id = requestAnimationFrame(renderPlot)
+      webGLPlot?.update()
+    }
+
+    id = requestAnimationFrame(renderPlot)
+
+    return () => {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      renderPlot = () => {}
+      cancelAnimationFrame(id)
+    }
+
+  }, [eegData])
+
+  return (
+    <div>
+      <StyledCanvas ref={canvasMain} />
+    </div>
+  )
+}
+
+const StyledCanvas = styled.canvas`
+  width: 100%;
+  height: 70vh;
+  border: 1px solid black;
+`
