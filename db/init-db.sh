2c16c01d6c30d907e68ec6c3072a1ec3da98bcb5
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2022-10-25 18:46:00 +0300

    DEL: Legacy code, move remaining to legacy directory

--- db/init-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")"
-rm -f topics.db
-sqlite3 topics.db ".read topics.sql"
-chmod a-w topics.db
--- py/integration_tests/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-./wait-for-it.sh "$BACKEND_HOST:$BACKEND_PORT" -t 60
-
-poetry run pytest
