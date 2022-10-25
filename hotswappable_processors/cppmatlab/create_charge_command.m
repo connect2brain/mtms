890eefa78ad6c1125bf380fe0b8896e295e3b60f
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-25 10:49:53 +0300

    UPD: fixes enqueue to use much faster circshift, separates create commands to their functions

---
+++ hotswappable_processors/cppmatlab/create_charge_command.m
@@ -0,0 +1,15 @@
+function command = create_charge_command(event_id, channel, execution_condition, time_us, target_voltage)
+    event.event_id = uint16(event_id);
+    event.execution_condition = uint8(execution_condition);
+    event.time_us = uint64(time_us);
+
+    command.event = event;
+    command.pieces = get_default_waveform(channel);
+    command.event_type = uint8(1);
+    
+    command.target_voltage = uint16(target_voltage);
+    
+    coder.cstructname(command, 'matlab_fpga_event');
+    coder.cstructname(command.event, 'event');
+    coder.cstructname(command.pieces, 'pulse_piece');
+end
