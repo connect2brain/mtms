fefa2c8974dd7602185d26ed0f40c0755fc75e88
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-25 17:02:20 +0300

    ADD: multiple views

---
+++ front/src/styles/StyledTypography.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const SmallHeader = styled.h2`
+  color: ${(p) => p.theme.colors.primary};
+  padding: 0.5rem;
+`
+export const Header = styled.h1`
+  color: ${(p) => p.theme.colors.primary};
+  padding-left: 0.5rem;
+`
