54272c08c3e36a6b587e73869b96edf6a6d015bd
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-25 16:28:30 +0300

    UPD: improves navbar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import Providers from './providers/Providers'
 import styled from 'styled-components'
@@ -10,17 +10,30 @@ import { ExperimentControl } from './components/ExperimentControl'
 import { SystemState } from './components/SystemState'
 import { Navbar } from './components/Navbar'
 
-function App() {
+const App = () => {
+
+  const [displaySystemControl, setDisplaySystemControl] = useState(true)
+  const [displayPlot, setDisplayPlot] = useState(false)
+  const [displayTargets, setDisplayTargets] = useState(false)
+  const [displayExperiment, setDisplayExperiment] = useState(false)
+
+
   return (
     <Providers>
       <Header>mTMS control panel</Header>
 
+      <SmallHeader>System control</SmallHeader>
       <Wrapper>
         <ExperimentControl />
         <SystemState />
       </Wrapper>
       <hr />
+
+      <SmallHeader>Select view</SmallHeader>
       <Navbar />
+      <hr />
+
+
       <Wrapper>
         <Routes>
           <Route path='/' element={<Targets />} />
@@ -38,6 +51,13 @@ const Wrapper = styled.div`
 
 const Header = styled.h1`
   color: ${(p) => p.theme.colors.primary};
+  padding-left: 0.5rem;
+`
+
+const SmallHeader = styled.h2`
+  color: ${(p) => p.theme.colors.primary};
+  padding: 0.5rem;
+
 `
 
 export default App
