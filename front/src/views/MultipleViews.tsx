fd65ffe06f6ba22a26c070c17e97a1ceb7b64543
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-10-26 10:26:57 +0300

    UPD: adds margin to option panel

--- front/src/views/MultipleViews.tsx
+++ front/src/views/MultipleViews.tsx
@@ -18,55 +18,56 @@ export const MultipleViews = () => {
 
   return (
     <div>
-      <input
-        id='toggle-system-control'
-        type='checkbox'
-        checked={displaySystemControl}
-        onChange={() => toggleView(displaySystemControl, setDisplaySystemControl)}
-      />
-      <label htmlFor='toggle-system-control'>System Control</label>
-      <br/>
+      <OptionWrapper>
+        <input
+          id='toggle-system-control'
+          type='checkbox'
+          checked={displaySystemControl}
+          onChange={() => toggleView(displaySystemControl, setDisplaySystemControl)}
+        />
+        <label htmlFor='toggle-system-control'>System Control</label>
+        <br />
 
-      <input
-        id='toggle-plot'
-        type='checkbox'
-        checked={displayPlot}
-        onChange={() => toggleView(displayPlot, setDisplayPlot)}
-      />
-      <label htmlFor='toggle-plot'>EEG plot</label>
-      <br/>
+        <input
+          id='toggle-plot'
+          type='checkbox'
+          checked={displayPlot}
+          onChange={() => toggleView(displayPlot, setDisplayPlot)}
+        />
+        <label htmlFor='toggle-plot'>EEG plot</label>
+        <br />
 
-      <input
-        id='toggle-targets'
-        type='checkbox'
-        checked={displayTargets}
-        onChange={() => toggleView(displayTargets, setDisplayTargets)}
-      />
-      <label htmlFor='toggle-targets'>Targets table</label>
-      <br/>
-
+        <input
+          id='toggle-targets'
+          type='checkbox'
+          checked={displayTargets}
+          onChange={() => toggleView(displayTargets, setDisplayTargets)}
+        />
+        <label htmlFor='toggle-targets'>Targets table</label>
+        <br />
+      </OptionWrapper>
       <ViewContainer>
-      {displaySystemControl ? (
-        <Wrapper>
-          <SmallHeader>System control</SmallHeader>
-          <ExperimentControl />
-          <SystemState />
-        </Wrapper>
-      ) : null}
+        {displaySystemControl ? (
+          <Wrapper>
+            <SmallHeader>System control</SmallHeader>
+            <ExperimentControl />
+            <SystemState />
+          </Wrapper>
+        ) : null}
 
-      {displayPlot ? (
-        <Wrapper>
-          <SmallHeader>EEG plot</SmallHeader>
-          <DataVisualize />
-        </Wrapper>
-      ) : null}
+        {displayPlot ? (
+          <Wrapper>
+            <SmallHeader>EEG plot</SmallHeader>
+            <DataVisualize />
+          </Wrapper>
+        ) : null}
 
-      {displayTargets ? (
-        <Wrapper>
-          <SmallHeader>Targets table</SmallHeader>
-          <Targets />
-        </Wrapper>
-      ) : null}
+        {displayTargets ? (
+          <Wrapper>
+            <SmallHeader>Targets table</SmallHeader>
+            <Targets />
+          </Wrapper>
+        ) : null}
       </ViewContainer>
     </div>
   )
@@ -75,6 +76,10 @@ export const MultipleViews = () => {
 const ViewContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+`
+
+const OptionWrapper = styled.div`
+  margin: 0.5rem;
 `
 
 const Wrapper = styled.div`
