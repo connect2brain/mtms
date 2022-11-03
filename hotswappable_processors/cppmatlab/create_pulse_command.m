8c3b4216266e3268492308cc097a359d05cfbe2b
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-11-03 09:12:50 +0200

    FIX: fixes create command pulse_piece to waveform

--- hotswappable_processors/cppmatlab/create_pulse_command.m
+++ hotswappable_processors/cppmatlab/create_pulse_command.m
@@ -5,12 +5,12 @@ function command = create_pulse_command(event_id, channel, execution_condition, 
 
     command.channel = uint8(channel);
     command.event = event;
-    command.pieces = get_default_waveform(channel);
+    command.waveform = get_default_waveform(channel);
     command.event_type = uint8(0);
     
     command.target_voltage = uint16(0);
     
     coder.cstructname(command, 'matlab_fpga_event');
     coder.cstructname(command.event, 'event');
-    coder.cstructname(command.pieces, 'pulse_piece');
+    coder.cstructname(command.waveform, 'waveform');
 end
