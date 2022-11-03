3c0724e9ca0c41c74e0f676c4c3d938e6740df33
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-11-03 14:54:22 +0200

    ADD: charge, discharge and signal out to webglplot

--- front/src/components/WebGLPlot.tsx
+++ front/src/components/WebGLPlot.tsx
@@ -6,15 +6,20 @@ import styled from 'styled-components'
 type WebGLPlotProps = {
   eegData: Datapoint[]
   pulseData: Datapoint[]
+  chargeData: Datapoint[]
+  dischargeData: Datapoint[]
+  signalOutData: Datapoint[]
 }
 
-export const WebGLPlot = ({ eegData, pulseData }: WebGLPlotProps) => {
+export const WebGLPlot = ({ eegData, pulseData, chargeData, dischargeData, signalOutData }: WebGLPlotProps) => {
   const canvasMain = useRef<HTMLCanvasElement>(null)
 
   const [webGLPlot, setWebGLPlot] = useState<WebglPlot>()
   const [eegLine, setEegLine] = useState<WebglLine>()
   const [pulseLine, setPulseLine] = useState<WebglLine>()
-  const [triggerLine, setTriggerLine] = useState<WebglLine>()
+  const [signalOutLine, setSignalOutLine] = useState<WebglLine>()
+  const [chargeLine, setChargeLine] = useState<WebglLine>()
+  const [dischargeLine, setDischargeLine] = useState<WebglLine>()
 
   useEffect(() => {
     if (canvasMain.current) {
@@ -28,15 +33,28 @@ export const WebGLPlot = ({ eegData, pulseData }: WebGLPlotProps) => {
 
       const line1 = new WebglLine(new ColorRGBA(1, 0, 0, 1), numX)
       const line2 = new WebglLine(new ColorRGBA(0, 0, 1, 1), numX)
+      const line3 = new WebglLine(new ColorRGBA(0.9, 0.8, 0.2, 1), numX)
+      const line4 = new WebglLine(new ColorRGBA(0, 1, 0, 1), numX)
+      const line5 = new WebglLine(new ColorRGBA(0.7, 0.45, 0.3, 1), numX)
+
       webglp.addLine(line1)
       webglp.addLine(line2)
+      webglp.addLine(line3)
+      webglp.addLine(line4)
+      webglp.addLine(line5)
 
       line1.arrangeX()
       line2.arrangeX()
+      line3.arrangeX()
+      line4.arrangeX()
+      line5.arrangeX()
 
       setWebGLPlot(webglp)
       setEegLine(line1)
       setPulseLine(line2)
+      setChargeLine(line3)
+      setSignalOutLine(line4)
+      setDischargeLine(line5)
     }
   }, [])
 
@@ -78,6 +96,57 @@ export const WebGLPlot = ({ eegData, pulseData }: WebGLPlotProps) => {
       cancelAnimationFrame(id)
     }
   }, [pulseData])
+
+  useEffect(() => {
+    let id = 0
+    let renderPlot = () => {
+      const y = new Float32Array(chargeData.map((datapoint) => datapoint.y))
+      chargeLine?.shiftAdd(y)
+      webGLPlot?.update()
+    }
+
+    id = requestAnimationFrame(renderPlot)
+
+    return () => {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      renderPlot = () => {}
+      cancelAnimationFrame(id)
+    }
+  }, [chargeData])
+
+  useEffect(() => {
+    let id = 0
+    let renderPlot = () => {
+      const y = new Float32Array(dischargeData.map((datapoint) => datapoint.y))
+      dischargeLine?.shiftAdd(y)
+      webGLPlot?.update()
+    }
+
+    id = requestAnimationFrame(renderPlot)
+
+    return () => {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      renderPlot = () => {}
+      cancelAnimationFrame(id)
+    }
+  }, [dischargeData])
+
+  useEffect(() => {
+    let id = 0
+    let renderPlot = () => {
+      const y = new Float32Array(signalOutData.map((datapoint) => datapoint.y))
+      signalOutLine?.shiftAdd(y)
+      webGLPlot?.update()
+    }
+
+    id = requestAnimationFrame(renderPlot)
+
+    return () => {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      renderPlot = () => {}
+      cancelAnimationFrame(id)
+    }
+  }, [signalOutData])
 
   return (
     <div>
