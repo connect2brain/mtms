d0295e9326c086887ab4dde24bc3bfe80f738a63
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-11-11 11:21:15 +0200

    UPD: minor clean up 2

--- hotswappable_processors/cpp/processor_factory.h
+++ hotswappable_processors/cpp/processor_factory.h
@@ -5,12 +5,12 @@
 #ifndef COMPILER_PROCESSOR_FACTORY_H
 #define COMPILER_PROCESSOR_FACTORY_H
 
-#include "processor.h"
+#include "MatlabProcessor.h"
 #include <chrono>
 
 
-extern "C" Processor *create_processor();
+extern "C" MatlabProcessor *create_processor();
 
-extern "C" void destroy_processor(Processor *p);
+extern "C" void destroy_processor(MatlabProcessor *p);
 
 #endif //COMPILER_PROCESSOR_FACTORY_H
