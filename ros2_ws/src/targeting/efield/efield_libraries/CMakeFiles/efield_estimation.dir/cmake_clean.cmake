e23bc52d06ab0b5217722830357f6c4dbca97263
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-11-11 11:07:55 +0200

    ADD: Adds efield service

---
+++ ros2_ws/src/targeting/efield/efield_libraries/CMakeFiles/efield_estimation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/efield_estimation.dir/src/efield_estimation.cpp.o"
+  "libefield_estimation.pdb"
+  "libefield_estimation.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/efield_estimation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- ros2_ws/src/targeting/efield/ros_entrypoint.sh
+++ ros2_ws/src/targeting/efield/ros_entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 source /opt/ros/galactic/setup.bash
 source /app/ros2_ws/install/setup.bash
 
-ros2 run efield efield_publisher
+ros2 launch efield efield.launch.py log-level:="$ROS_LOG_LEVEL"
