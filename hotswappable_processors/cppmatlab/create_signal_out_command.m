9d694018fc8608aaa05dfc686b81054eba92cd6f
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-11-17 09:48:24 +0200

    ADD: duration us to eeg_processor

--- hotswappable_processors/cppmatlab/create_signal_out_command.m
+++ hotswappable_processors/cppmatlab/create_signal_out_command.m
@@ -1,4 +1,4 @@
-function command = create_signal_out_command(event_id, port, execution_condition, time)
+function command = create_signal_out_command(event_id, port, duration_us, execution_condition, time)
     event.id = uint16(event_id);
     event.execution_condition = uint8(execution_condition);
     event.time = double(time);
@@ -7,8 +7,8 @@ function command = create_signal_out_command(event_id, port, execution_condition
     command.event = event;
     command.waveform = get_default_waveform(port);
     command.event_type = uint8(3);
-    
     command.target_voltage = uint16(0);
+    command.duration_us = uint32(duration_us);
     
     coder.cstructname(command, 'matlab_fpga_event');
     coder.cstructname(command.event, 'event');
