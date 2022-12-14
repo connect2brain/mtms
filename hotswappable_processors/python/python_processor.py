ff3558b037149545ae9f2b1301dd1d5f50ac9c3c
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-12-14 13:48:10 +0200

    ADD: Circular buffer for python processor

--- hotswappable_processors/python/python_processor.py
+++ hotswappable_processors/python/python_processor.py
@@ -4,16 +4,23 @@ from .execution_condition import ExecutionCondition
 
 
 class Processor(BaseProcessor):
-    def __init__(self) -> None:
+    def __init__(self):
+        super().__init__()
         self.event_index = 1
 
     def init_experiment(self):
+        super().init_experiment()
+
         return []
 
     def end_experiment(self):
+        super().end_experiment()
+
         return []
 
-    def data_received(self, data, time, first_sample_of_experiment):
+    def data_received(self, sample, time, first_sample_of_experiment):
+        super().data_received(sample, time, first_sample_of_experiment)
+
         event = {
             "id": self.event_index,
             "execution_condition": ExecutionCondition.INSTANT.value,
