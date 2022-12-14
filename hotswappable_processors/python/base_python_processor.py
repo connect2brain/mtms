9442471595927594c73063ec2a4d4ae1582d09d0
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-12-14 13:48:10 +0200

    ADD: Circular buffer for python processor

--- hotswappable_processors/python/base_python_processor.py
+++ hotswappable_processors/python/base_python_processor.py
@@ -1,7 +1,14 @@
 from abc import abstractmethod
+from circular_buffer import CircularBuffer
 
 
 class BaseProcessor:
+
+    def __init__(self, auto_enqueue=False, window_size=5000):
+        self.auto_enqueue = auto_enqueue
+        self.window_size = window_size
+        self.samples = CircularBuffer(self.window_size)
+
     @abstractmethod
     def init_experiment(self):
         pass
@@ -10,6 +17,12 @@ class BaseProcessor:
     def end_experiment(self):
         pass
 
+    def enqueue(self, sample):
+        self.samples.append(sample)
+
     @abstractmethod
-    def data_received(self, data, time, first_sample_of_experiment):
+    def data_received(self, sample, time, first_sample_of_experiment):
+        if self.auto_enqueue:
+            self.enqueue(sample)
+
         pass
