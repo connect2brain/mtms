d82e7c74b21aced4e147b7786eafd4450005da79
Kyösti Alkio <kyosti.alkio@gmail.com>
2022-12-15 10:13:05 +0200

    MOD: Update CircularBuffer to use numpy

--- hotswappable_processors/python/circular_buffer.py
+++ hotswappable_processors/python/circular_buffer.py
@@ -2,25 +2,22 @@
 Adapted from https://stackoverflow.com/a/40784706/6215543
 """
 
+import numpy as np
+
 
 class CircularBuffer(object):
-    def __init__(self, size):
+    def __init__(self, window_size, columns, dtype=float):
         self.index = 0
-        self.size = size
-        self.buffer = []
+        self.size = window_size
+        self.shape = (window_size, columns)
+        self.buffer = np.zeros(shape=self.shape, dtype=dtype)
 
     def get_buffer(self):
         return self.buffer[self.index:] + self.buffer[:self.index]
 
     def append(self, value):
-        if len(self.buffer) == self.size:
-            self.buffer[self.index] = value
-        else:
-            self.buffer.append(value)
+        self.buffer[self.index] = value
         self.index = (self.index + 1) % self.size
 
     def __getitem__(self, key):
-        if len(self.buffer) == self.size:
-            return self.buffer[(key + self.index) % self.size]
-        else:
-            return self.buffer[key]
+        return self.buffer[(key + self.index) % self.size]
