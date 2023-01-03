2c3c7463e29e9da07a7eaadc93a73dbb0d06e266
Kyösti Alkio <kyosti.alkio@gmail.com>
2023-01-03 15:05:01 +0200

    ADD: RingBuffer to cpp processor

---
+++ hotswappable_processors/cpp/src/ring_buffer.h
@@ -0,0 +1,31 @@
+//
+// Created by alqio on 3.1.2023.
+//
+
+#ifndef EEG_PROCESSOR_RING_BUFFER_H
+#define EEG_PROCESSOR_RING_BUFFER_H
+
+#include <vector>
+
+class RingBuffer {
+public:
+  RingBuffer(unsigned window_size, unsigned columns);
+
+  std::vector<std::vector<double>> get_buffer();
+
+  void append(std::vector<double> sample);
+
+  std::vector<double> operator[] (unsigned i);
+
+  void print();
+
+private:
+  unsigned window_size;
+  unsigned columns;
+  std::vector<std::vector<double>> buffer;
+  bool full;
+  unsigned index;
+  unsigned nof_elements;
+};
+
+#endif //EEG_PROCESSOR_RING_BUFFER_H
