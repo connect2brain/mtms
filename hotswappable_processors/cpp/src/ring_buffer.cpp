6812a26ad7994d8ee065a2d50f31242b9da1947c
Kyösti Alkio <kyosti.alkio@gmail.com>
2023-01-04 09:11:38 +0200

    UPD: Add timing

--- hotswappable_processors/cpp/src/ring_buffer.cpp
+++ hotswappable_processors/cpp/src/ring_buffer.cpp
@@ -4,6 +4,10 @@
 
 #include "ring_buffer.h"
 #include <iostream>
+#include <chrono>
+
+using namespace std::chrono_literals;
+using namespace std::chrono;
 
 RingBuffer::RingBuffer(unsigned int window_size, unsigned int columns) {
   this->window_size = window_size;
@@ -35,13 +39,35 @@ std::vector<double> RingBuffer::operator[](unsigned int i) {
   return this->buffer[(i + this->index) % this->window_size];
 }
 
+std::vector<double> RingBuffer::at(unsigned int i) {
+  return this->buffer[(i + this->index) % this->window_size];
+}
+
 std::vector<std::vector<double>> RingBuffer::get_buffer() {
+  std::vector<std::vector<double>> buf;
+
   if (!this->full) {
     return std::vector<std::vector<double>>(this->buffer.begin(), this->buffer.begin() + index);
   }
+  for (unsigned i = this->index; i <= this->nof_elements; i++) {
+    buf.push_back(this->at(i));
+  }
+  for (unsigned i = 0; i < this->index; i++) {
+    buf.push_back(this->at(i));
+  }
+
+  //return buf;
+
+  auto start = steady_clock::now();
+
   auto left = std::vector<std::vector<double>>(this->buffer.begin() + index, this->buffer.end());
   auto right = std::vector<std::vector<double>>(this->buffer.begin(), this->buffer.begin() + index);
+
+  auto end = steady_clock::now();
+  auto total = duration_cast<microseconds>(end - start);
   std::copy(right.begin(), right.end(), std::back_inserter(left));
+  std::cout << "duration: " << total.count() << std::endl;
+
   return left;
 }
 
