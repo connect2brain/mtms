1a15f8dfbff58215d305c4f195cad9d828cd46e0
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-01-16 20:06:59 +0200

    FIX: Minor bug in python_processor.py and spike_processor.py

--- hotswappable_processors/python/spike_processor.py
+++ hotswappable_processors/python/spike_processor.py
@@ -33,7 +33,7 @@ class Processor(BaseProcessor):
         event_info = {
             "id": self.event_index,
             "execution_condition": ExecutionCondition.IMMEDIATE.value,
-            "execution_time": execution_time,
+            "execution_time": time,
         }
         self.event_index += 1
 
