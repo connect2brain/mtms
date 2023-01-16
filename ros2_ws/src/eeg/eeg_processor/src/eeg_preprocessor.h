6791a3dd3b8c8cc4b5e582472efddf0a0414007d
Kyösti Alkio <kyosti.alkio@gmail.com>
2023-01-16 16:50:33 +0200

    MOD: Rename eeg_pre_processor -> eeg_preprocessor

--- ros2_ws/src/eeg/eeg_processor/src/eeg_preprocessor.h
+++ ros2_ws/src/eeg/eeg_processor/src/eeg_preprocessor.h
@@ -2,19 +2,19 @@
 // Created by alqio on 16.1.2023.
 //
 
-#ifndef EEG_PROCESSOR_EEG_PRE_PROCESSOR_H
-#define EEG_PROCESSOR_EEG_PRE_PROCESSOR_H
+#ifndef EEG_PROCESSOR_EEG_PREPROCESSOR_H
+#define EEG_PROCESSOR_EEG_PREPROCESSOR_H
 
 #include "mtms_interfaces/msg/eeg_datapoint.hpp"
 #include "processor_node.h"
 
-class EegPreProcessor : public ProcessorNode<mtms_interfaces::msg::EegDatapoint, mtms_interfaces::msg::EegDatapoint> {
+class EegPreprocessor : public ProcessorNode<mtms_interfaces::msg::EegDatapoint, mtms_interfaces::msg::EegDatapoint> {
 public:
-  EegPreProcessor();
+  EegPreprocessor();
 private:
   virtual void publish_events(double_t time, const std::vector<mtms_interfaces::msg::EegDatapoint> &events);
   rclcpp::Publisher<mtms_interfaces::msg::EegDatapoint>::SharedPtr publisher;
 
 };
 
-#endif //EEG_PROCESSOR_EEG_PRE_PROCESSOR_H
+#endif //EEG_PROCESSOR_EEG_PREPROCESSOR_H
