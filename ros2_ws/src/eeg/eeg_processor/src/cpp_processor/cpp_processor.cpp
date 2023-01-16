4051f914e9bdd716a6d6304fa484b30dd0e83cbf
Kyösti Alkio <kyosti.alkio@gmail.com>
2023-01-16 11:58:29 +0200

    ADD: raw_eeg_received and renames data_received to cleaned_eeg_received

---
+++ ros2_ws/src/eeg/eeg_processor/src/cpp_processor/cpp_processor.cpp
@@ -0,0 +1,65 @@
+//
+// Created by alqio on 1.9.2022.
+//
+
+#include "cpp_processor.h"
+#include "matlab_helpers.h"
+
+CppProcessor::CppProcessor(const std::string &script_path) {
+  processor_factory = dlopen(script_path.c_str(), RTLD_NOW);
+  if (processor_factory == nullptr) {
+    std::cerr << "Cannot load processor_factory: " << dlerror() << std::endl;
+  }
+  auto *create_processor_func = reinterpret_cast<create_cpp_processor >(dlsym(processor_factory, "create_processor"));
+
+  if (!create_processor_func) {
+    std::cerr << "Cannot load create_processor_func symbols: " << dlerror() << std::endl;
+  }
+
+  inner_processor = std::unique_ptr<CppProcessorInterface>(create_processor_func());
+}
+
+std::vector<Event> CppProcessor::present_stimulus_received(mtms_interfaces::msg::Event event) {}
+
+
+std::vector<mtms_interfaces::msg::EegDatapoint> CppProcessor::raw_eeg_received(mtms_interfaces::msg::EegDatapoint sample) {
+}
+
+std::vector<Event> CppProcessor::cleaned_eeg_received(mtms_interfaces::msg::EegDatapoint sample) {
+  auto events = inner_processor->data_received(
+      sample.eeg_channels,
+      sample.time,
+      sample.first_sample_of_experiment
+  );
+
+  std::vector<Event> output;
+
+  for (auto i = 0; i < events.size(); i++) {
+    auto event = events[i];
+    auto fpga_event = convert_matlab_fpga_event_to_fpga_event(event);
+    output.push_back(fpga_event);
+  }
+
+  return output;
+}
+
+std::vector<Event> CppProcessor::init() {
+  std::vector<Event> fpga_events;
+  auto events = inner_processor->init_experiment();
+
+  for (auto i = events.begin(); i != events.end(); i++) {
+    auto event = *i;
+    auto fpga_event = convert_matlab_fpga_event_to_fpga_event(event);
+    fpga_events.push_back(fpga_event);
+  }
+  return fpga_events;
+}
+
+CppProcessor::~CppProcessor() {
+  //Empty the pointer
+  //inner_processor.reset();
+
+  if (processor_factory != nullptr) {
+    dlclose(processor_factory);
+  }
+}
--- ros2_ws/src/eeg/eeg_processor/src/cpp_processor/cpp_processor.h
+++ ros2_ws/src/eeg/eeg_processor/src/cpp_processor/cpp_processor.h
@@ -11,89 +11,25 @@
 #include <cstdio>
 #include <cstring>
 #include "cpp_processor_interface.h"
-#include "matlab_helpers.h"
 
-
-template<class InputType, class OutputType>
-class CppProcessor : public ProcessorWrapper<InputType, OutputType> {
+class CppProcessor : public ProcessorWrapper {
 public:
   explicit CppProcessor(const std::string &script_path);
 
   ~CppProcessor();
 
-  std::vector<OutputType> init() override;
+  std::vector<Event> init();
 
-  std::vector<OutputType> eeg_received(mtms_interfaces::msg::EegDatapoint sample) override;
+  std::vector<mtms_interfaces::msg::EegDatapoint> raw_eeg_received(mtms_interfaces::msg::EegDatapoint sample);
 
-  std::vector<OutputType> event_received(mtms_interfaces::msg::Event event) override;
+  std::vector<Event> cleaned_eeg_received(mtms_interfaces::msg::EegDatapoint sample);
+
+  std::vector<Event> present_stimulus_received(mtms_interfaces::msg::Event event);
 
 private:
   void *processor_factory;
   std::unique_ptr<CppProcessorInterface> inner_processor;
 };
-
-
-template<class InputType, class OutputType>
-CppProcessor<InputType, OutputType>::CppProcessor(const std::string &script_path) {
-  processor_factory = dlopen(script_path.c_str(), RTLD_NOW);
-  if (processor_factory == nullptr) {
-    std::cerr << "Cannot load processor_factory: " << dlerror() << std::endl;
-  }
-  auto *create_processor_func = reinterpret_cast<create_cpp_processor >(dlsym(processor_factory, "create_processor"));
-
-  if (!create_processor_func) {
-    std::cerr << "Cannot load create_processor_func symbols: " << dlerror() << std::endl;
-  }
-
-  inner_processor = std::unique_ptr<CppProcessorInterface>(create_processor_func());
-}
-
-template<class InputType, class OutputType>
-std::vector<OutputType> CppProcessor<InputType, OutputType>::event_received(mtms_interfaces::msg::Event event) {
-
-}
-
-template<class InputType, class OutputType>
-std::vector<OutputType> CppProcessor<InputType, OutputType>::eeg_received(mtms_interfaces::msg::EegDatapoint data) {
-  auto events = inner_processor->data_received(
-      data.eeg_channels,
-      data.time,
-      data.first_sample_of_experiment
-  );
-
-  std::vector<Event> output;
-
-  for (auto i = 0; i < events.size(); i++) {
-    auto event = events[i];
-    auto fpga_event = convert_matlab_fpga_event_to_fpga_event(event);
-    output.push_back(fpga_event);
-  }
-
-  return output;
-}
-
-template<class InputType, class OutputType>
-std::vector<OutputType> CppProcessor<InputType, OutputType>::init() {
-  std::vector<Event> fpga_events;
-  auto events = inner_processor->init_experiment();
-
-  for (auto i = events.begin(); i != events.end(); i++) {
-    auto event = *i;
-    auto fpga_event = convert_matlab_fpga_event_to_fpga_event(event);
-    fpga_events.push_back(fpga_event);
-  }
-  return fpga_events;
-}
-
-template<class InputType, class OutputType>
-CppProcessor<InputType, OutputType>::~CppProcessor() {
-  //Empty the pointer
-  //inner_processor.reset();
-
-  if (processor_factory != nullptr) {
-    dlclose(processor_factory);
-  }
-}
 
 
 #endif //PROCESSOR_FACTORY_CPP_PROCESSOR_H
