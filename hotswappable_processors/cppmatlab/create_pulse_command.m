569127111b818d016360c220e8a9418af672f4ef
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-01-16 15:00:13 +0200

    MOD: Change Event type to EventInfo

--- hotswappable_processors/cppmatlab/create_pulse_command.m
+++ hotswappable_processors/cppmatlab/create_pulse_command.m
@@ -1,10 +1,10 @@
 function command = create_pulse_command(event_id, channel, execution_condition, time)
-    event.id = uint16(event_id);
-    event.execution_condition = uint8(execution_condition);
-    event.time = double(time);
+    event_info.id = uint16(event_id);
+    event_info.execution_condition = uint8(execution_condition);
+    event_info.time = double(time);
 
     command.channel = uint8(channel);
-    command.event = event;
+    command.event_info = event_info;
     command.waveform = get_default_waveform(channel);
     command.event_type = uint8(0);
     command.target_voltage = uint16(0);
--- ros2_ws/src/bridges/fpga_bridge/src/nodes/discharge_handler.cpp
+++ ros2_ws/src/bridges/fpga_bridge/src/nodes/discharge_handler.cpp
@@ -2,7 +2,7 @@
 
 #include "event_interfaces/srv/send_discharge.hpp"
 #include "event_interfaces/msg/discharge.hpp"
-#include "event_interfaces/msg/event.hpp"
+#include "event_interfaces/msg/event_info.hpp"
 
 #include "NiFpga_mTMS.h"
 #include "fpga.h"
@@ -27,11 +27,11 @@ public:
 
       /* Serialize event info. */
 
-      event_interfaces::msg::Event event = discharge.event;
+      event_interfaces::msg::EventInfo event_info = discharge.event_info;
 
-      uint16_t id = event.id;
-      uint8_t execution_condition = event.execution_condition.value;
-      double_t time = event.time;
+      uint16_t id = event_info.id;
+      uint8_t execution_condition = event_info.execution_condition.value;
+      double_t time = event_info.time;
       uint64_t time_ticks = (uint64_t)(time * CLOCK_FREQUENCY_HZ);
 
       serialized_message.init(channel);
