25fad5d792d7e478ffb38eaa99f347811f4b9814
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-01-16 15:50:19 +0200

    ADD: Field decision_time to EventInfo, rename time to execution_time

--- hotswappable_processors/cppmatlab/create_pulse_command.m
+++ hotswappable_processors/cppmatlab/create_pulse_command.m
@@ -1,7 +1,7 @@
-function command = create_pulse_command(event_id, channel, execution_condition, time)
+function command = create_pulse_command(event_id, channel, execution_condition, execution_time)
     event_info.id = uint16(event_id);
     event_info.execution_condition = uint8(execution_condition);
-    event_info.time = double(time);
+    event_info.execution_time = double(execution_time);
 
     command.channel = uint8(channel);
     command.event_info = event_info;
