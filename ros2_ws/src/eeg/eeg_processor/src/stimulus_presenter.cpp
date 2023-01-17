a5f7d6fe5360ed7b97b372645232ed71fb2d253c
Kyösti Alkio <kyosti.alkio@gmail.com>
2023-01-17 10:57:16 +0200

    MOD: Rename MatlabProcessor.cleaned_eeg_received matlab_events -> events_out

--- ros2_ws/src/eeg/eeg_processor/src/stimulus_presenter.cpp
+++ ros2_ws/src/eeg/eeg_processor/src/stimulus_presenter.cpp
@@ -21,12 +21,58 @@ StimulusPresenter::StimulusPresenter() : ProcessorNode("eeg_preprocessor") {
   };
 
   this->subscription = this->template create_subscription<event_interfaces::msg::Stimulus>("/event/stimulus", 5000,
-                                                                                              subscription_callback);
+                                                                                           subscription_callback);
 
 }
 
 void StimulusPresenter::publish_events(double_t time, const std::vector<Event> &events) {
-  for (Event event : events) {
+  for (Event event: events) {
+    RCLCPP_INFO(this->get_logger(), "Publishing events.");
+
+    switch (event.event_type) {
+      case PULSE:
+        RCLCPP_INFO(rclcpp::get_logger("stimulus_presenter"),
+                    "Published fpga pulse event timed at %.4f.",
+                    event.pulse.event_info.execution_time);
+
+        event.pulse.event_info.decision_time = time;
+        this->pulse_publisher->publish(event.pulse);
+        break;
+
+      case CHARGE:
+        RCLCPP_INFO(rclcpp::get_logger("stimulus_presenter"),
+                    "Published fpga charge event timed at %.4f.",
+                    event.charge.event_info.execution_time);
+
+        event.charge.event_info.decision_time = time;
+        this->charge_publisher->publish(event.charge);
+        break;
+
+      case DISCHARGE:
+        RCLCPP_INFO(rclcpp::get_logger("stimulus_presenter"),
+                    "Published fpga discharge event timed at %.4f.",
+                    event.discharge.event_info.execution_time);
+
+        event.discharge.event_info.decision_time = time;
+        this->discharge_publisher->publish(event.discharge);
+        break;
+
+      case SIGNAL_OUT:
+        RCLCPP_INFO(rclcpp::get_logger("stimulus_presenter"),
+                    "Published signal out pulse event timed at %.4f.",
+                    event.signal_out.event_info.execution_time);
+
+        event.signal_out.event_info.decision_time = time;
+        this->signal_out_publisher->publish(event.signal_out);
+        break;
+
+      case STIMULUS:
+        RCLCPP_WARN(rclcpp::get_logger("stimulus_presenter"),
+                    "Warning, sending stimulus from stimulus presenter is not supported");
+
+      default:
+        RCLCPP_WARN(rclcpp::get_logger("stimulus_presenter"), "Warning, unknown fpga event type: %d", event.event_type);
+    }
 
   }
 }
