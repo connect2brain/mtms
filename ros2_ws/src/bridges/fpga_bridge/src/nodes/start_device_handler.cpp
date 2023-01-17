0d2c892998e6032401fec1dcb5e95803d26a71ee
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-01-17 21:34:59 +0200

    MOD: Rename fpga_interfaces to mtms_device_interfaces

--- ros2_ws/src/bridges/fpga_bridge/src/nodes/start_device_handler.cpp
+++ ros2_ws/src/bridges/fpga_bridge/src/nodes/start_device_handler.cpp
@@ -1,14 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
 
-#include "fpga_interfaces/srv/start_device.hpp"
+#include "mtms_device_interfaces/srv/start_device.hpp"
 
 #include "NiFpga_mTMS.h"
 #include "fpga.h"
 #include "memory_utils.h"
 #include "scheduling_utils.h"
 
-void start_device(const std::shared_ptr<fpga_interfaces::srv::StartDevice::Request> request,
-                  std::shared_ptr<fpga_interfaces::srv::StartDevice::Response> response) {
+void start_device(const std::shared_ptr<mtms_device_interfaces::srv::StartDevice::Request> request,
+                  std::shared_ptr<mtms_device_interfaces::srv::StartDevice::Response> response) {
 
   NiFpga_MergeStatus(&status, NiFpga_WriteBool(session, NiFpga_mTMS_ControlBool_Startdevice, true));
 
@@ -20,11 +20,11 @@ class StartDevice : public rclcpp::Node {
 public:
   StartDevice()
       : Node("start_device") {
-    start_device_service_ = this->create_service<fpga_interfaces::srv::StartDevice>("/fpga/start_device", start_device);
+    start_device_service_ = this->create_service<mtms_device_interfaces::srv::StartDevice>("/fpga/start_device", start_device);
   }
 
 private:
-  rclcpp::Service<fpga_interfaces::srv::StartDevice>::SharedPtr start_device_service_;
+  rclcpp::Service<mtms_device_interfaces::srv::StartDevice>::SharedPtr start_device_service_;
 };
 
 int main(int argc, char **argv) {
