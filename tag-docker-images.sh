00dfbe30d1891aa0fe7ad36bfce8579a415782f1
Kim-Valen <kim.valen@aalto.fi>
2023-01-18 13:17:44 +0200

    MOD: Rename data_batcher to eeg_batcher

--- tag-docker-images.sh
+++ tag-docker-images.sh
@@ -7,6 +7,6 @@ docker tag mtms_rosbridge okahilak/mtms:rosbridge
 docker tag mtms_planner okahilak/mtms:planner
 docker tag mtms_pulse_sequence_controller okahilak/mtms:pulse_sequence_controller
 docker tag mtms_eeg_simulator okahilak/mtms:eeg_simulator
-docker tag mtms_data_batcher okahilak/mtms:data_batcher
+docker tag mtms_eeg_batcher okahilak/mtms:eeg_batcher
 docker tag mtms_front okahilak/mtms:front
 docker tag mtms_optitrack_bridge okahilak/mtms:optitrack_bridge
