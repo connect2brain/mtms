0244ab325b67e214d75a6708f8199cd23d92cea3
Kyösti Alkio <kyosti.alkio@gmail.com>
2023-02-28 09:48:41 +0200

    ADD: End experiment back

--- ros2_ws/src/eeg/eeg_processor/src/processor.h
+++ ros2_ws/src/eeg/eeg_processor/src/processor.h
@@ -22,6 +22,8 @@ public:
 
   virtual std::vector<Event> present_stimulus_received(event_interfaces::msg::Stimulus event) = 0;
 
+  virtual std::vector<Event> end_experiment() = 0;
+
 };
 
 #endif //EEG_PROCESSOR_PROCESSOR_H
