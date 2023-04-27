2af5de88c1155cf43d370f41a1e9a9d1586e3e8f
sotodela <ana.sotodelacruz@gmail.com>
2023-04-27 14:04:57 +0300

    ADD: ROS services for coilmodel and modify meshes to vector

--- ros2_ws/src/targeting/efield/src/efield_estimation.h
+++ ros2_ws/src/targeting/efield/src/efield_estimation.h
@@ -5,6 +5,8 @@
 
 void efield_estimation(std::vector<float> &position, std::vector<double> &orientation, std::vector<float> &rot_matrix,
                        std::vector<double> &efield_vector);
-void init_efield(std::string cortexfile, std::string meshfile, bool &success);
+//void init_efield(std::string cortexfile, std::string meshfile, bool &success);
+void init_efield(std::string cortexfile, std::vector<std::string> meshfile, bool &success);
 
+void add_coil(std::string coilfile, bool &success);
 #endif
