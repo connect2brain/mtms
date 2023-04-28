6f3993ab2280d2c3f50286d49cd542b23f684af3
sotodela <ana.sotodelacruz@gmail.com>
2023-04-28 09:24:54 +0300

    ADD: Conductivities vectors from InVesalius

--- ros2_ws/src/targeting/efield/src/efield_estimation.h
+++ ros2_ws/src/targeting/efield/src/efield_estimation.h
@@ -6,7 +6,7 @@
 void efield_estimation(std::vector<float> &position, std::vector<double> &orientation, std::vector<float> &rot_matrix,
                        std::vector<double> &efield_vector);
 //void init_efield(std::string cortexfile, std::string meshfile, bool &success);
-void init_efield(std::string cortexfile, std::vector<std::string> meshfile, bool &success);
+void init_efield(std::string cortexfile, std::vector<std::string> meshfile, std::vector<float> ci, std::vector<float> co, bool &success);
 
 void add_coil(std::string coilfile, bool &success);
 #endif
