dca9d300aacd695d5dc9f2a1b978013822a46d2d
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-05-02 11:45:30 +0300

    MOD: Make bag_recorder service record bags in project directory

--- ros2_ws/src/other/bag_recorder/ros_entrypoint.sh
+++ ros2_ws/src/other/bag_recorder/ros_entrypoint.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
 
+if [ -z "$PROJECT_NAME" ]; then
+    echo "Error: Project name is undefined."
+    exit 1
+fi
+
+if [ -z "$BAG_NAME" ]; then
+    echo "Error: Bag name is undefined."
+    exit 1
+fi
+
 source /opt/ros/galactic/setup.bash
 source /app/ros2_ws/install/setup.bash
 
-ros2 bag record -a -o /app/data/bags/$(date +%Y-%m-%d_%H-%M-%S)_$BAG_NAME
+ros2 bag record -a -o /app/projects/$PROJECT_NAME/bags/$(date +%Y-%m-%d_%H-%M-%S)_$BAG_NAME
