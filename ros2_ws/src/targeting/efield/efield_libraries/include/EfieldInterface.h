cdb5cca36be9bcfa5ca29df9bbe7b669d65c4702
sotodela <ana.sotodelacruz@gmail.com>
2023-05-02 14:03:46 +0300

    ADD: header files for EfieldInterface class

---
+++ ros2_ws/src/targeting/efield/efield_libraries/include/EfieldInterface.h
@@ -0,0 +1,34 @@
+//
+// Created by mtms on 18.4.2023.
+//
+
+#ifndef TMS_CPP_EFIELDINTERFACE_H
+#define TMS_CPP_EFIELDINTERFACE_H
+#include <phi_lc>
+#include <tms>
+class EfieldInterface{
+public:
+    EfieldInterface(std::string cortexfile,std::string meshfile,bool &success);
+    void E_norm(Matrix<float, RowMajor> &Etms, std::vector<double> &efield_vector);
+    void efield_estimation(RowVector<float>& position, Matrix<float, RowMajor>& rot_matrix, std::vector<double> &efield_vector);
+
+private:
+    Mesh<float> cortex;
+    std::vector<float> ci = {0.3300};
+    std::vector<float> co = {0};
+    float minusdIPerdt = -6600.0F*10000.0F;
+
+    Matrix<float, RowMajor> spos;
+    Coil<float> coilmodel;
+    Matrix<float>Phi;
+
+public:
+    Mesh<float> m;
+    std::vector< Mesh<float>>meshes;
+    TMS<float,float> *TMS_obj;
+    Matrix<float, RowMajor> Etms;
+
+};
+
+
+#endif //TMS_CPP_EFIELDINTERFACE_H
--- ros2_ws/src/targeting/efield/efield_libraries/src/efield_estimation.cpp
+++ ros2_ws/src/targeting/efield/efield_libraries/src/efield_estimation.cpp
@@ -1,9 +1,6 @@
 //The functions in this file are partially based on hbftms_cpp library example codes written by M. Stenroos
 //This library is not free to use without written permission from the owner Matti Stenroos
 
-#define DATAROOT "/app/ros2_ws/src/targeting/efield/efield_libraries/data/"
-#define DATAROOT1 "/app/data/neuronavigation/efield/"
-#define FLAG_MAGSTIM70 1 // set to 1 to use 42-dipole Magstim 70 model
 
 #include <phi_lc>
 #include <tms>
@@ -12,61 +9,27 @@
 #include "EfieldInterface.h"
 
 
-std::string meshroot = std::string(DATAROOT1) ;//+ "headmodels/invesalius/";
-std::string coilfile = std::string(DATAROOT) + "coilmodels/magstim70/magstim70_42.bin";
-//std::string meshfile = meshroot + "is_scaled_up_inv_exp.bin";
 std::vector<double> efield_vector;
-// Initialize first the mesh, then build the meshes vector:
-
-
-//Mesh<float> cortex();
-//Matrix<float, RowMajor> spos(cortex.Points());
-
 Timer t;
-
-// set BEM model conductivities --- in single-shell model, the value of 'ci' doesn't matter
-//std::vector<float> ci = {0.3300};
-//std::vector<float> co = {0};
-
-//std::vector< Matrix<float> > D = BEMOperatorsPhi_LC(meshes);
-//Matrix<float> TM = TM_Phi_LC(D, ci, co);
-//Matrix<float> Phi;
 Mesh<float> *cortex;
-//Coil<float> coilmodel(coilfile);
 Coil<float> coilmodel;
 TMS<float,float> *TMS_obj;
-std::vector<float> ci;//= {0.3300};
-std::vector<float> co;//= {0};
 Mesh<float> m;
 std::vector< Mesh<float>>meshes;
 Matrix<float>Phi;
 Matrix<float, RowMajor> spos;
 
 //function to input data from efield_service
-/*void init_efield(std::string cortexfile, std::string meshfile, bool &success)
-{
-    std::cout<<"cortexfile: "<<cortexfile<<std::endl;
-    std::cout<<"meshfile: "<<meshfile<<std::endl;
-
-    m =Mesh<float>(meshfile);
-    meshes = std::vector<Mesh<float>>({m});
-    cortex =new Mesh<float>(cortexfile);
-    spos= Matrix<float, RowMajor>(cortex->Points());
-    std::vector< Matrix<float> > D = BEMOperatorsPhi_LC(meshes);
-    Matrix<float> TM = TM_Phi_LC(D, ci, co);
-    Phi=LFM_Phi_LC(meshes, TM, spos);
-    TMS_obj=new TMS<float,float>(Phi, meshes, spos);
-    WeightedPhi(meshes, Phi, ci, co);
-    success = true;
-    std::cout<<"Done init! "<<std::endl;
-}*/
 void init_efield(std::string cortexfile, std::vector<std::string> meshfile, std::vector<float> ci, std::vector<float> co, bool &success)
 {
     std::cout<<"cortexfile: "<<cortexfile<<std::endl;
-    std::cout<<"meshfile: "<<meshfile.at(0)<<std::endl;
+    std::cout<<"meshfile: "<<meshfile.size()<<std::endl;
+    std::cout <<"co: "<< co.at(0)<<std::endl;
+    std::cout <<"ci: "<< ci.at(0)<<std::endl;
 
-    m =Mesh<float>(meshfile.at(0));
-    meshes = std::vector<Mesh<float>>({m});
+    for (auto element: meshfile){
+        meshes.push_back(element);
+    }
     cortex =new Mesh<float>(cortexfile);
     spos= Matrix<float, RowMajor>(cortex->Points());
     std::vector< Matrix<float> > D = BEMOperatorsPhi_LC(meshes);
@@ -74,12 +37,17 @@ void init_efield(std::string cortexfile, std::vector<std::string> meshfile, std:
     Phi=LFM_Phi_LC(meshes, TM, spos);
     TMS_obj=new TMS<float,float>(Phi, meshes, spos);
     WeightedPhi(meshes, Phi, ci, co);
+
+    co.clear();
+    ci.clear();
+    D.clear();
+    meshes.clear();
     success = true;
     std::cout<<"Done init! "<<std::endl;
 }
 
 void add_coil(std::string coilfile, bool &success)
-{   std::cout<<"coilfile: "<<coilfile<<std::endl;
+{   std::cout<<"coilfile changed: "<<coilfile<<std::endl;
     coilmodel = Coil<float>(coilfile);
     success = true;
 }
@@ -123,41 +91,5 @@ void efield_estimation(std::vector<float>& position, std::vector<double>& orient
 
 int main(void)
 {
-    std::string meshroot = std::string(DATAROOT) + "/data/headmodels/invesalius1/";
-    std::string coilroot = std::string(DATAROOT) + "/data/coilmodels/";
-    std::vector<double> efield_vector;
-
-    std::string coilfile;
-    if (FLAG_MAGSTIM70)  // Magstim 70, 42 magnetic dipoles
-        coilfile = coilroot + "magstim70/magstim70_42.bin";
-    else // "Navigation coil" of the 5-coil mTMS transducer
-        coilfile = coilroot + "coilset-5/coilset-5-4.bin";
-    std::cout<<"coilfile"<<coilfile<<std::endl;
-
-    std::string cortexfile1 = meshroot + "S2-brainc.bin";
-    std::string bmeshfile1 = meshroot + "S2-scalp.bin";
-    bool success= false;
-    std::string conf_file = "sub-c2b006_1shell.json";
-
-
-    EfieldInterface EfieldInterface(cortexfile1, bmeshfile1, success);
-    std::cout << "Success: " << success<<std::endl;
-    std::cout << "Elements: "<<success <<std::endl;
-    std::cout<<"TMS_obj size Main: "<<EfieldInterface.TMS_obj->CortexSize()<<std::endl;
-
-
-    // set example coil location to vertex 599 (in 0-basis) of the scalp mesh
-    int32_t pind = 599;
-    RowVector<float> cp = EfieldInterface.m.Points().GetRow(pind);
-    std::cout <<"cp:"<< cp(0) <<std::endl;
-
-//    // Define rotation matrix for the coil (Applied from right  x' = x*T_rot)
-    Matrix<float, RowMajor> T_rot(3,3,{0.698830F, 0.0F, 0.715288F,
-                                       0.118700F, 0.986135F, -0.115969F,
-                                       -0.705370F, 0.165947F, 0.689140F});
-
-
-    EfieldInterface.efield_estimation(cp, T_rot, efield_vector);
-
     return 0;
 }
