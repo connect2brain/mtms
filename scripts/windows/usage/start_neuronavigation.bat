6a522aeae0a22c8854d54fed4cb2b836c8d57bd4
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-05-08 13:36:16 +0300

    MOD: Update script to run neuronavigation on lab computer

--- scripts/windows/usage/start_neuronavigation.bat
+++ scripts/windows/usage/start_neuronavigation.bat
@@ -1,4 +1,19 @@
+REM This script is used to start neuronavigation on the Windows computer in the lab in Aalto. Its contents are
+REM bound to change; for instance, we currently have to run neuronavigation outside Docker to be able to use
+REM Optitrack motion tracking. Once Optitrack works with Docker, this can be changed to just run something like:
+REM
+REM docker-compose up neuronavigation
+
 @echo off
 
-cd %MTMS_PATH%
-docker-compose up efield pedal_listener neuronavigation
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+
+call C:\dev\ros2_humble\local_setup.bat
+
+cd %MTMS_ROOT%\ros2_ws
+
+REM Note that neuronavigation_interfaces and ui_interfaces are NOT rebuilt to save some time when starting neuronavigation.
+colcon build --packages-select neuronavigation
+call install\local_setup.bat
+
+ros2 run neuronavigation start
