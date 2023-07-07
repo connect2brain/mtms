f30f7784689ce3e27ee150e848bcb3c7a1b9ae7a
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-07-07 15:56:02 +0300

    MOD: Remove need for explicit channel count in Python EEG (pre)processor
    Instead, infer the channel count from the first EEG datapoint.

--- pipeline/python/base/base_pipeline_stage.py
+++ pipeline/python/base/base_pipeline_stage.py
@@ -5,17 +5,16 @@ from .circular_buffer import CircularBuffer
 
 
 class BasePipelineStage:
-    def __init__(self, disable_sample_buffer=False, sample_buffer_size=5000, channels=63, analysis_interval_in_samples=1):
+    def __init__(self, disable_sample_buffer=False, sample_buffer_size=5000, analysis_interval_in_samples=1):
         self._disable_sample_buffer = disable_sample_buffer
+        self._sample_buffer_size = sample_buffer_size
         self._analysis_interval_in_samples = analysis_interval_in_samples
-
-        self.sample_buffer = CircularBuffer(
-            window_size=sample_buffer_size,
-            columns=channels,
-        )
 
         self.num_of_samples = 0
         self._num_of_samples_since_last_analysis = 0
+
+        self.sample_buffer = None
+        self.num_of_channels = None
 
     @abstractmethod
     def init_experiment(self):
@@ -30,14 +29,28 @@ class BasePipelineStage:
         # Update number of samples received.
         self.num_of_samples += 1
 
-        if self.sample_buffer.is_full:
+        sample_buffer_full = self.sample_buffer is not None and self.sample_buffer.is_full
+
+        if self._disable_sample_buffer or sample_buffer_full:
             self._num_of_samples_since_last_analysis += 1
 
         self._num_of_samples_since_last_analysis = self._num_of_samples_since_last_analysis % self._analysis_interval_in_samples
 
+        # Return if sample buffer is disabled.
+        if self._disable_sample_buffer:
+            return
+
+        # Initialize sample buffer if uninitialized.
+        if self.sample_buffer is None:
+            self.num_of_channels = len(sample)
+
+            self.sample_buffer = CircularBuffer(
+                window_size=self._sample_buffer_size,
+                columns=self.num_of_channels,
+            )
+
         # Append to the sample buffer.
-        if not self._disable_sample_buffer:
-            self.sample_buffer.append(sample)
+        self.sample_buffer.append(sample)
 
     def is_ready_to_analyze(self):
         if not self.sample_buffer.is_full:
