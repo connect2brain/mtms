f4a87c3d9dbe7a160f298ced3abb5386fe8a9979
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-09-11 12:04:31 +0300

    MOD: Change env variable SITE to COIL_ARRAY
    Decoupling site from the coil array that is being used.

--- ros2_ws/src/targeting/targeting/ros_entrypoint.sh
+++ ros2_ws/src/targeting/targeting/ros_entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 source /opt/ros/galactic/setup.bash
 source /app/ros2_ws/install/setup.bash
 
-ros2 launch targeting targeting.launch.py log-level:="$ROS_LOG_LEVEL" site:="$SITE"
+ros2 launch targeting targeting.launch.py log-level:="$ROS_LOG_LEVEL" coil-array:="$COIL_ARRAY"
