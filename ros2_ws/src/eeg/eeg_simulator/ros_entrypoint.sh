f410a0c00ebe50d14b5debd2696b9c7c8e94f075
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-09-18 15:55:28 +0200

    UPD: Upgrade all containers to use ROS Iron

--- ros2_ws/src/eeg/eeg_simulator/ros_entrypoint.sh
+++ ros2_ws/src/eeg/eeg_simulator/ros_entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$EEG_SIMULATOR_ENABLE" != "true" ]; then
   exit 1
 fi
 
-source /opt/ros/galactic/setup.bash
+source /opt/ros/iron/setup.bash
 source /app/ros2_ws/install/setup.bash
 
 ros2 launch eeg_simulator eeg_simulator.launch.py log-level:="$ROS_LOG_LEVEL" data-file:="$EEG_SIMULATOR_DATA_FILE_NAME" sampling-frequency:="$EEG_SIMULATOR_SAMPLING_FREQUENCY" loop:="true" eeg-channels:="$EEG_SIMULATOR_NUMBER_OF_EEG_CHANNELS" emg-channels:="$EEG_SIMULATOR_NUMBER_OF_EMG_CHANNELS" simulate-eeg-device:="$EEG_SIMULATOR_SIMULATE_EEG_DEVICE"
