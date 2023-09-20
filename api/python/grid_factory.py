228ad3355932d4fa22543cdc6dd4a02d582e5e04
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-09-20 17:16:59 +0200

    ADD: API: Action factory, grid factory, and experiment modules to Python API

---
+++ api/python/grid_factory.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class GridFactory:
+    def __init__(self, actions):
+        
+        self.actions = actions
+
+    def to_coords(self, grid):
+        grid = np.asarray(grid)
+
+        coords = np.argwhere(grid) - (np.asarray(grid.shape) - 1) / 2
+        return coords
+
+    def to_params(self, grid):
+        coords = self.to_coords(grid)
+        params = [{
+            'x': x,
+            'y': y,
+        } for x, y in coords]
+
+        return params
+
+    def to_pulses(self, grid, angle, intensity):
+        pulses = [self.actions.create_pulse(
+            params['x'],
+            params['y'],
+            angle,
+            intensity,
+            0.0,
+        ) for params in self.to_params(grid)]
+
+        return pulses
