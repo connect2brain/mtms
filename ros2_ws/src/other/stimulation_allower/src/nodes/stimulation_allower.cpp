296e07349ec294af12803251d3fde6e4f18ac1fb
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-09-22 19:12:47 +0200

    FIX: Deadline for 'coil at target' message
    There were several things wrong:
    
    - The deadline was too short, 0.3 seconds works better
    - Python publisher used a wrong (outdated?) API to define deadline'
    - Subscription options were not passed onto C++ subscriber
    - Lifespan is ignored on the subscriber as per specifications of DDS,
    hence do not define it. (However, defining it anyway does not
    cause harm.)

--- ros2_ws/src/other/stimulation_allower/src/nodes/stimulation_allower.cpp
+++ ros2_ws/src/other/stimulation_allower/src/nodes/stimulation_allower.cpp
@@ -11,7 +11,7 @@ using namespace std::chrono_literals;
 using std::placeholders::_1;
 
 /* HACK: Needs to match the value in neuronavigation bridge. */
-const milliseconds COIL_AT_TARGET_PUBLISHING_INTERVAL = 100ms;
+const milliseconds COIL_AT_TARGET_PUBLISHING_INTERVAL = 300ms;
 
 class StimulationAllower : public rclcpp::Node {
 
@@ -19,41 +19,28 @@ public:
   StimulationAllower() : Node("stimulation_allower") {
 
     /* Create QoS profile for 'coil at target' subscriber. */
-    const uint64_t deadline_ns = static_cast<uint64_t>(std::chrono::nanoseconds(COIL_AT_TARGET_PUBLISHING_INTERVAL).count());
-    const rmw_time_t rmw_deadline = {0, deadline_ns};
-    const rmw_time_t rmw_lifespan = rmw_deadline;
-
-    const rmw_qos_profile_t qos_profile = {
-        RMW_QOS_POLICY_HISTORY_KEEP_LAST,
-        1,
-        RMW_QOS_POLICY_RELIABILITY_RELIABLE,
-        RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL,
-        rmw_deadline,
-        rmw_lifespan,
-        RMW_QOS_POLICY_LIVELINESS_SYSTEM_DEFAULT,
-        RMW_QOS_LIVELINESS_LEASE_DURATION_DEFAULT,
-        false
-    };
-    auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, qos_profile.depth), qos_profile);
+    auto qos = rclcpp::QoS(rclcpp::KeepLast(1))
+        .reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE)
+        .durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL)
+        .deadline(std::chrono::nanoseconds(COIL_AT_TARGET_PUBLISHING_INTERVAL))
+        .liveliness(RMW_QOS_POLICY_LIVELINESS_SYSTEM_DEFAULT);
 
     rclcpp::SubscriptionOptions subscription_options;
     subscription_options.event_callbacks.deadline_callback = [this]([[maybe_unused]] rclcpp::QOSDeadlineRequestedInfo & event) -> void {
-      RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 1000, "'Coil at target' message not received within deadline, disallowing stimulation.");
+      RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 1000, "'Coil at target' message was not received within deadline. Disallowing stimulation.");
 
       this->allow_stimulation(false);
     };
 
     /* Create subscriber and service client. */
     subscription_ = this->create_subscription<std_msgs::msg::Bool>(
-      "/neuronavigation/coil_at_target", qos, std::bind(&StimulationAllower::coil_at_target_callback, this, _1));
+      "/neuronavigation/coil_at_target", qos, std::bind(&StimulationAllower::coil_at_target_callback, this, _1), subscription_options);
 
     client_ = this->create_client<mtms_device_interfaces::srv::AllowStimulation>("/mtms_device/allow_stimulation");
   }
 
 private:
   void allow_stimulation(bool value) {
-    RCLCPP_INFO(this->get_logger(), "%s stimulation.", value ? "Allowing" : "Disallowing");
-
     auto request = std::make_shared<mtms_device_interfaces::srv::AllowStimulation::Request>();
     request->allow_stimulation = value;
 
@@ -68,7 +55,11 @@ private:
   }
 
   void coil_at_target_callback(const std_msgs::msg::Bool::SharedPtr msg) {
-    allow_stimulation(msg->data);
+    auto value = msg->data;
+
+    RCLCPP_INFO(this->get_logger(), "%s stimulation.", value ? "Allowing" : "Disallowing");
+
+    allow_stimulation(value);
   }
   rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr subscription_;
   rclcpp::Client<mtms_device_interfaces::srv::AllowStimulation>::SharedPtr client_;
