56bde0b8bb9bffa73a98bd84e4b87b84006643fc
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-09-24 17:00:21 +0200

    MOD: Frontend: Format time more nicely

--- front/src/components/SystemState.tsx
+++ front/src/components/SystemState.tsx
@@ -51,6 +51,20 @@ export const SystemState = ({ systemState }: Props) => {
     }
   }
 
+  const formatDate = (isoString: any) => {
+    const date = new Date(isoString)
+
+    const year = date.getUTCFullYear()
+    const month = String(date.getUTCMonth() + 1).padStart(2, '0')
+    const day = String(date.getUTCDate()).padStart(2, '0')
+
+    const hours = String(date.getUTCHours()).padStart(2, '0')
+    const minutes = String(date.getUTCMinutes()).padStart(2, '0')
+    const seconds = String(date.getUTCSeconds()).padStart(2, '0')
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+  }
+
   return (
     <div>
       <StateRow>
@@ -64,7 +78,7 @@ export const SystemState = ({ systemState }: Props) => {
       <br />
       <StateRow>
         <StateTitle>Time</StateTitle>
-        <StateValue>{latestUpdate?.toISOString()}</StateValue>
+        <StateValue>{latestUpdate ? formatDate(latestUpdate.toISOString()) : ''}</StateValue>
       </StateRow>
       <StateRow>
         <StateTitle>System time</StateTitle>
