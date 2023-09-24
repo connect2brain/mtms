12b17c8312ea9ba8f1bbb8ddf5d9a2e1c4228168
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-09-24 16:40:56 +0200

    MOD: Style start/stop device and session buttons in frontend

--- front/src/components/SessionControl.tsx
+++ front/src/components/SessionControl.tsx
@@ -62,4 +62,27 @@ export const SessionControl = ({ deviceState, sessionState }: Props) => {
   )
 }
 
-const StyledButton = styled.button``
+const StyledButton = styled.button`
+  width: 200px;
+  height: 50px;
+
+  font-size: 1.00rem;
+  padding: 0.8rem 0.5rem;
+  margin-bottom: 0.8rem;
+  border: none;
+  border-radius: 5px;
+  background-color: #007BFF;
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+  &:disabled {
+    background-color: #CCCCCC;
+    color: #888888;
+  }
+  &:hover:disabled {
+    background-color: #CCCCCC;
+  }
+`
