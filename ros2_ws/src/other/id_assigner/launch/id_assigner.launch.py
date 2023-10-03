eb52703237b6f608af5e7dcf427af7b9eba6d3e9
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-10-03 09:53:25 +0200

    ADD: ID assigner node to get next unique ID

---
+++ ros2_ws/src/other/id_assigner/launch/id_assigner.launch.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    log_arg = DeclareLaunchArgument(
+        "log-level",
+        description="Logging level",
+    )
+
+    logger = LaunchConfiguration("log-level")
+
+    node_executables = [
+        "id_assigner",
+    ]
+
+    for node_executable in node_executables:
+        node = Node(
+            package="id_assigner",
+            executable=node_executable,
+            arguments=['--ros-args', '--log-level', logger]
+        )
+        ld.add_action(node)
+
+    ld.add_action(log_arg)
+
+    return ld
