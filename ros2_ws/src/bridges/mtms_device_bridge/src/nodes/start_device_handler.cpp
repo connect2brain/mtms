ed854bae4e91ca05941031f4ee2123e36e0241d5
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-10-05 10:36:05 +0200

    MOD: Handle powering device on and off properly in mTMS device bridge

--- ros2_ws/src/bridges/mtms_device_bridge/src/nodes/start_device_handler.cpp
+++ ros2_ws/src/bridges/mtms_device_bridge/src/nodes/start_device_handler.cpp
@@ -28,7 +28,6 @@ private:
 };
 
 int main(int argc, char **argv) {
-  init_fpga();
   rclcpp::init(argc, argv);
 
 #if defined(ON_UNIX) && defined(SCHEDULING_OPTIMIZATION)
@@ -46,8 +45,15 @@ int main(int argc, char **argv) {
 
   RCLCPP_INFO(rclcpp::get_logger("start_device_handler"), "Start device handler ready.");
 
-  rclcpp::spin(node);
-  rclcpp::shutdown();
+  init_fpga();
 
+  while (rclcpp::ok()) {
+    if (!is_fpga_ok()) {
+      close_fpga();
+      init_fpga();
+    }
+    rclcpp::spin_some(node);
+  }
   close_fpga();
+  rclcpp::shutdown();
 }
