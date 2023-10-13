27035dd11f4d064a456bb6a943db88b3aa838561
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-10-13 20:09:05 +0300

    MOD: Use ValidatedInput for selecting trigger delay in front-end

--- front/src/components/ValidatedInput.tsx
+++ front/src/components/ValidatedInput.tsx
@@ -43,7 +43,7 @@ export const ValidatedInput: React.FC<ValidatedInputProps> = ({
     if (isNaN(numericValue)) {
       numericValue = defaultValue
     }
-    
+
     // Boundary checks
     if (min !== undefined && numericValue < min) {
       numericValue = min
