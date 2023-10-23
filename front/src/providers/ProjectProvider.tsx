074ab419249770ff594c9ab4aafec35294ab6a5b
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-10-23 16:45:36 +0300

    ADD: Rudimentary pipeline view to front-end

---
+++ front/src/providers/ProjectProvider.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect, ReactNode } from 'react'
+import { Topic, Message } from 'roslib'
+
+import { ros } from 'services/ros'
+
+interface RosString extends ROSLIB.Message {
+  data: string
+}
+
+interface ProjectContextType {
+  activeProject: string
+}
+
+const defaultProjectState: ProjectContextType = {
+  activeProject: ''
+}
+
+export const ProjectContext = React.createContext<ProjectContextType>(defaultProjectState)
+
+interface ProjectProviderProps {
+  children: ReactNode
+}
+
+export const ProjectProvider: React.FC<ProjectProviderProps> = ({ children }) => {
+  const [activeProject, setActiveProject] = useState<string>('')
+
+  useEffect(() => {
+    const activeProjectSubscriber = new Topic({
+      ros: ros,
+      name: '/projects/active',
+      messageType: 'std_msgs/String'
+    }) as Topic<RosString>
+
+    activeProjectSubscriber.subscribe((message) => {
+      setActiveProject(message.data)
+    })
+
+    return () => {
+      activeProjectSubscriber.unsubscribe()
+    }
+  }, [])
+
+  return (
+    <ProjectContext.Provider value={{ activeProject }}>
+      {children}
+    </ProjectContext.Provider>
+  )
+}
