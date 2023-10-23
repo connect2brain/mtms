1fda1b4f424b73d1a2bf06533d2cd18cb95c145a
Oula Siljamo <oula.siljamo@aalto.fi>
2023-10-23 10:41:36 +0300

    ADD: Base for mTMS simulator

---
+++ ros2_ws/src/simulators/mtms_simulator/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'mtms_simulator'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='connect2brain',
+    maintainer_email='connect2brain@aalto.fi',
+    description='mTMS device simulator',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mtms_simulator = mtms_simulator.mtms_simulator:main'
+        ],
+    },
+)
