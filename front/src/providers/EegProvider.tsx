57c3e9689585e03ae1a27fbcc9d03641d360a0fa
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-10-27 15:11:29 +0300

    ADD: Git add missing EegProvider.tsx

---
+++ front/src/providers/EegProvider.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, ReactNode } from 'react'
+import { Topic, Message } from 'roslib'
+
+import { ros } from 'ros/ros'
+
+interface EegInfo extends Message {
+  sampling_frequency: number
+  num_of_eeg_channels: number
+  num_of_emg_channels: number
+  send_trigger_as_channel: boolean
+}
+
+interface EegContextType {
+  eegInfo: EegInfo | null
+}
+
+const defaultEegState: EegContextType = {
+  eegInfo: null
+}
+
+export const EegContext = React.createContext<EegContextType>(defaultEegState)
+
+interface EegProviderProps {
+  children: ReactNode
+}
+
+export const EegProvider: React.FC<EegProviderProps> = ({ children }) => {
+  const [eegInfo, setEegInfo] = useState<EegInfo | null>(null)
+
+  useEffect(() => {
+    const eegInfoSubscriber = new Topic<EegInfo>({
+      ros: ros,
+      name: '/eeg/info',
+      messageType: 'eeg_interfaces/EegInfo',
+    })
+    
+    eegInfoSubscriber.subscribe((message) => {
+      setEegInfo(message)
+      console.log(message)
+    })
+
+    return () => {
+      eegInfoSubscriber.unsubscribe()
+    }
+  }, [])
+
+  return (
+    <EegContext.Provider value={{ eegInfo }}>
+      {children}
+    </EegContext.Provider>
+  )
+}
