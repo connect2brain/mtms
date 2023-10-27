77c70751a38edaa04ac56b103c0b657386f9cd29
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-10-27 14:28:33 +0300

    ADD: Support 'disabled' prop in ToggleSwitch in front-end

--- front/src/components/Experiment/ToggleSwitch.tsx
+++ front/src/components/Experiment/ToggleSwitch.tsx
@@ -7,25 +7,27 @@ type ToggleProps = {
     onLabel?: string
     offLabel?: string
     checked?: boolean
+    disabled?: boolean
     onChange?: (checked: boolean) => void
 }
 
 let idCounter = 0
 
-export const ToggleSwitch: React.FC<ToggleProps> = ({ type, onLabel = '', offLabel = '', checked = false, onChange }) => {
+export const ToggleSwitch: React.FC<ToggleProps> = ({ type, onLabel = '', offLabel = '', checked = false, disabled = false, onChange }) => {
     const inputId = `toggle-${type}-${idCounter}`
     idCounter++
 
     return (
         <div className="tg-list-item">
-            <input
-                className={`tgl tgl-${type}`}
-                id={inputId}
-                type="checkbox"
-                checked={checked}
-                onChange={e => onChange && onChange(e.target.checked)}
-            />
-            <label className="tgl-btn" data-tg-off={offLabel} data-tg-on={onLabel} htmlFor={inputId}></label>
+          <input
+            className={`tgl tgl-${type}`}
+            id={inputId}
+            type="checkbox"
+            checked={checked}
+            disabled={disabled}
+            onChange={e => onChange && onChange(e.target.checked)}
+          />
+          <label className="tgl-btn" data-tg-off={offLabel} data-tg-on={onLabel} htmlFor={inputId}></label>
         </div>
     )
 }
