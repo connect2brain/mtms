7cf02df4369bc261d61b942af382065797e8b378
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-10-30 17:26:47 +0200

    MOD: Rename preprocessor_bindings to cpp_bindings
    To have identical name with other pipeline stages, making it
    easier to take a module from one pipeline stage and adapt it
    to another.

--- ros2_ws/src/pipeline/preprocessor/src/cpp_bindings.cpp
+++ ros2_ws/src/pipeline/preprocessor/src/cpp_bindings.cpp
@@ -11,6 +11,6 @@ void PreprocessorWrapper::log(const std::string& message) {
   RCLCPP_INFO(*logger_ptr, "From Python: %s", message.c_str());
 }
 
-PYBIND11_MODULE(preprocessor_bindings, m) {
+PYBIND11_MODULE(cpp_bindings, m) {
     m.def("log", &PreprocessorWrapper::log);
 }
