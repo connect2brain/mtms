ff3692d8ed14b807e8e1608819600ea369d82f6d
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-10-30 18:27:18 +0200

    ADD: Initial version of decider node

---
+++ ros2_ws/src/pipeline/decider/src/decider_wrapper.h
@@ -0,0 +1,66 @@
+#ifndef DECIDER_WRAPPER_H
+#define DECIDER_WRAPPER_H
+
+#include <pybind11/pybind11.h>
+#include <pybind11/embed.h>
+#include <pybind11/numpy.h>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "eeg_interfaces/msg/eeg_sample.hpp"
+#include "eeg_interfaces/msg/preprocessed_eeg_sample.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include "ring_buffer.h"
+
+namespace py = pybind11;
+
+class DeciderWrapper {
+public:
+  DeciderWrapper(rclcpp::Logger& logger);
+  ~DeciderWrapper();
+
+  void reset_module(
+      const std::string& directory,
+      const std::string& module_name,
+      const size_t eeg_data_size,
+      const size_t emg_data_size,
+      const uint16_t sampling_frequency);
+
+  std::pair<bool, bool> process(
+    const RingBuffer<std::shared_ptr<eeg_interfaces::msg::PreprocessedEegSample>>& buffer,
+    double_t sample_time);
+
+  bool is_initialized() const;
+  std::size_t get_buffer_size() const;
+
+  /* Exposed to Python, defined in cpp_bindings.cpp. */
+  static void log(const std::string& message);
+
+private:
+  /* XXX: Have a static ROS2 logger to expose it more easily to the Python side (see cpp_bindings.cpp). */
+  static rclcpp::Logger* logger_ptr;
+
+  bool _is_initialized;
+
+  py::module decider_module;
+  py::object decider_instance;
+
+  std::unique_ptr<py::scoped_interpreter> guard;
+
+  std::unique_ptr<py::array_t<double>> py_timestamps;
+  std::unique_ptr<py::array_t<double>> py_eeg_data;
+  std::unique_ptr<py::array_t<double>> py_emg_data;
+
+  int earliest_sample;
+  int latest_sample;
+  uint16_t sampling_frequency;
+
+  std::size_t buffer_size = 0;
+  std::size_t eeg_data_size;
+  std::size_t emg_data_size;
+};
+
+#endif
---
+++ ros2_ws/src/pipeline/decider/src/ring_buffer.h
@@ -0,0 +1,64 @@
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+
+#include <vector>
+#include <stdexcept>
+
+#include "eeg_interfaces/msg/eeg_sample.hpp"
+
+#include <vector>
+#include <stdexcept>
+
+template<typename T>
+class RingBuffer {
+private:
+  std::vector<T> buffer;
+  size_t capacity;
+  size_t current_size;
+  size_t head;
+  size_t tail;
+
+public:
+  RingBuffer() : capacity(0), current_size(0), head(0), tail(0) {}
+
+  /* Reset buffer and set its size. Discards all current data. */
+  void reset(size_t size) {
+    buffer.resize(size);
+
+    capacity = size;
+    current_size = 0;
+    head = 0;
+    tail = 0;
+  }
+
+  /* Appends a message to the buffer, overwriting the oldest message if buffer is full. */
+  void append(const T& msg) {
+    buffer[tail] = msg;
+    tail = (tail + 1) % capacity;
+
+    if (current_size < capacity) {
+      current_size++;
+    } else {
+      head = (head + 1) % capacity;
+    }
+  }
+
+  /* Returns all the messages in the buffer as a vector. */
+  std::vector<T> get_buffer() const {
+    std::vector<T> output;
+    output.reserve(current_size);
+
+    for (size_t i = 0; i < current_size; ++i) {
+      output.push_back(buffer[(head + i) % capacity]);
+    }
+
+    return output;
+  }
+
+  /* Queries if the buffer is full. */
+  bool is_full() const {
+    return current_size == capacity;
+  }
+};
+
+#endif // RING_BUFFER_H
