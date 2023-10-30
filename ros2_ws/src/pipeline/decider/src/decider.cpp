4187cfdfdef73475270a50b5eabe54086f59ad77
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-10-30 18:27:18 +0200

    ADD: Initial version of decider node

---
+++ ros2_ws/src/pipeline/decider/src/decider.cpp
@@ -0,0 +1,341 @@
+#include <chrono>
+#include <filesystem>
+
+#include "decider_wrapper.h"
+#include "decider.h"
+
+#include "memory_utils.h"
+#include "scheduling_utils.h"
+
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::placeholders;
+
+const std::string EEG_INFO_TOPIC = "/eeg/info";
+const std::string EEG_PREPROCESSED_TOPIC = "/eeg/preprocessed";
+
+const std::string PROJECTS_DIRECTORY = "projects/";
+
+EegDecider::EegDecider() : Node("decider") {
+  /* Subscriber for EEG info. */
+  auto qos_persist_latest = rclcpp::QoS(rclcpp::KeepLast(1))
+        .reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE)
+        .durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL)
+        .history(RMW_QOS_POLICY_HISTORY_KEEP_LAST);
+
+  this->eeg_info_subscriber = this->create_subscription<eeg_interfaces::msg::EegInfo>(
+    EEG_INFO_TOPIC,
+    qos_persist_latest,
+    std::bind(&EegDecider::update_eeg_info, this, _1));
+
+  /* Subscriber for preprocessed EEG data. */
+  auto preprocessed_eeg_subscriber_callback = [this](const std::shared_ptr<eeg_interfaces::msg::PreprocessedEegSample> msg) -> void {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    RCLCPP_INFO_THROTTLE(this->get_logger(),
+                         *this->get_clock(),
+                         1000,
+                         "Received preprocessed EEG datapoint on topic %s with timestamp %.4f.",
+                         EEG_PREPROCESSED_TOPIC.c_str(),
+                         msg->time);
+
+    this->process_eeg_sample(msg);
+
+    /* Print the time taken to preprocess the datapoint. */
+
+    auto finish = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = finish - start;
+
+    RCLCPP_DEBUG(this->get_logger(),
+                 "Time taken to process EEG datapoint: %.3f ms.",
+                 1000 * elapsed.count());
+  };
+
+  this->preprocessed_eeg_subscriber = create_subscription<eeg_interfaces::msg::PreprocessedEegSample>(
+    EEG_PREPROCESSED_TOPIC,
+    /* TODO: Should the queue be 1 samples long to make it explicit if we are too slow? */
+    5000,
+    preprocessed_eeg_subscriber_callback);
+
+  RCLCPP_INFO(this->get_logger(), "Listening to EEG data on topic %s.", EEG_PREPROCESSED_TOPIC.c_str());
+
+  /* Subscriber for active project. */
+  this->active_project_subscriber = create_subscription<std_msgs::msg::String>(
+    "/projects/active",
+    qos_persist_latest,
+    std::bind(&EegDecider::set_active_project, this, _1));
+
+  /* Publisher for listing deciders. */
+  this->decider_list_publisher = this->create_publisher<project_interfaces::msg::DeciderList>(
+    "/pipeline/decider/list",
+    qos_persist_latest);
+
+  /* Service for changing decider. */
+  this->set_decider_service = this->create_service<project_interfaces::srv::SetDecider>(
+    "/pipeline/decider/set",
+    std::bind(&EegDecider::set_decider, this, _1, _2));
+
+  /* Service for enabling and disabling decider. */
+  this->set_decider_enabled_service = this->create_service<project_interfaces::srv::SetDeciderEnabled>(
+    "/pipeline/decider/enabled/set",
+    std::bind(&EegDecider::set_decider_enabled, this, _1, _2));
+
+  /* Publisher for decider enabled message. */
+  this->decider_enabled_publisher = this->create_publisher<std_msgs::msg::Bool>(
+    "/pipeline/decider/enabled",
+    qos_persist_latest);
+
+  /* Publisher for event trigger. */
+  this->event_trigger_publisher = this->create_publisher<event_interfaces::msg::EventTrigger>(
+    "/event/send/event_trigger",
+    10);
+
+  /* Initialize variables. */
+  rclcpp::Logger logger = rclcpp::get_logger("decider_wrapper");
+  this->decider_wrapper = std::make_unique<DeciderWrapper>(logger);
+
+  this->sample_buffer = RingBuffer<std::shared_ptr<eeg_interfaces::msg::PreprocessedEegSample>>();
+}
+
+/* Listing and setting EEG deciders. */
+
+void EegDecider::set_decider_enabled(
+      const std::shared_ptr<project_interfaces::srv::SetDeciderEnabled::Request> request,
+      std::shared_ptr<project_interfaces::srv::SetDeciderEnabled::Response> response) {
+
+  bool enabled = request->enabled;
+
+  /* Update local state variable. */
+  this->decider_enabled = enabled;
+
+  /* Update ROS state variable. */
+  auto msg = std_msgs::msg::Bool();
+  msg.data = enabled;
+
+  this->decider_enabled_publisher->publish(msg);
+
+  /* Reset sample buffer when enabling decider to avoid using remains of old EEG data. */
+  if (enabled) {
+    reset_sample_buffer();
+  }
+
+  RCLCPP_INFO(this->get_logger(), "%s decider.", enabled ? "Enabling" : "Disabling");
+
+  response->success = true;
+}
+
+void EegDecider::reset_decider_module() {
+  if (this->script_directory == UNSET_STRING ||
+      this->module_name == UNSET_STRING ||
+      this->num_of_eeg_channels == UNSET_NUM_OF_CHANNELS ||
+      this->num_of_emg_channels == UNSET_NUM_OF_CHANNELS ||
+      this->sampling_frequency == UNSET_SAMPLING_FREQUENCY) {
+
+    return;
+  }
+  this->decider_wrapper->reset_module(
+    this->script_directory,
+    this->module_name,
+    this->num_of_eeg_channels,
+    this->num_of_emg_channels,
+    this->sampling_frequency);
+}
+
+void EegDecider::set_decider(
+      const std::shared_ptr<project_interfaces::srv::SetDecider::Request> request,
+      std::shared_ptr<project_interfaces::srv::SetDecider::Response> response) {
+
+  this->module_name = request->decider;
+
+  RCLCPP_INFO(this->get_logger(), "Setting decider to: %s.", this->module_name.c_str());
+
+  /* Reset the wrapper to use the changed decider module. */
+  reset_decider_module();
+
+  /* We don't want left-over samples from the previous decider, hence
+     reset the sample buffer. */
+  reset_sample_buffer();
+
+  response->success = true;
+}
+
+/* Reset sample buffer to the size determined by the Python module. */
+void EegDecider::reset_sample_buffer() {
+  size_t buffer_size = this->decider_wrapper->get_buffer_size();
+  this->sample_buffer.reset(buffer_size);
+
+  RCLCPP_DEBUG(this->get_logger(), "Sample buffer reset to %lu elements.", buffer_size);
+}
+
+void EegDecider::set_active_project(const std::shared_ptr<std_msgs::msg::String> msg) {
+  this->active_project = msg->data;
+
+  std::ostringstream oss;
+  oss << PROJECTS_DIRECTORY << this->active_project << "/decider";
+  this->script_directory = oss.str();
+
+  RCLCPP_INFO(this->get_logger(), "Active project set to: %s.", this->active_project.c_str());
+
+  update_decider_list();
+}
+
+std::vector<std::string> EegDecider::list_python_scripts(const std::string& path) {
+  std::vector<std::string> scripts;
+
+  /* Check that the directory exists. */
+  if (!std::filesystem::exists(path) || !std::filesystem::is_directory(path)) {
+    std::cerr << "Warning: Directory does not exist: " << path << std::endl;
+    return scripts;
+  }
+
+  /* List all .py files in the directory. */
+  for (const auto &entry : std::filesystem::directory_iterator(path)) {
+    if (entry.is_regular_file() && entry.path().extension() == ".py") {
+      scripts.push_back(entry.path().stem().string());
+    }
+  }
+  return scripts;
+}
+
+void EegDecider::update_decider_list() {
+  auto scripts = this->list_python_scripts(this->script_directory);
+
+  auto msg = project_interfaces::msg::DeciderList();
+  msg.scripts = scripts;
+
+  this->decider_list_publisher->publish(msg);
+}
+
+/* EEG functions */
+
+void EegDecider::update_eeg_info(const std::shared_ptr<eeg_interfaces::msg::EegInfo> msg) {
+  this->sampling_frequency = msg->sampling_frequency;
+  this->num_of_eeg_channels = msg->num_of_eeg_channels;
+  this->num_of_emg_channels = msg->num_of_emg_channels;
+
+  this->sampling_period = 1.0 / this->sampling_frequency;
+
+  RCLCPP_INFO(this->get_logger(), " ");
+  RCLCPP_INFO(this->get_logger(), "Eeg configuration");
+  RCLCPP_INFO(this->get_logger(), " ");
+  RCLCPP_INFO(this->get_logger(), "  - Sampling frequency: %d Hz", this->sampling_frequency);
+  RCLCPP_INFO(this->get_logger(), "  - # of EEG channels: %d", this->num_of_eeg_channels);
+  RCLCPP_INFO(this->get_logger(), "  - # of EMG channels: %d", this->num_of_emg_channels);
+  RCLCPP_INFO(this->get_logger(), " ");
+
+  /* The number of EEG and EMG channels may have changed, therefore reset decider Python module. */
+  reset_decider_module();
+
+  /* EEG info is updated if streaming is restarted on the EEG device. We don't want
+     left-over samples from the previous run, therefore reset the sample buffer. */
+  reset_sample_buffer();
+}
+
+/* XXX: Very close to a similar check in eeg_gatherer.cpp and other pipeline stages. Unify? */
+void EegDecider::check_dropped_samples(double_t sample_time) {
+  if (this->sampling_frequency == UNSET_SAMPLING_FREQUENCY) {
+    RCLCPP_WARN(this->get_logger(), "Sampling frequency not received, cannot check for dropped samples.");
+  }
+
+  if (this->sampling_frequency != UNSET_SAMPLING_FREQUENCY &&
+      this->previous_time) {
+
+    auto time_diff = sample_time - this->previous_time;
+    auto threshold = this->sampling_period + this->TOLERANCE_S;
+
+    if (time_diff > threshold) {
+      /* Err if sample(s) were dropped. */
+      RCLCPP_ERROR(this->get_logger(),
+          "Sample(s) dropped. Time difference between consecutive samples: %.5f, should be: %.5f, limit: %.5f", time_diff, this->sampling_period, threshold);
+
+    } else {
+      /* If log-level is set to DEBUG, print time difference for all samples, regardless of if samples were dropped or not. */
+      RCLCPP_DEBUG(this->get_logger(),
+        "Time difference between consecutive samples: %.5f", time_diff);
+    }
+  }
+  this->previous_time = sample_time;
+}
+
+void EegDecider::process_eeg_sample(const std::shared_ptr<eeg_interfaces::msg::PreprocessedEegSample> msg) {
+  auto start_time = std::chrono::high_resolution_clock::now();
+
+  double_t sample_time = msg->time;
+
+  check_dropped_samples(sample_time);
+
+  if (!this->decider_enabled) {
+    RCLCPP_INFO_THROTTLE(this->get_logger(),
+                         *this->get_clock(),
+                         1000,
+                         "Decider not enabled");
+    return;
+  }
+
+  if (!this->decider_wrapper->is_initialized()) {
+    RCLCPP_INFO_THROTTLE(this->get_logger(),
+                         *this->get_clock(),
+                         1000,
+                         "Decider enabled but not selected");
+    return;
+  }
+
+  this->sample_buffer.append(msg);
+
+  if (!this->sample_buffer.is_full()) {
+    return;
+  }
+
+  auto [send_trigger, success] = this->decider_wrapper->process(
+    this->sample_buffer,
+    sample_time);
+
+  if (success) {
+    /* Measure the decision time for the buffer. */
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    /* TODO: Unused at the moment. */
+    double_t decision_time = std::chrono::duration<double_t>(end_time - start_time).count();
+
+    /* Send trigger if desired. */
+    if (send_trigger) {
+      RCLCPP_INFO(this->get_logger(), "Sending trigger at time %.3f (s).", sample_time);
+      auto msg = event_interfaces::msg::EventTrigger();
+      this->event_trigger_publisher->publish(msg);
+    }
+
+    RCLCPP_INFO_THROTTLE(this->get_logger(),
+                         *this->get_clock(),
+                          1000,
+                          "Processed EEG sample at time %.3f (s).",
+                          sample_time);
+  } else {
+    RCLCPP_ERROR_THROTTLE(this->get_logger(),
+                          *this->get_clock(),
+                          1000,
+                          "Python call failed, not processing EEG sample at time %.3f (s).",
+                          sample_time);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+
+#if defined(ON_UNIX) && defined(SCHEDULING_OPTIMIZATION)
+  RCLCPP_INFO(rclcpp::get_logger("decider"), "Setting thread scheduling");
+  set_thread_scheduling(pthread_self(), DEFAULT_SCHEDULING_POLICY, DEFAULT_REALTIME_SCHEDULING_PRIORITY);
+#endif
+
+  auto node = std::make_shared<EegDecider>();
+
+#if defined(ON_UNIX) && defined(MEMORY_OPTIMIZATION)
+  RCLCPP_INFO(rclcpp::get_logger("decider"), "Locking memory");
+  lock_memory();
+  preallocate_memory(1024 * 1024 * 10); //10 MB
+#endif
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+
+  return 0;
+}
---
+++ ros2_ws/src/pipeline/decider/src/decider_wrapper.cpp
@@ -0,0 +1,170 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/embed.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
+
+#include "decider_wrapper.h"
+#include <eeg_interfaces/msg/eeg_sample.hpp>
+
+namespace py = pybind11;
+
+DeciderWrapper::DeciderWrapper(rclcpp::Logger& logger) {
+  logger_ptr = &logger;
+  _is_initialized = false;
+  guard = std::make_unique<py::scoped_interpreter>();
+}
+
+void DeciderWrapper::reset_module(
+    const std::string& directory,
+    const std::string& module_name,
+    const size_t eeg_data_size,
+    const size_t emg_data_size,
+    const uint16_t sampling_frequency) {
+
+  this->sampling_frequency = sampling_frequency;
+
+  decider_module.release();
+  decider_instance.release();
+
+  /* Set the sys.path to include the directory of the module. */
+  py::module sys_module = py::module::import("sys");
+  py::list sys_path = sys_module.attr("path");
+  sys_path.append(directory);
+
+  /* Remove the module from sys.modules if it exists, to ensure it is reloaded. */
+  py::dict sys_modules = sys_module.attr("modules");
+  if (sys_modules.contains(module_name.c_str())) {
+    sys_modules.attr("__delitem__")(module_name.c_str());
+  }
+
+  /* Import the module and initialize the Decider instance. */
+
+  try {
+    decider_module = py::module::import(module_name.c_str());
+    decider_instance = decider_module.attr("Decider")(eeg_data_size, emg_data_size, sampling_frequency);
+
+  } catch(const py::error_already_set& e) {
+    RCLCPP_ERROR(*logger_ptr, "Python error: %s", e.what());
+    return;
+
+  } catch(const std::exception& e) {
+    RCLCPP_ERROR(*logger_ptr, "C++ error: %s", e.what());
+    return;
+  }
+
+  /* Extract the sample_window from decider_instance. */
+  if (py::hasattr(decider_instance, "sample_window")) {
+    py::list sample_window = decider_instance.attr("sample_window").cast<py::list>();
+    if (sample_window.size() == 2) {
+      this->earliest_sample = sample_window[0].cast<int>();
+      this->latest_sample = sample_window[1].cast<int>();
+
+      this->buffer_size = this->latest_sample - this->earliest_sample + 1;
+    } else {
+      RCLCPP_WARN(*logger_ptr, "sample_window class attribute is of incorrect length (should be two elements).");
+    }
+  } else {
+    RCLCPP_WARN(*logger_ptr, "sample_window class attribute not defined by the decider.");
+  }
+
+  RCLCPP_INFO(*logger_ptr, "Decider set to: %s.", module_name.c_str());
+  RCLCPP_INFO(*logger_ptr, " ");
+  RCLCPP_INFO(*logger_ptr, "Decider configuration");
+  RCLCPP_INFO(*logger_ptr, " ");
+  RCLCPP_INFO(*logger_ptr, "  - Sample window: [%d, %d]", this->earliest_sample, this->latest_sample);
+  RCLCPP_INFO(*logger_ptr, " ");
+
+  /* Initialize numpy arrays. */
+  py_timestamps = std::make_unique<py::array_t<double>>(buffer_size);
+
+  std::vector<size_t> eeg_data_shape = {buffer_size, eeg_data_size};
+  py_eeg_data = std::make_unique<py::array_t<double>>(eeg_data_shape);
+
+  std::vector<size_t> emg_data_shape = {buffer_size, emg_data_size};
+  py_emg_data = std::make_unique<py::array_t<double>>(emg_data_shape);
+
+  this->eeg_data_size = eeg_data_size;
+  this->emg_data_size = emg_data_size;
+
+  this->_is_initialized = true;
+}
+
+DeciderWrapper::~DeciderWrapper() {
+  py_timestamps.reset();
+  py_eeg_data.reset();
+  py_emg_data.reset();
+}
+
+bool DeciderWrapper::is_initialized() const {
+  return this->_is_initialized;
+}
+
+std::size_t DeciderWrapper::get_buffer_size() const {
+  return this->buffer_size;
+}
+
+std::pair<bool, bool>DeciderWrapper::process(
+    const RingBuffer<std::shared_ptr<eeg_interfaces::msg::PreprocessedEegSample>>& buffer,
+    double_t sample_time) {
+
+  /* TODO: The logic below, as well as the difference in semantics between "sample time" and "current time", needs to
+     be documented somewhere more thoroughly. */
+
+  /* An example: If the sample window is set to [-2, 2], the earliest sample will be -2, and the current sample,
+     i.e., the sample corresponding to 0, will have the index 2, hence the calculation below. */
+  int current_sample_index = -this->earliest_sample;
+
+  /* An example: If sample time is 5.0 and the sampling frequency is 5 kHz, and we have a sample window [-2, 2]
+     (the latest sample being 2), the sample 2 in the sample window corresponds to the time 5.0. Hence, sample 0
+     corresponds to the time 5.0 s - 2 / (5000 Hz) = 4.9996 s. */
+  double_t current_time = sample_time - (double)this->latest_sample / this->sampling_frequency;
+
+  /* Fill the numpy arrays. */
+  auto timestamps_ptr = py_timestamps->mutable_data();
+  auto eeg_data_ptr = py_eeg_data->mutable_data();
+  auto emg_data_ptr = py_emg_data->mutable_data();
+
+  for (const auto& sample_ptr : buffer.get_buffer()) {
+    const auto& sample = *sample_ptr;
+
+    *timestamps_ptr++ = sample.time - current_time;
+    std::memcpy(eeg_data_ptr, sample.eeg_data.data(), eeg_data_size * sizeof(double));
+    eeg_data_ptr += eeg_data_size;
+    std::memcpy(emg_data_ptr, sample.emg_data.data(), emg_data_size * sizeof(double));
+    emg_data_ptr += emg_data_size;
+  }
+
+  /* Call the Python function. */
+  py::object result;
+  try {
+    result = decider_instance.attr("process")(*py_timestamps, *py_eeg_data, *py_emg_data, current_sample_index);
+
+  } catch(const py::error_already_set& e) {
+    RCLCPP_ERROR(*logger_ptr, "Python error: %s", e.what());
+    return {false, false};
+
+  } catch(const std::exception& e) {
+    RCLCPP_ERROR(*logger_ptr, "C++ error: %s", e.what());
+    return {false, false};
+  }
+
+  /* Validate the return value of the Python function call. */
+  if (!py::isinstance<py::dict>(result)) {
+    RCLCPP_ERROR(*logger_ptr, "Python module should return a dictionary.");
+    return {false, false};
+  }
+
+  py::dict dict_result = result.cast<py::dict>();
+
+  if (!dict_result.contains("send_trigger")) {
+    RCLCPP_ERROR(*logger_ptr, "Python module should return a dictionary with the field: send_trigger.");
+    return {false, false};
+  }
+
+  /* Convert the Python dictionary to a ROS message. */
+  bool send_trigger = dict_result["send_trigger"].cast<bool>();
+
+  return {send_trigger, true};
+}
+
+rclcpp::Logger* DeciderWrapper::logger_ptr = nullptr;
