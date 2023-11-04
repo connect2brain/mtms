559b50ac32bcfc5d138eea98be602b6499f36848
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-11-04 10:57:56 +0200

    ADD: Publish project-specific dataset list in EEG simulator

--- ros2_ws/src/eeg/eeg_simulator/src/eeg_simulator.h
+++ ros2_ws/src/eeg/eeg_simulator/src/eeg_simulator.h
@@ -11,13 +11,27 @@
 #include "eeg_interfaces/msg/eeg_info.hpp"
 #include "eeg_interfaces/msg/trigger.hpp"
 
+#include "std_msgs/msg/string.hpp"
+
+#include "project_interfaces/msg/dataset.hpp"
+#include "project_interfaces/msg/dataset_list.hpp"
+
+
 class EegSimulator : public rclcpp::Node {
 public:
   EegSimulator();
+  ~EegSimulator();
 
 private:
+  std::vector<std::pair<std::string, std::string>> list_datasets(const std::string& path);
+  void update_dataset_list();
+  void handle_set_active_project(const std::shared_ptr<std_msgs::msg::String> msg);
+
   void publish_sample();
   void publish_trigger();
+
+  void update_inotify_watch();
+  void inotify_timer_callback();
 
   double current_time = 0.0;
   double sampling_period = 0.0;
@@ -32,6 +46,12 @@ private:
   int num_of_emg_channels = 0;
   int total_channels = 0;
   std::ifstream file;
+
+  std::string active_project;
+  std::string dataset_directory;
+
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr active_project_subscriber;
+  rclcpp::Publisher<project_interfaces::msg::DatasetList>::SharedPtr dataset_list_publisher;
 
   rclcpp::Publisher<eeg_interfaces::msg::EegSample>::SharedPtr eeg_publisher_;
   rclcpp::Publisher<eeg_interfaces::msg::Trigger>::SharedPtr trigger_publisher_;
