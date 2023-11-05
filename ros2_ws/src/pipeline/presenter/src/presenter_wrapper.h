ba747bbab8d18a28eefad0e01bd6fa45f4c1fdd1
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-11-06 00:21:13 +0200

    ADD: Initial version of presenter node

---
+++ ros2_ws/src/pipeline/presenter/src/presenter_wrapper.h
@@ -0,0 +1,45 @@
+#ifndef presenter_WRAPPER_H
+#define presenter_WRAPPER_H
+
+#include <pybind11/pybind11.h>
+#include <pybind11/embed.h>
+#include <pybind11/numpy.h>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "pipeline_interfaces/msg/sensory_stimulus.hpp"
+
+namespace py = pybind11;
+
+class PresenterWrapper {
+public:
+  PresenterWrapper(rclcpp::Logger& logger);
+
+  void initialize_module(
+      const std::string& directory,
+      const std::string& module_name);
+
+  void reset_module();
+
+  bool process(pipeline_interfaces::msg::SensoryStimulus& msg);
+
+  bool is_initialized() const;
+
+  /* Exposed to Python, defined in cpp_bindings.cpp. */
+  static void log(const std::string& message);
+
+private:
+  /* XXX: Have a static ROS2 logger to expose it more easily to the Python side (see cpp_bindings.cpp). */
+  static rclcpp::Logger* logger_ptr;
+
+  bool _is_initialized;
+
+  py::module presenter_module;
+  py::object presenter_instance;
+
+  std::unique_ptr<py::scoped_interpreter> guard;
+};
+
+#endif
