bb3767ebe91a7652f6d816c7352ad91b62c893ae
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2023-11-10 16:54:49 +0200

    MOD: Use preprocessed EEG data in EEG gatherer

--- ros2_ws/src/eeg/eeg_gatherer/src/gather_eeg_server.cpp
+++ ros2_ws/src/eeg/eeg_gatherer/src/gather_eeg_server.cpp
@@ -63,14 +63,14 @@ void GatherEegServer::execute(const std::shared_ptr<GoalHandleGatherEeg> goal_ha
 
   auto eeg_gatherer = EegGatherer(goal_id, start_time, end_time, sampling_frequency);
 
-  auto eeg_subscription = this->create_subscription<eeg_interfaces::msg::EegSample>(
-    "/eeg/raw",
+  auto eeg_subscription = this->create_subscription<eeg_interfaces::msg::PreprocessedEegSample>(
+    "/eeg/preprocessed",
     EEG_DATA_SUBSCRIBER_QUEUE_SIZE,
-    [this, &eeg_gatherer](const eeg_interfaces::msg::EegSample::SharedPtr msg) {
+    [this, &eeg_gatherer](const eeg_interfaces::msg::PreprocessedEegSample::SharedPtr msg) {
       RCLCPP_INFO_THROTTLE(rclcpp::get_logger("eeg_gatherer"),
                           *this->get_clock(),
                           1000,
-                          "Received EEG datapoint with timestamp %.4f.",
+                          "Received preprocessed EEG datapoint with timestamp %.4f.",
                           msg->time);
       eeg_gatherer.handle_eeg_sample(msg);
     });
