dcba4ee89b538650a09132e8b9148fb8862ff0e5
Oula Siljamo <oula.siljamo@aalto.fi>
2023-11-10 15:40:19 +0200

    MOD: Refactor mTMS simulator channel behaviour to own class

--- ros2_ws/src/simulators/mtms_simulator/mtms_simulator/mtms_simulator.py
+++ ros2_ws/src/simulators/mtms_simulator/mtms_simulator/mtms_simulator.py
@@ -48,7 +48,10 @@ from event_interfaces.msg import (
     DischargeFeedback,
     TriggerOutFeedback,
     WaveformPhase,
+    WaveformPiece,
 )
+
+from .channel import Channel
 
 
 class MTMSSimulator(Node):
@@ -58,7 +61,7 @@ class MTMSSimulator(Node):
     ros2 node interface described by the mtms_device_bridge packages.
     """
 
-    SYSTEM_STATE_PUBLISHING_INTERVAL_MS = 20
+    SYSTEM_STATE_PUBLISHING_INTERVAL_MS = 400
     SYSTEM_STATE_PUBLISHING_INTERVAL_TOLERANCE_MS = 5
 
     # TODO: Make these values configurable
@@ -149,7 +152,17 @@ class MTMSSimulator(Node):
         # Internal state variables
         self.system_state: SystemState = SystemState()
         self.system_state.channel_states = [
-            ChannelState(channel_index=i) for i in range(self.NUM_OF_CHANNELS)
+            ChannelState(channel_index=i) for i in range(MTMSSimulator.NUM_OF_CHANNELS)
+        ]
+
+        self.channels = [
+            Channel(
+                charge_rate=self.CHARGE_RATE,
+                time_constant=self.TIME_CONSTANT,
+                pulse_discharge_percentage=self.PULSE_DISCHARGE_PERCENTAGE,
+                max_voltage=self.MAX_VOLTAGE,
+            )
+            for _ in range(self.NUM_OF_CHANNELS)
         ]
 
         self.session_start_time = 0.0
@@ -162,15 +175,23 @@ class MTMSSimulator(Node):
         )
 
     def allow_stimulation_handler(self, request, response):
-        self.allow_stimulation = request.allow_stimulation
         self.get_logger().info("Allow stimulation set to %r" % self.allow_stimulation)
+
+        self.allow_stimulation = request.allow_stimulation
+        for c in self.channels:
+            c.allow_stimulation = request.allow_stimulation
+
         response.success = True
         return response
 
     def send_settings_handler(self, request, response):
-        self.settings = request.settings
         self.get_logger().info("Received settings")
-        self.get_logger().debug("Settings: %r" % self.settings)
+        self.get_logger().debug("Settings: %r" % request.settings)
+
+        self.settings = request.settings
+        for c in self.channels:
+            c.settings = self.settings
+
         response.success = True
         return response
 
@@ -205,97 +226,6 @@ class MTMSSimulator(Node):
         self.get_logger().info("Session stopped")
         response.success = True
         return response
-
-    def charge_handler(self, message: Charge) -> None:
-        """
-        Charges the given channel to desired voltage.
-
-        New voltage is not set immediately, and rather models the behaviour of a
-        capacitor to change the voltage. Charging is done with constant power and the
-        required energy to charge to certain voltage increases quadratically.
-
-        Args:
-            message: contains the charge information.
-        """
-        self.get_logger().info("Charge message received: %r" % message)
-
-        # Session operational
-        if self._check_session_started():
-            self.get_logger().warn("Session not started. Can't charge coil.")
-            return
-
-        # Input validation
-        error = self._validate_voltage_update_input(message)
-        if error.value != ChargeError.NO_ERROR:
-            feedback_msg = ChargeFeedback(id=message.event_info.id, error=error)
-            self.charge_feedback_publisher.publish(feedback_msg)
-            return
-
-        # Wait for execution condition
-        event_info: EventInfo = message.event_info
-        self._wait_for_execution_execution_condition(
-            event_info.execution_condition, event_info.execution_time
-        )
-
-        # Update voltage
-        channel_state: ChannelState = self.system_state.channel_states[message.channel]
-
-        current_voltage = channel_state.voltage
-        target_voltage = message.target_voltage
-
-        # TODO: Check the formula and constants
-        t = self.CHARGE_RATE * (target_voltage**2 - current_voltage**2)
-        time.sleep(t)
-        channel_state.voltage = target_voltage
-
-        # Send charge feedback after update ready
-        feedback_msg = ChargeFeedback(id=message.event_info.id, error=error)
-        self.charge_feedback_publisher.publish(feedback_msg)
-
-    def discharge_handler(self, message: Discharge) -> None:
-        """
-        Discharges the given channel to given voltage.
-
-        New voltage is not set immediately, and rather models the behaviour of a
-        capacitor to change the voltage. Discharge follows exponential decay.
-
-        Args:
-            message: contains the discharge information.
-        """
-        self.get_logger().info("Discharge message received: %r" % message)
-
-        # Session operational
-        if self._check_session_started():
-            self.get_logger().warn("Session not started. Can't discharge coil.")
-            return
-
-        # Input validation
-        error = self._validate_voltage_update_input(message)
-        if error.value != DischargeError.NO_ERROR:
-            feedback_msg = DischargeFeedback(id=message.event_info.id, error=error)
-            self.discharge_feedback_publisher.publish(feedback_msg)
-            return
-
-        # Wait for execution condition
-        event_info: EventInfo = message.event_info
-        self._wait_for_execution_execution_condition(
-            event_info.execution_condition, event_info.execution_time
-        )
-
-        # Update voltage
-        channel_state: ChannelState = self.system_state.channel_states[message.channel]
-
-        current_voltage = channel_state.voltage
-        target_voltage = message.target_voltage
-
-        # TODO: Check the formula and constants
-        t = self.TIME_CONSTANT * math.log(current_voltage / target_voltage)
-        time.sleep(t)
-        channel_state.voltage = target_voltage
-
-        # Send charge feedback after update ready
-        feedback_msg = DischargeFeedback(id=message.event_info.id, error=error)
-        self.discharge_feedback_publisher.publish(feedback_msg)
 
     def _check_session_started(self) -> bool:
         return self.system_state.session_state.value != SessionState.STARTED
@@ -364,6 +294,155 @@ class MTMSSimulator(Node):
                     "Execution condition WAIT_FOR_TRIGGER not supported. Doing nothing."
                 )
 
+    def charge_handler(self, message: Charge) -> None:
+        """
+        Charges the given channel to desired voltage.
+
+        New voltage is not set immediately, and rather models the behaviour of a
+        capacitor to change the voltage. Charging is done with constant power and the
+        required energy to charge to certain voltage increases quadratically.
+
+        Args:
+            message: contains the charge information.
+        """
+        self.get_logger().info("Charge message received: %r" % message)
+
+        # Session operational
+        if self._check_session_started():
+            self.get_logger().warn("Session not started. Can't charge coil.")
+            return
+
+        # Input validation
+        error = self._validate_voltage_update_input(message)
+        if error.value != ChargeError.NO_ERROR:
+            feedback_msg = ChargeFeedback(id=message.event_info.id, error=error)
+            self.charge_feedback_publisher.publish(feedback_msg)
+            return
+
+        # Wait for execution condition
+        event_info: EventInfo = message.event_info
+        self._wait_for_execution_execution_condition(
+            event_info.execution_condition, event_info.execution_time
+        )
+
+        # Update voltage
+        channel = self.channels[message.channel]
+        feedback_msg = channel.charge(message.target_voltage, message.event_info)
+        self.charge_feedback_publisher.publish(feedback_msg)
+
+    def discharge_handler(self, message: Discharge) -> None:
+        """
+        Discharges the given channel to given voltage.
+
+        New voltage is not set immediately, and rather models the behaviour of a
+        capacitor to change the voltage. Discharge follows exponential decay.
+
+        Args:
+            message: contains the discharge information.
+        """
+        self.get_logger().info("Discharge message received: %r" % message)
+
+        # Session operational
+        if self._check_session_started():
+            self.get_logger().warn("Session not started. Can't discharge coil.")
+            return
+
+        # Input validation
+        error = self._validate_voltage_update_input(message)
+        if error.value != DischargeError.NO_ERROR:
+            feedback_msg = DischargeFeedback(id=message.event_info.id, error=error)
+            self.discharge_feedback_publisher.publish(feedback_msg)
+            return
+
+        # Wait for execution condition
+        event_info: EventInfo = message.event_info
+        self._wait_for_execution_execution_condition(
+            event_info.execution_condition, event_info.execution_time
+        )
+
+        # Update voltage
+        channel = self.channels[message.channel]
+        feedback_msg = channel.discharge(message.target_voltage, message.event_info)
+        self.discharge_feedback_publisher.publish(feedback_msg)
+
+    def _validate_pulse(self, message: Pulse) -> PulseError:
+        """
+        Validates the pulse message.
+
+        Args:
+            message: the message containing pulse information.
+
+        Returns:
+            First encountered error or no error if no errors.
+        """
+
+        # General errors
+        if not self.allow_stimulation:
+            self.get_logger().warn("Stimulation not allowed, skipping pulse.")
+            return PulseError(value=PulseError.NOT_ALLOWED)
+
+        if message.channel >= len(self.system_state.channel_states):
+            self.get_logger().warn("Invalid channel index: %d" % message.channel)
+            return PulseError(value=PulseError.INVALID_CHANNEL)
+
+        execution_condition = message.event_info.execution_condition
+        if execution_condition not in (
+            ExecutionCondition.IMMEDIATE,
+            ExecutionCondition.TIMED,
+            ExecutionCondition.WAIT_FOR_TRIGGER,
+        ):
+            return PulseError(value=PulseError.INVALID_EXECUTION_CONDITION)
+
+        # Channel specific errors
+        channel = self.channels[message.channel]
+
+        if channel.is_charging:
+            return PulseError(value=PulseError.OVERLAPPING_WITH_CHARGING)
+        if channel.is_discharging:
+            return PulseError(value=PulseError.OVERLAPPING_WITH_DISCHARGING)
+
+        # Waveform related errors
+        (
+            total_duration,
+            rising_duration,
+            falling_duration,
+        ) = self._calculate_waveform_durations(waveform=message.waveform)
+        if total_duration > self.settings.maximum_pulse_duration_ticks:
+            return PulseError(value=PulseError.INVALID_DURATIONS)
+
+        rise_fall_diff = abs(rising_duration - falling_duration)
+        if rise_fall_diff > self.settings.maximum_rising_falling_difference_ticks:
+            return PulseError(value=PulseError.INVALID_DURATIONS)
+
+        return PulseError(value=PulseError.NO_ERROR)
+
+    def _calculate_waveform_durations(
+        self, waveform: list[WaveformPiece]
+    ) -> (int, int, int):
+        """
+        Calculated the waveform durations
+
+        Args:
+            waveform: the pulse waveform
+
+        Returns:
+            total, rising and falling duration in ticks respectively.
+        """
+        total_duration = 0
+        rising_duration = 0
+        falling_duration = 0
+        for waveform_piece in waveform:
+            # TODO: Check waveform phase type requirements.
+            duration = waveform_piece.duration_in_ticks
+            total_duration += duration
+            match waveform_piece.waveform_phase:
+                case WaveformPhase.RISING:
+                    rising_duration += duration
+                case WaveformPhase.FALLING:
+                    falling_duration += duration
+
+        return total_duration, rising_duration, falling_duration
+
     def pulse_handler(self, message: Pulse) -> None:
         """
         Simulates the behaviour of giving a pulse.
@@ -377,59 +456,23 @@ class MTMSSimulator(Node):
         """
         self.get_logger().info("Pulse: %r" % message)
 
-        # Validate input
-        error = PulseError()
-        if not self.allow_stimulation:
-            self.get_logger().warn("Stimulation not allowed, skipping pulse.")
-            return
-        if message.channel >= len(self.system_state.channel_states):
-            self.get_logger().warn("Invalid channel index: %d" % message.channel)
-            error.value = PulseError.INVALID_CHANNEL
-            return
+        # Wait for execution condition
+        event_info = message.event_info
+        self._wait_for_execution_execution_condition(
+            event_info.execution_condition, event_info.execution_time
+        )
 
-        total_waveform_duration_in_ticks = 0
-        rising_duration_in_ticks = 0
-        falling_duration_in_ticks = 0
-        for waveform_piece in message.waveform:
-            # TODO: Check waveform phase type requirements.
-            duration = waveform_piece.duration_in_ticks
-            total_waveform_duration_in_ticks += duration
-            match waveform_piece.waveform_phase:
-                case WaveformPhase.RISING:
-                    rising_duration_in_ticks += duration
-                case WaveformPhase.FALLING:
-                    falling_duration_in_ticks += duration
-
-        if total_waveform_duration_in_ticks > self.settings.maximum_pulse_duration_ticks:
-            error.value = PulseError.INVALID_DURATIONS
-
-        rise_fall_diff = abs(rising_duration_in_ticks - falling_duration_in_ticks)
-        if rise_fall_diff > self.settings.maximum_rising_falling_difference_ticks:
-            error.value = PulseError.INVALID_DURATIONS
-
-        # TODO: Implement pulse count limit checks.
-
+        # Validate pulse
+        error = self._validate_pulse(message)
         if error.value != PulseError.NO_ERROR:
             feedback_msg = PulseFeedback(id=message.event_info.id, error=error)
             self.pulse_feedback_publisher.publish(feedback_msg)
             return
 
-        # Wait for execution condition
-        event_info = EventInfo()
-        self._wait_for_execution_execution_condition(
-            event_info.execution_condition, event_info.execution_time
-        )
-
         # Give pulse
-        channel_state: ChannelState = self.system_state.channel_states[message.channel]
-
-        channel_state.pulse_count += 1
-        channel_state.voltage = (
-            1 - self.PULSE_DISCHARGE_PERCENTAGE
-        ) * channel_state.voltage
-
-        # Send feedback
-        feedback_msg = PulseFeedback(id=message.event_info.id, error=error)
+        channel = self.channels[message.channel]
+        total_duration, _, _ = self._calculate_waveform_durations(message.waveform)
+        feedback_msg = channel.pulse(event_info.id, total_duration)
         self.pulse_feedback_publisher.publish(feedback_msg)
 
     def trigger_out_handler(self, trigger_out):
@@ -440,9 +483,22 @@ class MTMSSimulator(Node):
     # need to be translated to cpp later.
     def system_state_callback(self):
         msg = self.system_state
+
         msg.time = 0.0
-        if self.system_state.session_state.value == SessionState.STARTED:
+        if msg.session_state.value == SessionState.STARTED:
             msg.time = time.time() - self.session_start_time  # in seconds
+
+        for i in range(self.NUM_OF_CHANNELS):
+            channel = self.channels[i]
+            channel_state = ChannelState(
+                channel_index=i,
+                voltage=channel.current_voltage,
+                temperature=channel.temperature,
+                pulse_count=channel.pulse_count,
+                channel_error=channel.errors,
+            )
+            msg.channel_states[i] = channel_state
+
         self.system_state_publisher.publish(msg)
 
 
