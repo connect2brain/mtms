037ecf8e78bea52e9c14d7a1a7f9dfa34e1aeeb0
Oula Siljamo <oula.siljamo@aalto.fi>
2023-11-21 16:00:25 +0200

    ADD: Parameterized mTMS simulator properties

--- ros2_ws/src/simulators/mtms_simulator/launch/mtms_simulator.launch.py
+++ ros2_ws/src/simulators/mtms_simulator/launch/mtms_simulator.launch.py
@@ -10,15 +10,43 @@ def generate_launch_description():
             description="Logging level",
             default_value="info"
         )
+
+    channels_arg = DeclareLaunchArgument(
+            "channels",
+            description="Number of mTMS channels",
+            default_value="5"
+    )
+    max_voltage_arg = DeclareLaunchArgument(
+            "max-voltage",
+            description="Maximum voltage of the coils",
+            default_value="1500"
+    )
+
+    charge_rate_arg = DeclareLaunchArgument(
+            "charge-rate",
+            description="Charging rate of the coils in J/s",
+            default_value="1500"
+    )
+
     logger = LaunchConfiguration("log-level")
 
     node = Node(
         package="mtms_simulator",
         executable="mtms_simulator",
+        parameters=[
+            {
+                "channels": LaunchConfiguration("channels"),
+                "max_voltage": LaunchConfiguration("max-voltage"),
+                "charge_rate": LaunchConfiguration("charge-rate"),
+            }
+        ],
         arguments=["--ros-args", "--log-level", logger]
     )
 
     return LaunchDescription([
+        channels_arg,
+        max_voltage_arg,
+        charge_rate_arg,
         log_arg,
         node,
     ])
