01b72128dbd37b85f029a64e1fa0ad9be22c9e1a
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2024-02-05 14:30:33 +0200

    MOD: Triggerer to use LabJack instead of serial port

--- ros2_ws/src/pipeline/triggerer/include/triggerer.h
+++ ros2_ws/src/pipeline/triggerer/include/triggerer.h
@@ -20,18 +20,23 @@ public:
   ~Triggerer();
 
 private:
+  void attempt_labjack_connection();
+  bool safe_error_check(int err, const char* action);
+
   void handle_mtms_device_healthcheck(const std::shared_ptr<system_interfaces::msg::Healthcheck> msg);
 
   void process_event_trigger(const std::shared_ptr<event_interfaces::msg::EventTrigger> msg);
 
   rclcpp::Logger logger;
+  rclcpp::TimerBase::SharedPtr timer;
 
   rclcpp::Subscription<event_interfaces::msg::EventTrigger>::SharedPtr event_trigger_subscriber;
 
   rclcpp::Subscription<system_interfaces::msg::Healthcheck>::SharedPtr mtms_device_healthcheck_subscriber;
   rclcpp::Publisher<event_interfaces::msg::EventTrigger>::SharedPtr event_trigger_publisher;
 
-  struct sp_port *port;
+  /* LabJack connection */
+  int labjack_handle = -1;
 
   /* Healthcheck */
   uint8_t status;
