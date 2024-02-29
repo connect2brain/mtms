67e8d5d5b27b1770b5d2319d24f7d874e7302fb1
Oula Siljamo <oula.siljamo@aalto.fi>
2024-02-29 19:48:29 +0200

    FIX: Missing backslashes in ros_entrypoint.sh in EEG bridge

--- ros2_ws/src/bridges/eeg_bridge/ros_entrypoint.sh
+++ ros2_ws/src/bridges/eeg_bridge/ros_entrypoint.sh
@@ -7,6 +7,6 @@ source /app/ros2_ws/install/setup.bash
 ros2 launch eeg_bridge eeg_bridge.launch.py \
   log-level:="$ROS_LOG_LEVEL" \
   port:="$EEG_PORT" \
-  eeg-device:="$EEG_DEVICE"
-  turbolink-sampling-frequency:="$TURBOLINK_SAMPLING_FREQUENCY"
+  eeg-device:="$EEG_DEVICE" \
+  turbolink-sampling-frequency:="$TURBOLINK_SAMPLING_FREQUENCY" \
   turbolink-eeg-channel-count:="$TURBOLINK_EEG_CHANNEL_COUNT"
