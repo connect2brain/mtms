0ad360fb6e8a62230ecc08133c218742afb0d7d6
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2024-03-04 11:55:52 +0200

    ADD: Env variable for minimum pulse interval in real-time pipeline

--- ros2_ws/src/pipeline/decider/launch/decider.launch.py
+++ ros2_ws/src/pipeline/decider/launch/decider.launch.py
@@ -1,27 +1,43 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PythonExpression
 
 
 def generate_launch_description():
-    ld = LaunchDescription()
-
     log_arg = DeclareLaunchArgument(
         "log-level",
         default_value=["info"],
         description="Logging level",
     )
 
+    minimum_pulse_interval_arg = DeclareLaunchArgument(
+        "minimum-pulse-interval",
+        description="Minimum interval between consecutive pulses (in seconds)",
+    )
+
     logger = LaunchConfiguration("log-level")
+    minimum_pulse_interval = LaunchConfiguration("minimum-pulse-interval")
+
+    # Ensure that the value of the minimum_pulse_interval argument is a float.
+    #
+    # This makes it possible to pass either, e.g., "3" or "3.0" as the value of the minimum_pulse_interval argument.
+    minimum_pulse_interval_float = PythonExpression(['float(', minimum_pulse_interval, ')'])
 
     node = Node(
         package="decider",
         executable="decider",
         name="decider",
+        parameters=[
+            {
+                "minimum-pulse-interval": minimum_pulse_interval_float,
+            }
+        ],
         arguments=['--ros-args', '--log-level', logger]
     )
-    ld.add_action(node)
-    ld.add_action(log_arg)
 
-    return ld
+    return LaunchDescription([
+        log_arg,
+        minimum_pulse_interval_arg,
+        node
+    ])
