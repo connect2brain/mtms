73536e77622c0c9fd591d779c958149ccd167204
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2024-06-18 18:12:17 +0300

    FIX: Retry connecting to MATLAB license server at start-up

--- ros2_ws/src/targeting/waveform_approximator/ros_entrypoint.sh
+++ ros2_ws/src/targeting/waveform_approximator/ros_entrypoint.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Function to check if the MATLAB license server is available
+check_license_server() {
+  # Replace with your actual license server and port
+  local server="lic-matlab.aalto.fi"
+  local port="7313"
+
+  timeout 1 bash -c "</dev/tcp/$server/$port" >/dev/null 2>&1
+}
+
+# Wait for the license server to be available if $USE_MATLAB_LICENSE_SERVER is set
+if [ "$USE_MATLAB_LICENSE_SERVER" = "true" ]; then
+  echo "Checking MATLAB license server..."
+  until check_license_server; do
+    echo "MATLAB license server is not available. Retrying in 5 seconds..."
+    sleep 5
+  done
+  echo "MATLAB license server is available."
+fi
+
+# Register custom ROS messages
 echo "Registering ROS messages in MATLAB..."
 matlab -batch "ros2RegisterMessages('/home/matlab');"
 
