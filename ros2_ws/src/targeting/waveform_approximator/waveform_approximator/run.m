4a49b4497e85ba7d0a418fc93261647159b2a317
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2024-06-19 18:55:37 +0300

    MOD: Start MATLAB only once in waveform approximator
    Minimizes the start-up time of the container.

--- ros2_ws/src/targeting/waveform_approximator/waveform_approximator/run.m
+++ ros2_ws/src/targeting/waveform_approximator/waveform_approximator/run.m
@@ -1,3 +1,12 @@
+% XXX: Registering the messages would fit better in ros_entrypoint.sh,
+%   but running it separately there causes MATLAB to start twice, once
+%   for the registering the messages and once for running the present script.
+%
+%   In Aalto, running the Docker container with network_mode: host causes
+%   MATLAB to take around 2 minutes to start up, which is why we want to
+%   avoid starting it twice.
+ros2RegisterMessages('/home/matlab');
+
 wrapper = WaveformApproximatorWrapper();
 
 % An infinite loop to let the ROS node run in the background.
