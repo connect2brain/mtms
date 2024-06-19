f5bf742b232a478f4ddcd02c7a1d893fdeae52bd
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2024-06-19 10:09:08 +0300

    MOD: Improve example decider, preprocessor and presenter

--- projects/template/presenter/example.py
+++ projects/template/presenter/example.py
@@ -1,18 +1,12 @@
 from psychopy import visual, core
 import numpy as np
 
-import cpp_bindings
-
-
-# Override Python's native print() function.
-def print(x):
-    cpp_bindings.log(str(x))
+from common.utils import print, print_throttle
 
 
 class Presenter:
     def __init__(self):
         self.win = visual.Window(size=[2560, 1440], pos=[2560, 0], allowGUI=False)
-        pass
 
     def __del__(self):
         self.win.close()
