24abd74be306259128d13301e6f584cf3829c371
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2024-06-19 09:54:51 +0300

    ADD: log_throttle method to presenter and preprocessor

--- ros2_ws/src/pipeline/presenter/src/cpp_bindings.cpp
+++ ros2_ws/src/pipeline/presenter/src/cpp_bindings.cpp
@@ -11,6 +11,19 @@ void PresenterWrapper::log(const std::string& message) {
   RCLCPP_INFO(*logger_ptr, "From Python: %s", message.c_str());
 }
 
+void PresenterWrapper::log_throttle(const std::string& message, const double_t period) {
+  static double_t last_log_time = 0.0;
+
+  double_t current_time = rclcpp::Clock().now().seconds();
+  if (current_time - last_log_time < period) {
+    return;
+  }
+  RCLCPP_INFO(*logger_ptr, "From Python (throttled): %s", message.c_str());
+
+  last_log_time = current_time;
+}
+
 PYBIND11_MODULE(cpp_bindings, m) {
     m.def("log", &PresenterWrapper::log);
+    m.def("log_throttle", &PresenterWrapper::log_throttle);
 }
