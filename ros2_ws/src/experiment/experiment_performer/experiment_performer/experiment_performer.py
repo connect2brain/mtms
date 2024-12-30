39c21e5058c19d3bd905b1c4830fbde7f61e3eba
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2024-12-30 15:57:56 +0200

    MOD: Extract trial_interfaces from experiment_interfaces

--- ros2_ws/src/experiment/experiment_performer/experiment_performer/experiment_performer.py
+++ ros2_ws/src/experiment/experiment_performer/experiment_performer/experiment_performer.py
@@ -3,9 +3,12 @@ from threading import Event, Lock
 
 import numpy as np
 
-from experiment_interfaces.msg import ExperimentState, TrialTiming, TrialConfig
-from experiment_interfaces.action import PerformExperiment, PerformTrial
+from experiment_interfaces.msg import ExperimentState
+from experiment_interfaces.action import PerformExperiment
 from experiment_interfaces.srv import ValidateTrial, CountValidTrials, PauseExperiment, ResumeExperiment, CancelExperiment, LogTrial
+
+from trial_interfaces.action import PerformTrial
+from trial_interfaces.msg import TrialTiming, TrialConfig
 
 from std_msgs.msg import Bool
 
