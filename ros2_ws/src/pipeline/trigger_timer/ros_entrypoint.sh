2f7e4a4dcf8a5ccadc919ca3e006b238b56a1bf9
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@aalto.fi>
2024-12-30 13:31:11 +0200

    DEL: First attempt to delete NeuroSimo-related components

--- ros2_ws/src/pipeline/trigger_timer/ros_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-source /opt/ros/iron/setup.bash
-source /app/ros2_ws/install/setup.bash
-
-ros2 launch trigger_timer trigger_timer.launch.py log-level:="$ROS_LOG_LEVEL" \
-    triggering-tolerance:="$TRIGGERING_TOLERANCE"
