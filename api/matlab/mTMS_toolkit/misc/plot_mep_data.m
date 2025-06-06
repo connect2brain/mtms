a337cd01bc67b4d0cbb08592394d307144312c1e
lainem11 <mikael.h.laine@gmail.com>
2025-06-06 12:09:31 +0300

    FIX: De-mean filtered data

--- api/matlab/mTMS_toolkit/misc/plot_mep_data.m
+++ api/matlab/mTMS_toolkit/misc/plot_mep_data.m
@@ -23,9 +23,6 @@ function plot_mep_data(readout, mep_configuration, N)
     fs = 5000;
     time = (window(1) + (0:length(readout.buffer)-1) / fs) * 1000;  % Time in ms
     
-    % Baseline correction
-    %mep.buffer = mep.buffer - mean(mep.buffer(1:25));
-    
     % Shift existing subplots to the left
     subplot_data = circshift(subplot_data, -1);  % Shift all elements to the left
     subplot_time = circshift(subplot_time, -1);
@@ -53,26 +50,23 @@ function plot_mep_data(readout, mep_configuration, N)
             % Plot the EMG data
             current_time = subplot_time{i};
             current_data_filt = subplot_data{i}.buffer_filt;
+            current_data_filt = current_data_filt - mean(current_data_filt);
             current_data_raw = subplot_data{i}.buffer;
             current_data_raw = current_data_raw - mean(current_data_raw);
             plot(current_time, current_data_filt, 'LineWidth', 2);
             hold on;
             plot(current_time,current_data_raw,'LineStyle','--','Color',[0.7,0.7,0.7])
+
             % Mark the maximum and minimum points
-            
             max_mep = current_data_filt(subplot_data{i}.max_ind);
             min_mep = current_data_filt(subplot_data{i}.min_ind);
             
-            %max_signal = max([max(mep.buffer),max(subplot_data{i})]);
-            %min_signal = min([min(mep.buffer),min(subplot_data{i})]);
-            %signal_peak = max(abs([max_signal,min_signal]));
             plot(current_time(subplot_data{i}.min_ind), min_mep, '.r', 'MarkerSize', 40);
             plot(current_time(subplot_data{i}.max_ind), max_mep, '.r', 'MarkerSize', 40);
-            %ylim([min_signal-signal_peak*0.2, max_mep+signal_peak*0.2])
             mep_amp = abs(max_mep-min_mep);
             title(sprintf("N = %i ; A = %.0f µV",pulse_index+i-N,mep_amp));
-            %annotation('textarrow',x,y,'String','y = x ')
-            % Label the axes (only for the first subplot)
+
+            % Label the axes
             if i == 1
                 ylabel('EMG (µV)');
             end
