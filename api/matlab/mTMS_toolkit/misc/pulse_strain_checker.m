b067a4a22c97e0fc204c3da3216ca33f27263025
lainem11 <mikael.h.laine@gmail.com>
2025-12-01 15:22:33 +0200

    UPD: Pulse strain now integrates strain over the pulse waveform

--- api/matlab/mTMS_toolkit/misc/pulse_strain_checker.m
+++ api/matlab/mTMS_toolkit/misc/pulse_strain_checker.m
@@ -11,50 +11,47 @@ classdef pulse_strain_checker < handle
             obj.strain_limit = obj.calculate_strain_limit_from_load_voltages([0,0,0,1200,1200]);
         end
 
-        function pulse_strain = calculate_strain(obj,max_currents)
-            assert(size(max_currents,2)==5,"Pulse strain calculation requires five values")
+        function pulse_strain = calculate_strain(obj,current_waveforms)
+            assert(size(current_waveforms,2)==5,"Pulse strain calculation requires N by 5 matrix.")
 
-            coil_5_strain = (sqrt(0.9.*max_currents(:,1).^2+max_currents(:,2).^2).*0.2...
-           + sqrt(0.9.*max_currents(:,3).^2+max_currents(:,4).^2)).*max_currents(:,5);
+            coil_5_strain = sum((sqrt(0.9.*current_waveforms(:,1).^2+current_waveforms(:,2).^2).*0.2...
+           + sqrt(0.9.*current_waveforms(:,3).^2+current_waveforms(:,4).^2)).*current_waveforms(:,5));
 
 
-            coil_1_strain = (0.3.*max_currents(:,2)...
-                   +0.7*sqrt(max_currents(:,3).^2+0.9*max_currents(:,4).^2)...
-                   +0.2.*max_currents(:,5)).*max_currents(:,1);
+            coil_1_strain = sum((0.3.*current_waveforms(:,2)...
+                   +0.7*sqrt(current_waveforms(:,3).^2+0.9*current_waveforms(:,4).^2)...
+                   +0.2.*current_waveforms(:,5)).*current_waveforms(:,1));
             
             pulse_strain = max([coil_5_strain,coil_1_strain]);
         end
 
         function strain_limit = calculate_strain_limit_from_load_voltages(obj,load_voltages)
-            % Assume 60 us rise waveform
-            waveforms{1} = struct('mode','r','duration',60*1e-6);
+            % Assume pre-defined monophasic waveform
+            waveforms{1} = struct('mode',{'r','h','f'},'duration',{60e-6,30e-6,37e-6});
             waveforms = repmat(waveforms,1,5);
             
-            max_currents = [];
             for i = 1:length(waveforms)
                 obj.approximator.select_coil(i);
                 state_trajectory = obj.approximator.generate_state_trajectory_from_waveform(abs(load_voltages(i)),waveforms{i});
-                max_currents(i) = max(abs([state_trajectory.I_coil]));
+                current_waveforms(:,i) = abs([state_trajectory.I_coil]);
             end
 
-            strain_limit = obj.calculate_strain(max_currents);
+            strain_limit = obj.calculate_strain(current_waveforms);
         end
 
         function [strain_ok, stimulation_intensity_multiplier] = check_pulse_strain(obj,load_voltages,waveforms)
                 N_pulses = size(load_voltages,1);
-                max_currents = zeros(size(load_voltages));
 
                 for i = 1:N_pulses
                     for j = 1:length(waveforms)
                         obj.approximator.select_coil(j);
                         state_trajectory = obj.approximator.generate_state_trajectory_from_waveform(abs(load_voltages(i,j)),waveforms{i,j});
-                        max_currents(i,j) = max(abs([state_trajectory.I_coil]));
+                        current_waveforms(:,j) = abs([state_trajectory.I_coil]);
                     end
+                    pulse_strain(i) = obj.calculate_strain(current_waveforms);
                 end
 
-                pulse_strain = obj.calculate_strain(max_currents);
-
-                strain_ok = pulse_strain < obj.strain_limit;
+                strain_ok = pulse_strain <= obj.strain_limit;
                 stimulation_intensity_multiplier = sqrt(obj.strain_limit / pulse_strain);
         end
 
