2355a3a8a605b37bd42af5619e29cc8d47572c6f
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-03-18 21:48:57 +0200

    CLP: Remove unused redux hooks and store from UI

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './providers/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
