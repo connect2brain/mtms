864b8080c941e7909946b9209003290ecaf35cd2
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-03-18 08:58:42 +0200

    MOD: Remove traces of using actions in UI

--- src/rosbridge/ros_entrypoint.sh
+++ src/rosbridge/ros_entrypoint.sh
@@ -4,6 +4,4 @@ set -e
 source /opt/ros/jazzy/setup.bash
 source /app/install/setup.bash
 
-# Note: Action calls can take a while, so run them in separate threads to avoid blocking. Service calls should be fast, so
-#   run them consecutively in the main thread. This logic seems to work for now in the front-end.
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml call_services_in_new_thread:=false send_action_goals_in_new_thread:=true port:=$ROSBRIDGE_PORT
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml port:=$ROSBRIDGE_PORT
