ed822b97d03851ea3ea482b1ed9f3ecc691f929a
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-03-31 11:09:04 +0300

    FIX: Remove timeouts from HealthProvider.tsx

--- front/src/providers/HealthProvider.tsx
+++ front/src/providers/HealthProvider.tsx
@@ -74,60 +74,24 @@ export const HealthProvider: React.FC<HealthProviderProps> = ({ children }) => {
       messageType: 'mtms_system_interfaces/ComponentHealth',
     })
 
-    let eegTimeout: NodeJS.Timeout | null = null
-    let mtmsTimeout: NodeJS.Timeout | null = null
-    let mepTimeout: NodeJS.Timeout | null = null
-    let remoteControllerTimeout: NodeJS.Timeout | null = null
-    let timebaseCalibratorTimeout: NodeJS.Timeout | null = null
-
     eegSubscriber.subscribe((message) => {
       setEegHealth(message)
-      if (eegTimeout) {
-        clearTimeout(eegTimeout)
-      }
-      eegTimeout = setTimeout(() => {
-        setEegHealth(null)
-      }, 1200)
     })
 
     mtmsSubscriber.subscribe((message) => {
       setMtmsDeviceHealth(message)
-      if (mtmsTimeout) {
-        clearTimeout(mtmsTimeout)
-      }
-      mtmsTimeout = setTimeout(() => {
-        setMtmsDeviceHealth(null)
-      }, 1200)
     })
 
     mepSubscriber.subscribe((message) => {
       setMepHealth(message)
-      if (mepTimeout) {
-        clearTimeout(mepTimeout)
-      }
-      mepTimeout = setTimeout(() => {
-        setMepHealth(null)
-      }, 1200)
     })
 
     remoteControllerSubscriber.subscribe((message) => {
       setRemoteControllerHealth(message)
-      if (remoteControllerTimeout) {
-        clearTimeout(remoteControllerTimeout)
-      }
-      remoteControllerTimeout = setTimeout(() => {
-        setRemoteControllerHealth(null)
-      }, 1200)
     })
 
     timebaseCalibratorSubscriber.subscribe((message) => {
       setTimebaseCalibratorHealth(message)
-      if (timebaseCalibratorTimeout) {
-        clearTimeout(timebaseCalibratorTimeout)
-      }
-      timebaseCalibratorTimeout = setTimeout(() => {
-        setTimebaseCalibratorHealth(null)
-      }, 1200)
     })
 
     return () => {
@@ -136,21 +100,6 @@ export const HealthProvider: React.FC<HealthProviderProps> = ({ children }) => {
       mepSubscriber.unsubscribe()
       remoteControllerSubscriber.unsubscribe()
       timebaseCalibratorSubscriber.unsubscribe()
-      if (eegTimeout) {
-        clearTimeout(eegTimeout)
-      }
-      if (mtmsTimeout) {
-        clearTimeout(mtmsTimeout)
-      }
-      if (mepTimeout) {
-        clearTimeout(mepTimeout)
-      }
-      if (remoteControllerTimeout) {
-        clearTimeout(remoteControllerTimeout)
-      }
-      if (timebaseCalibratorTimeout) {
-        clearTimeout(timebaseCalibratorTimeout)
-      }
     }
   }, [])
 
